a6cdce6bb43051393e5a7a7c831e79000ada2079
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-05-07 16:32:29 -0400

    make sync a navigation page

--- src/App/Pages/MainPage.cs
+++ src/App/Pages/MainPage.cs
@@ -10,19 +10,22 @@ namespace Bit.App.Pages
         {
             var settingsNavigation = new NavigationPage(new SettingsPage());
             var vaultNavigation = new NavigationPage(new VaultListPage());
-            var syncPage = new SyncPage();
+            var syncNavigation = new NavigationPage(new SyncPage());
 
-            vaultNavigation.BarBackgroundColor = settingsNavigation.BarBackgroundColor = Color.FromHex("3c8dbc");
-            vaultNavigation.BarTextColor = settingsNavigation.BarTextColor = Color.FromHex("ffffff");
+            vaultNavigation.BarBackgroundColor = settingsNavigation.BarBackgroundColor = syncNavigation.BarBackgroundColor = Color.FromHex("3c8dbc");
+            vaultNavigation.BarTextColor = settingsNavigation.BarTextColor = syncNavigation.BarTextColor = Color.FromHex("ffffff");
 
             vaultNavigation.Title = AppResources.MyVault;
             vaultNavigation.Icon = "fa-lock";
+
+            syncNavigation.Title = AppResources.Sync;
+            syncNavigation.Icon = "fa-refresh";
 
             settingsNavigation.Title = AppResources.Settings;
             settingsNavigation.Icon = "fa-cogs";
 
             Children.Add(vaultNavigation);
-            Children.Add(syncPage);
+            Children.Add(syncNavigation);
             Children.Add(settingsNavigation);
         }
     }
