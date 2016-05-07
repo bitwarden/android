1664c3fbf0bcd7e2473a4cee269ac32d553fa4dd
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-05-07 01:54:44 -0400

    Added and configured iOS Extension project with ExtensionPreprocessingJS for webpage analysis.

---
+++ src/iOS.Extension/ActionViewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using MobileCoreServices;
+using Foundation;
+using UIKit;
+
+namespace Bit.iOS.Extension
+{
+    public partial class ActionViewController : UIViewController
+    {
+        public ActionViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public string Content { get; set; }
+        public Uri Uri { get; set; }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            foreach(var item in ExtensionContext.InputItems)
+            {
+                foreach(var itemProvider in item.Attachments)
+                {
+                    if(itemProvider.HasItemConformingTo(UTType.PropertyList))
+                    {
+                        itemProvider.LoadItem(UTType.PropertyList, null, delegate (NSObject list, NSError error)
+                        {
+                            if(list != null)
+                            {
+                                var dict = list as NSDictionary;
+                                var result = dict[NSJavaScriptExtension.PreprocessingResultsKey];
+                                if(result != null)
+                                {
+                                    Content = result.ValueForKey(new NSString("content")) as NSString;
+                                    Uri = new Uri(result.ValueForKey(new NSString("uri")) as NSString);
+                                    Console.WriteLine("BITWARDEN LOG, Content: {0}", Content);
+                                    Console.WriteLine("BITWARDEN LOG, Uri: {0}", Uri);
+                                }
+                            }
+                        });
+                        break;
+                    }
+                }
+            }
+        }
+
+        partial void DoneClicked(NSObject sender)
+        {
+
+        }
+    }
+}
---
+++ src/iOS.Extension/extension.js
@@ -0,0 +1,16 @@
+﻿var BitwardenExtension = function () { };
+
+BitwardenExtension.prototype = {
+    run: function (arguments) {
+        var args = {
+            content: document.body.innerHTML,
+            uri: document.baseURI
+        };
+        arguments.completionFunction(args);
+    },
+    finalize: function (arguments) {
+        alert('finalize');
+    }
+};
+
+var ExtensionPreprocessingJS = new BitwardenExtension;
