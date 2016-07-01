515412f8632ea44be97153be6c918d3df26a98c9
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-06-30 21:40:44 -0400

    Added device identifier as header to api calls

--- src/App/Utilities/TokenHttpRequestMessage.cs
+++ src/App/Utilities/TokenHttpRequestMessage.cs
@@ -11,7 +11,12 @@ namespace Bit.App
         public TokenHttpRequestMessage()
         {
             var authService = Resolver.Resolve<IAuthService>();
+            var appIdService = Resolver.Resolve<IAppIdService>();
             Headers.Add("Authorization", $"Bearer {authService.Token}");
+            if(!string.IsNullOrWhiteSpace(appIdService.AppId))
+            {
+                Headers.Add("DeviceIdentifier", appIdService.AppId);
+            }
         }
 
         public TokenHttpRequestMessage(object requestObject)
