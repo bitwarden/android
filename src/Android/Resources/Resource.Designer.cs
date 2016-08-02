c42a1504a7796f3a98cc6d728b994ea1e2d5416e
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-08-01 21:15:09 -0400

    upadted package and isntalled pclcrypto into device projects

--- src/Android/Resources/Resource.Designer.cs
+++ src/Android/Resources/Resource.Designer.cs
@@ -184,6 +184,8 @@ namespace Bit.Android
 			global::HockeyApp.Resource.Style.HockeyApp_EditTextStyle = global::Bit.Android.Resource.Style.HockeyApp_EditTextStyle;
 			global::HockeyApp.Resource.Style.HockeyApp_SingleLineInputStyle = global::Bit.Android.Resource.Style.HockeyApp_SingleLineInputStyle;
 			global::ModernHttpClient.Resource.String.library_name = global::Bit.Android.Resource.String.library_name;
+			global::PCLCrypto.Resource.String.ApplicationName = global::Bit.Android.Resource.String.ApplicationName;
+			global::PCLCrypto.Resource.String.Hello = global::Bit.Android.Resource.String.Hello;
 			global::Plugin.Fingerprint.Resource.Drawable.fingerprint_white = global::Bit.Android.Resource.Drawable.fingerprint_white;
 			global::Plugin.Fingerprint.Resource.Id.fingerprint_btnCancel = global::Bit.Android.Resource.Id.fingerprint_btnCancel;
 			global::Plugin.Fingerprint.Resource.Id.fingerprint_txtReason = global::Bit.Android.Resource.Id.fingerprint_txtReason;
