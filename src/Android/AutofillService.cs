873ebee256510f31872d7977c1fa3ba8504f3257
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-08-26 18:56:09 -0400

    Rename accessibility service to AutofillService. Remove autofill service from app for now until it is being worked on again.

---
+++ src/Android/AutofillService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.AccessibilityServices;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Views.Accessibility;
+using Android.Widget;
+
+namespace Bit.Android
+{
+    //[Service(Permission = "android.permission.BIND_ACCESSIBILITY_SERVICE", Label = "bitwarden")]
+    //[IntentFilter(new string[] { "android.accessibilityservice.AccessibilityService" })]
+    //[MetaData("android.accessibilityservice", Resource = "@xml/accessibilityservice")]
+    public class AutofillService : AccessibilityService
+    {
+        public override void OnAccessibilityEvent(AccessibilityEvent e)
+        {
+            var eventType = e.EventType;
+            switch(eventType)
+            {
+                case EventTypes.ViewTextSelectionChanged:
+                    if(e.Source.Password && string.IsNullOrWhiteSpace(e.Source.Text))
+                    {
+                        var bundle = new Bundle();
+                        bundle.PutCharSequence(AccessibilityNodeInfo.ActionArgumentSetTextCharsequence, "mypassword");
+                        e.Source.PerformAction(global::Android.Views.Accessibility.Action.SetText, bundle);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override void OnInterrupt()
+        {
+
+        }
+    }
+}
