d4749c139b04ceeed8675f5898ca4e8c9ddf670b
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-02-04 21:19:54 -0500

    increment version script

---
+++ src/Android/increment-version.ps1
@@ -0,0 +1,8 @@
+﻿$rootPath = "%APPVEYOR_BUILD_FOLDER%";
+$newVersionCode = "%APPVEYOR_BUILD_NUMBER%";
+
+$xml=New-Object XML;
+$xml.Load($rootPath + "src\Android\Properties\AndroidManifest.xml");
+$node=$xml.SelectNodes("/manifest");
+$node.SetAttribute("android:versionCode", $newVersionCode);
+$xml.Save($rootPath + "src\Android\Properties\AndroidManifest.xml");
