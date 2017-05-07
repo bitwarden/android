3f5c8fe2cb81f8b8e628b70588930bf550f19554
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-06 21:21:20 -0400

    IdentityHttpClient url fix

--- src/App/Utilities/IdentityHttpClient.cs
+++ src/App/Utilities/IdentityHttpClient.cs
@@ -22,7 +22,7 @@ namespace Bit.App
             //BaseAddress = new Uri("http://169.254.80.80:33656"); // Desktop from VS Android Emulator
             //BaseAddress = new Uri("http://192.168.1.8:33656"); // Desktop
             //BaseAddress = new Uri("https://identity-api.bitwarden.com"); // Preview
-            BaseAddress = new Uri("https://api.bitwarden.com"); // Production
+            BaseAddress = new Uri("https://identity.bitwarden.com"); // Production
             DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
     }
