cc12ae7712aad24002895525942c4dca3df847c5
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-09-21 10:50:15 -0400

    sub-classed login props for cipher req

--- src/App/Models/Api/Request/CipherRequest.cs
+++ src/App/Models/Api/Request/CipherRequest.cs
@@ -10,23 +10,41 @@ namespace Bit.App.Models.Api
             OrganizationId = login.OrganizationId;
             FolderId = login.FolderId;
             Name = login.Name?.EncryptedString;
-            Uri = login.Uri?.EncryptedString;
-            Username = login.Username?.EncryptedString;
-            Password = login.Password?.EncryptedString;
             Notes = login.Notes?.EncryptedString;
-            Totp = login.Totp?.EncryptedString;
             Favorite = login.Favorite;
+
+            switch(Type)
+            {
+                case CipherType.Login:
+                    Login = new LoginType(login);
+                    break;
+                default:
+                    break;
+            }
         }
 
         public CipherType Type { get; set; }
         public string OrganizationId { get; set; }
         public string FolderId { get; set; }
-        public string Name { get; set; }
-        public string Uri { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Notes { get; set; }
-        public string Totp { get; set; }
         public bool Favorite { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public LoginType Login { get; set; }
+
+        public class LoginType
+        {
+            public LoginType(Login login)
+            {
+                Uri = login.Uri?.EncryptedString;
+                Username = login.Username?.EncryptedString;
+                Password = login.Password?.EncryptedString;
+                Totp = login.Totp?.EncryptedString;
+            }
+
+            public string Uri { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Totp { get; set; }
+        }
     }
 }
