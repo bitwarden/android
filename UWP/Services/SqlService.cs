9f95a3732505fa024a308d70bbf8c24b747cee5c
Hicham Boushaba <hicham.boushaba@gmail.com>
2017-09-27 21:56:52 +0100

    SqliteService implementation and sqlite-net-pcl update (#bug https://stackoverflow.com/questions/45418669/xamarin-forms-pcl-assemly-issue)

--- UWP/Services/SqlService.cs
+++ UWP/Services/SqlService.cs
@@ -11,7 +11,12 @@ namespace Bit.UWP.Services
     {
         public SQLiteConnection GetConnection()
         {
-            throw new NotImplementedException();
+            var sqliteFilename = "bitwarden.db3";
+
+            string path = Path.Combine(ApplicationData.Current.LocalFolder.Path, sqliteFilename);
+            var conn = new SQLite.SQLiteConnection(path, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.SharedCache);
+            // Return the database connection 
+            return conn;
         }
     }
 }
