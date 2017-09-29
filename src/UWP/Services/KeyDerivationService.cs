625c77b3dbd872cf366f1f20b9901887e4e84ce3
Hicham Boushaba <hicham.boushaba@gmail.com>
2017-09-29 15:51:15 +0100

    Remove dependency on BouncyCastle, and calculate key derivation using native support

--- src/UWP/Services/KeyDerivationService.cs
+++ src/UWP/Services/KeyDerivationService.cs
@@ -1,7 +1,4 @@
 ﻿using Bit.App.Abstractions;
-using Org.BouncyCastle.Crypto.Digests;
-using Org.BouncyCastle.Crypto.Generators;
-using Org.BouncyCastle.Crypto.Parameters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,14 +15,24 @@ namespace Bit.UWP.Services
 {
     public class KeyDerivationService : IKeyDerivationService
     {
-        private const int KeyLength = 256; // 32 bytes
+        private const int KeyLength = 32; // 32 bytes
 
         //todo review this
         public byte[] DeriveKey(byte[] password, byte[] salt, uint rounds)
         {
-            var generator = new Pkcs5S2ParametersGenerator(new Sha256Digest());
-            generator.Init(password, salt, Convert.ToInt32(rounds));
-            return ((KeyParameter)generator.GenerateDerivedMacParameters(KeyLength)).GetKey();
+            IBuffer buffSalt = salt.AsBuffer();
+            IBuffer buffPassword = password.AsBuffer();
+            KeyDerivationAlgorithmProvider provider = KeyDerivationAlgorithmProvider.OpenAlgorithm(KeyDerivationAlgorithmNames.Pbkdf2Sha256);
+            KeyDerivationParameters pbkdf2Params = KeyDerivationParameters.BuildForPbkdf2(buffSalt, rounds);
+            CryptographicKey keyOriginal = provider.CreateKey(buffPassword);
+
+            IBuffer keyDerived = CryptographicEngine.DeriveKeyMaterial(
+                keyOriginal,
+                pbkdf2Params,
+                KeyLength
+               );
+
+            return keyDerived.ToArray();
         }
     }
 }
