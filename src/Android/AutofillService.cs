9a22a1dbf4da70d46d6be4e33e7ac1e8cb917fc9
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-10-12 22:53:41 -0400

    add autofill support for edge browser

--- src/Android/AutofillService.cs
+++ src/Android/AutofillService.cs
@@ -55,7 +55,8 @@ namespace Bit.Android
             new Browser("com.linkbubble.playstore", "url_text"),
             new Browser("com.ksmobile.cb", "address_bar_edit_text"),
             new Browser("acr.browser.lightning", "search"),
-            new Browser("acr.browser.barebones", "search")
+            new Browser("acr.browser.barebones", "search"),
+            new Browser("com.microsoft.emmx", "url_bar")
         }.ToDictionary(n => n.PackageName);
 
         private readonly IAppSettingsService _appSettings;
