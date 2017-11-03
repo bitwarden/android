d4f6e9c587ff780b8d6c22f1e562d4bedb277cf8
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-11-03 17:22:53 -0400

    no max lines on android labels

---
+++ src/Android/Controls/CustomLabelRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Bit.Android.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace Bit.Android.Controls
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetMaxLines(int.MaxValue);
+        }
+    }
+}
