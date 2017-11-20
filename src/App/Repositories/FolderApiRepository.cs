7ad639599ade432ef486179b162c9949f2303d37
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-11-20 07:32:58 -0500

    added slash to folder route

--- src/App/Repositories/FolderApiRepository.cs
+++ src/App/Repositories/FolderApiRepository.cs
@@ -14,6 +14,6 @@ namespace Bit.App.Repositories
             : base(connectivity, httpService, tokenService)
         { }
 
-        protected override string ApiRoute => "folders";
+        protected override string ApiRoute => "/folders";
     }
 }
