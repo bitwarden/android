c180422e8bf6d00b0f21459330de0692112c0bef
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-14 10:19:50 -0500

    fixes for UWP

--- src/UWP/Services/UwpPushNotificationService.cs
+++ src/UWP/Services/UwpPushNotificationService.cs
@@ -31,7 +31,7 @@ namespace Bit.UWP.Services
             Debug.WriteLine("Registering call back for Push Notification Channel");
             _channel.PushNotificationReceived += Channel_PushNotificationReceived;
 
-            CrossPushNotification.PushNotificationListener.OnRegistered(Token, Device.Windows);
+            CrossPushNotification.PushNotificationListener.OnRegistered(Token, Device.UWP);
         }
 
         private void Channel_PushNotificationReceived(PushNotificationChannel sender, PushNotificationReceivedEventArgs args)
@@ -59,7 +59,7 @@ namespace Bit.UWP.Services
             }
 
             Debug.WriteLine("Sending JObject to PushNotificationListener " + args.NotificationType);
-            CrossPushNotification.PushNotificationListener.OnMessage(jobject, Device.Windows);
+            CrossPushNotification.PushNotificationListener.OnMessage(jobject, Device.UWP);
         }
 
         public void Unregister()
@@ -70,7 +70,7 @@ namespace Bit.UWP.Services
                 _channel = null;
             }
 
-            CrossPushNotification.PushNotificationListener.OnUnregistered(Device.Windows);
+            CrossPushNotification.PushNotificationListener.OnUnregistered(Device.UWP);
         }
     }
 
