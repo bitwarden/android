d4aaa547a7020b41c4a1d71f1ced7dc07969e18c
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-01-10 14:55:18 -0500

    remove libs for fdroid build

---
+++ src/Android/fdroid-remove-libs.ps1
@@ -0,0 +1,27 @@
+﻿$rootPath = $args[0];
+
+# Android.csproj
+
+$xml=New-Object XML;
+$xml.Load($rootPath + "\src\Android\Android.csproj");
+
+$ns=New-Object System.Xml.XmlNamespaceManager($xml.NameTable);
+$ns.AddNamespace("ns", $xml.DocumentElement.NamespaceURI);
+
+$firebaseNode=$xml.SelectSingleNode("/ns:Project/ns:ItemGroup/ns:PackageReference[@Include='Xamarin.Firebase.Messaging']", $ns);
+$firebaseNode.ParentNode.RemoveChild($firebaseNode);
+
+$playServiceNode=$xml.SelectSingleNode("/ns:Project/ns:ItemGroup/ns:PackageReference[@Include='Xamarin.GooglePlayServices.Analytics']", $ns);
+$playServiceNode.ParentNode.RemoveChild($playServiceNode);
+
+$xml.Save($rootPath + "\src\Android\Android.csproj");
+
+# App.csproj
+
+$xml=New-Object XML;
+$xml.Load($rootPath + "\src\App\App.csproj");
+
+$hockeyNode=$xml.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='HockeySDK.Xamarin']");
+$hockeyNode.ParentNode.RemoveChild($hockeyNode);
+
+$xml.Save($rootPath + "\src\App\App.csproj");
