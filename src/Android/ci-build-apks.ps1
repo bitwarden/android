b3573f94826dc850a0c8f9d75b1a76be157bfbd3
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-01-11 08:36:37 -0500

    choose/when fdroid conditional on packages

--- src/Android/ci-build-apks.ps1
+++ src/Android/ci-build-apks.ps1
@@ -1,7 +1,5 @@
 ﻿$rootPath = $env:APPVEYOR_BUILD_FOLDER;
-
 $androidPath = $($rootPath + "\src\Android\Android.csproj");
-$appPath = $($rootPath + "\src\App\App.csproj");
 
 echo "##### Increment Version"
 
@@ -33,51 +31,8 @@ $signedApkDestPath = $($rootPath + "\com.x8bit.bitwarden-" + $env:APPVEYOR_BUILD
 
 Copy-Item $signedApkPath $signedApkDestPath
 
-echo "##### Clean Android and App"
-
-msbuild "$($androidPath)" "/t:Clean" "/p:Configuration=FDroid"
-msbuild "$($appPath)" "/t:Clean" "/p:Configuration=FDroid"
-
-echo "##### Backup project files"
-
-Copy-Item $androidPath $($androidPath + ".original");
-Copy-Item $appPath $($appPath + ".original");
-
-echo "##### Uninstall from Android.csproj"
-
-$xml=New-Object XML;
-$xml.Load($androidPath);
-
-$ns=New-Object System.Xml.XmlNamespaceManager($xml.NameTable);
-$ns.AddNamespace("ns", $xml.DocumentElement.NamespaceURI);
-
-$firebaseNode=$xml.SelectSingleNode("/ns:Project/ns:ItemGroup/ns:PackageReference[@Include='Xamarin.Firebase.Messaging']", $ns);
-$firebaseNode.ParentNode.RemoveChild($firebaseNode);
-
-$playServiceNode=$xml.SelectSingleNode("/ns:Project/ns:ItemGroup/ns:PackageReference[@Include='Xamarin.GooglePlayServices.Analytics']", $ns);
-$playServiceNode.ParentNode.RemoveChild($playServiceNode);
-
-$xml.Save($androidPath);
-
-echo "##### Uninstall from App.csproj"
-
-$xml=New-Object XML;
-$xml.Load($appPath);
-
-$hockeyNode=$xml.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='HockeySDK.Xamarin']");
-$hockeyNode.ParentNode.RemoveChild($hockeyNode);
-
-$xml.Save($appPath);
-
-echo "##### Restore NuGet"
-
-$nugetPath = $($rootPath + "\nuget.exe");
-
-Invoke-Expression "& `"$nugetPath`" restore"
-
 echo "##### Build and Sign FDroid Configuration"
 
-msbuild "$($androidPath)" "/logger:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" "/p:Configuration=FDroid"
 msbuild "$($androidPath)" "/t:SignAndroidPackage" "/p:Configuration=FDroid" "/p:AndroidKeyStore=true" "/p:AndroidSigningKeyAlias=bitwarden" "/p:AndroidSigningKeyPass=$($env:keystore_password)" "/p:AndroidSigningKeyStore=8bit.keystore" "/p:AndroidSigningStorePass=$($env:keystore_password)" "/v:quiet"
 
 echo "##### Copy FDroid apk to project root"
