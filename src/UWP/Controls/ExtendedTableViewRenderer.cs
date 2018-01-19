592c7951dfdf30e7bf7d0d61324f768dd9c1ea57
Alistair Francis <alistair@alistair23.me>
2018-01-19 11:04:58 -0800

    UWP: Add a ExtendedTableViewRenderer (#260)
    Add a base ExtendedTableViewRenderer we can use.
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

---
+++ src/UWP/Controls/ExtendedTableViewRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Bit.App.Controls;
+using Bit.UWP.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+[assembly: ExportRenderer(typeof(ExtendedTableView), typeof(ExtendedTableViewRenderer))]
+namespace Bit.UWP.Controls
+{
+    public class ExtendedTableViewRenderer : TableViewRenderer
+    {
+        public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
+        {
+            var baseSize = new Size(Control.Width, Control.Height);
+
+            return new SizeRequest(new Size(baseSize.Width, baseSize.Height));
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TableView> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
