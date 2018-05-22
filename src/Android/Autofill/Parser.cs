add5189bb106906b84839ad9fcad108418f7cbf5
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-21 23:26:54 -0400

    add via to oreo trusted browsers

--- src/Android/Autofill/Parser.cs
+++ src/Android/Autofill/Parser.cs
@@ -14,7 +14,7 @@ namespace Bit.Android.Autofill
             "com.opera.browser.beta","com.opera.mini.native","com.chrome.dev","com.chrome.canary",
             "com.google.android.apps.chrome","com.google.android.apps.chrome_dev","com.yandex.browser",
             "com.sec.android.app.sbrowser","com.sec.android.app.sbrowser.beta","org.codeaurora.swe.browser",
-            "com.amazon.cloud9","org.mozilla.klar", "com.duckduckgo.mobile.android"
+            "com.amazon.cloud9","org.mozilla.klar","com.duckduckgo.mobile.android","mark.via.gp"
         };
 
         public static HashSet<string> ExcludedPackageIds = new HashSet<string>
