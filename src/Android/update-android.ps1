3e0097c22cf8c3edd4f574d7f3520713448ccbd1
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-09-04 07:58:09 -0400

    update android sdks on appveyor

---
+++ src/Android/update-android.ps1
@@ -0,0 +1,28 @@
+﻿$AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\android" 
+#$AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android"
+Function Get-AndroidSDKs() { 
+    $output = & $AndroidToolPath list sdk --all 
+    $sdks = $output |% { 
+        if ($_ -match '(?<index>\d+)- (?<sdk>.+), revision (?<revision>[\d\.]+)') { 
+            $sdk = New-Object PSObject 
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Index -Value $Matches.index 
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Name -Value $Matches.sdk 
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Revision -Value $Matches.revision 
+            $sdk 
+        } 
+    } 
+    $sdks 
+}
+Function Install-AndroidSDK() { 
+    [CmdletBinding()] 
+    Param( 
+        [Parameter(Mandatory=$true, Position=0)] 
+        [PSObject[]]$sdks 
+    )
+    $sdkIndexes = $sdks |% { $_.Index } 
+    $sdkIndexArgument = [string]::Join(',', $sdkIndexes) 
+    Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument 
+}
+$sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 28*' } 
+Install-AndroidSDK -sdks $sdks
+dir "${env:ProgramFiles(x86)}\Android\android-sdk\platforms"
