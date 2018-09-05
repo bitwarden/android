fdcc6b0cb852c641198b134c102da058a4029bed
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-09-05 17:19:53 -0400

    change back to pwd, out-null

--- src/Android/update-android.ps1
+++ src/Android/update-android.ps1
@@ -1,5 +1,13 @@
-﻿cd "${env:ProgramFiles(x86)}\Android\android-sdk\tools\bin"
+﻿dir "${env:ProgramFiles(x86)}\Android\android-sdk\platforms"
+dir "${env:ProgramFiles(x86)}\Android\android-sdk\build-tools"
+
+$pwd = Get-Location
+cd "${env:ProgramFiles(x86)}\Android\android-sdk\tools\bin"
 .\sdkmanager --list
-Echo 'y' | .\sdkmanager "platforms;android-28" "build-tools;28.0.2"
-Echo 'y' | .\sdkmanager --update
+Echo 'y' | .\sdkmanager "platforms;android-28" "build-tools;28.0.2" | Out-Null
+Echo 'y' | .\sdkmanager --update | Out-Null
+
 dir "${env:ProgramFiles(x86)}\Android\android-sdk\platforms"
+dir "${env:ProgramFiles(x86)}\Android\android-sdk\build-tools"
+
+cd $pwd
