d32eb9c9bce5fa882a9283f6f2fffff08bd86734
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-09-20 22:24:13 -0400

    copy classes to autofill

---
+++ src/iOS.Autofill/LockPinViewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UIKit;
+
+namespace Bit.iOS.Extension
+{
+    public partial class LockPinViewController : Core.Controllers.LockPinViewController
+    {
+        public LockPinViewController(IntPtr handle) : base(handle)
+        { }
+
+        public LoadingViewController LoadingController { get; set; }
+        public override UINavigationItem BaseNavItem => NavItem;
+        public override UIBarButtonItem BaseCancelButton => CancelButton;
+        public override UILabel BasePinLabel => PinLabel;
+        public override UILabel BaseInstructionLabel => InstructionLabel;
+        public override UITextField BasePinTextField => PinTextField;
+        public override Action Success => () => LoadingController.DismissLockAndContinue();
+
+        partial void CancelButton_Activated(UIBarButtonItem sender)
+        {
+            LoadingController.CompleteRequest(null);
+        }
+    }
+}
