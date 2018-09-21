e6131982528cd3b412f6133cf4bbb80d3e004963
kspearrin <kyle.spearrin@gmail.com>
2018-09-21 15:15:02 -0400

    updated

--- src/iOS.Autofill/SetupViewController.designer.cs
+++ src/iOS.Autofill/SetupViewController.designer.cs
@@ -1,6 +1,6 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
