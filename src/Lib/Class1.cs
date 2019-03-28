daee1a8e9d47704c8adabf6e5d1683e7a78d3833
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-27 21:34:39 -0400

    lib project

---
+++ src/Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bit.Lib
+{
+    public class Class1
+    {
+    }
+}
--- src/iOS/Properties/AssemblyInfo.cs
+++ src/iOS/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App.iOS")]
+[assembly: AssemblyTitle("BitwardeniOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("8bit Solutions LLC")]
+[assembly: AssemblyProduct("Bitwarden")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
