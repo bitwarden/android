61c1c6ebf8ea3584b497b9e1d4dd3e174e820b38
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-02 22:15:11 -0400

    build css from less

---
+++ src/App/gulpfile.js
@@ -0,0 +1,16 @@
+﻿const gulp = require('gulp');
+const gulpLess = require('gulp-less');
+const del = require('del');
+
+function less() {
+    return gulp.src(['./Less/android.less', './Less/ios.less', './Less/dark.less', './Less/styles.less'])
+        .pipe(gulpLess())
+        .pipe(gulp.dest('./Css'));
+}
+
+function cleanCss() {
+    return del('./Css');
+}
+
+exports.cleanCss = cleanCss;
+exports.less = gulp.series(cleanCss, less);
