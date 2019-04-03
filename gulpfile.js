b2c0b02be49c43734bcfde39374b938d2176c3cc
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-02 22:43:22 -0400

    move gulp tasks to sln root

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿const gulp = require('gulp');
+const gulpLess = require('gulp-less');
+const del = require('del');
+
+function less() {
+    return gulp.src([
+        './src/App/Less/android.less',
+        './src/App/Less/ios.less',
+        './src/App/Less/dark.less',
+        './src/App/Less/styles.less'
+    ]).pipe(gulpLess()).pipe(gulp.dest('./src/App/Css'));
+}
+
+function cleanCss() {
+    return del('./src/App/Css');
+}
+
+exports.cleanCss = cleanCss;
+exports.less = gulp.series(cleanCss, less);
