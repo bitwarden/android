929727ba418b33c28acfd9b9793d03df703011bb
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-09 10:35:21 -0400

    token service

---
+++ src/Core/Abstractions/ITokenService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Bit.Core.Abstractions
+{
+    public interface ITokenService
+    {
+        Task ClearTokenAsync();
+        Task ClearTwoFactorTokenAsync(string email);
+        JObject DecodeToken();
+        string GetEmail();
+        bool GetEmailVerified();
+        string GetIssuer();
+        string GetName();
+        bool GetPremium();
+        Task<string> GetRefreshTokenAsync();
+        Task<string> GetTokenAsync();
+        DateTime? GetTokenExpirationDate();
+        Task<string> GetTwoFactorTokenAsync(string email);
+        string GetUserId();
+        Task SetRefreshTokenAsync(string refreshToken);
+        Task SetTokenAsync(string token);
+        Task SetTokensAsync(string accessToken, string refreshToken);
+        Task SetTwoFactorTokenAsync(string token, string email);
+        bool TokenNeedsRefresh(int minutes = 5);
+        int TokenSecondsRemaining();
+    }
+}
---
+++ src/Core/Utilities/CoreHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bit.Core.Utilities
+{
+    public static class CoreHelpers
+    {
+        public static readonly DateTime Epoc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static bool InDebugMode()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
