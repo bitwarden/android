9fb2ce92971f55991201c5302a2da87d7856f5c2
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-11 15:50:51 -0400

    mobile messaging service

---
+++ src/App/Services/MobileMessagingService.cs
@@ -0,0 +1,12 @@
+﻿using Bit.Core.Abstractions;
+
+namespace Bit.App.Services
+{
+    public class MobileMessagingService : IMessagingService
+    {
+        public void Send(string subscriber, object arg = null)
+        {
+            Xamarin.Forms.MessagingCenter.Send(Xamarin.Forms.Application.Current, subscriber, arg);
+        }
+    }
+}
