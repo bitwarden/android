351c99fb4282375d3d2e4eaae0ea69670dc038af
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-16 11:08:28 -0400

    locale string comparer for i18n service

--- src/Core/Abstractions/II18nService.cs
+++ src/Core/Abstractions/II18nService.cs
@@ -1,10 +1,12 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace Bit.Core.Abstractions
 {
     public interface II18nService
     {
         CultureInfo Culture { get; set; }
+        StringComparer StringComparer { get; }
         string T(string id, string p1, string p2, string p3);
         string Translate(string id, string p1, string p2, string p3);
     }
