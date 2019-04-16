1103354de347e41605a971eb5a26fdbb2d64bb1e
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-16 07:44:18 -0400

    new constants for mobile app prefixes

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -2,7 +2,10 @@
 {
     public static class Constants
     {
+        public const string AndroidAppProtocol = "androidapp://";
+        public const string iOSAppProtocol = "iosapp://";
         public static string LockOptionKey = "lockOption";
         public static string PinProtectedKey = "pinProtectedKey";
+        public static string DefaultUriMatch = "defaultUriMatch";
     }
 }
