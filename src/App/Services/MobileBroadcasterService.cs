da73a2f5d223253ef3dcfa5782dbffec9de99cf7
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-18 12:53:05 -0400

    mobile broadcaster service

---
+++ src/App/Services/MobileBroadcasterService.cs
@@ -0,0 +1,29 @@
+﻿using Bit.Core.Abstractions;
+using System;
+using Xamarin.Forms;
+
+namespace Bit.App.Services
+{
+    public class MobileBroadcasterService : IBroadcasterService
+    {
+        public void Send<T>(T message, string id = null)
+        {
+            if(string.IsNullOrWhiteSpace(id))
+            {
+                throw new NotSupportedException("Cannot send a message to all subscribers.");
+            }
+            MessagingCenter.Send(Application.Current, id, message);
+        }
+
+        public void Subscribe<T>(string id, Action<T> messageCallback)
+        {
+            MessagingCenter.Subscribe<Application, T>(Application.Current, id,
+                (sender, message) => messageCallback(message));
+        }
+
+        public void Unsubscribe(string id)
+        {
+            MessagingCenter.Unsubscribe<Application, object>(Application.Current, id);
+        }
+    }
+}
---
+++ src/Core/Abstractions/IBroadcasterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bit.Core.Abstractions
+{
+    public interface IBroadcasterService
+    {
+        void Send<T>(T message, string id = null);
+        void Subscribe<T>(string id, Action<T> messageCallback);
+        void Unsubscribe(string id);
+    }
+}
