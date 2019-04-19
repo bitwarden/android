b3c67bab92773456b586c528f3804c3d0244fa79
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-04-19 09:25:15 -0400

    json property mapping

--- src/Core/Models/Response/IdentityTokenResponse.cs
+++ src/Core/Models/Response/IdentityTokenResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace Bit.Core.Models.Response
+﻿using Newtonsoft.Json;
+
+namespace Bit.Core.Models.Response
 {
     public class IdentityTokenResponse
     {
+        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
+        [JsonProperty("expires_in")]
         public string ExpiresIn { get; set; }
+        [JsonProperty("refresh_token")]
         public string RefreshToken { get; set; }
+        [JsonProperty("token_type")]
         public string TokenType { get; set; }
         public string PrivateKey { get; set; }
         public string Key { get; set; }
