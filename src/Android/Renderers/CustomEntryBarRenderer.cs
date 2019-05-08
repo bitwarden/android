5dc4357078ce0dd7e491791db159f25f3d548754
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-07 23:22:25 -0400

    padding/margins for entry/picker

---
+++ src/Android/Renderers/CustomEntryBarRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Bit.Droid.Renderers.BoxedView;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryBarRenderer))]
+namespace Bit.Droid.Renderers.BoxedView
+{
+    public class CustomEntryBarRenderer : EntryRenderer
+    {
+        public CustomEntryBarRenderer(Context context)
+            : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(Control != null && e.NewElement != null)
+            {
+                Control.SetPadding(Control.PaddingLeft, Control.PaddingTop - 10, Control.PaddingRight,
+                    Control.PaddingBottom + 20);
+            }
+        }
+    }
+}
---
+++ src/Android/Renderers/CustomPickerBarRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Bit.Droid.Renderers.BoxedView;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerBarRenderer))]
+namespace Bit.Droid.Renderers.BoxedView
+{
+    public class CustomPickerBarRenderer : PickerRenderer
+    {
+        public CustomPickerBarRenderer(Context context)
+            : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if(Control != null && e.NewElement != null)
+            {
+                Control.SetPadding(Control.PaddingLeft, Control.PaddingTop - 10, Control.PaddingRight,
+                    Control.PaddingBottom + 20);
+            }
+        }
+    }
+}
