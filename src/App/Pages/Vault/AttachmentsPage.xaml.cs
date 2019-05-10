34fd9b58423303addab6eb83c6b6b9ac163c28e7
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-10 14:33:33 -0400

    stub attachments page

---
+++ src/App/Pages/Vault/AttachmentsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+
+namespace Bit.App.Pages
+{
+    public partial class AttachmentsPage : BaseContentPage
+    {
+        private AttachmentsPageViewModel _vm;
+
+        public AttachmentsPage(string cipherId)
+        {
+            InitializeComponent();
+            _vm = BindingContext as AttachmentsPageViewModel;
+            _vm.Page = this;
+            _vm.CipherId = cipherId;
+            SetActivityIndicator();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadOnAppearedAsync(_scrollView, true, () => _vm.LoadAsync());
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+
+        private async void Save_Clicked(object sender, System.EventArgs e)
+        {
+            if(DoOnce())
+            {
+                await _vm.SubmitAsync();
+            }
+        }
+    }
+}
