29d876eaedf46065f7ab4ca08eb0f90fc3108bf7
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-13 15:24:38 -0400

    extended slider

--- src/Android/Resources/Resource.designer.cs
+++ src/Android/Resources/Resource.designer.cs
@@ -7993,26 +7993,26 @@ namespace Bit.Droid
 			// aapt resource value: 0x7f02005a
 			public const int avd_hide_password = 2130837594;
 			
-			// aapt resource value: 0x7f02014c
-			public const int avd_hide_password_1 = 2130837836;
-			
 			// aapt resource value: 0x7f02014d
-			public const int avd_hide_password_2 = 2130837837;
+			public const int avd_hide_password_1 = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int avd_hide_password_3 = 2130837838;
+			public const int avd_hide_password_2 = 2130837838;
+			
+			// aapt resource value: 0x7f02014f
+			public const int avd_hide_password_3 = 2130837839;
 			
 			// aapt resource value: 0x7f02005b
 			public const int avd_show_password = 2130837595;
 			
-			// aapt resource value: 0x7f02014f
-			public const int avd_show_password_1 = 2130837839;
-			
 			// aapt resource value: 0x7f020150
-			public const int avd_show_password_2 = 2130837840;
+			public const int avd_show_password_1 = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int avd_show_password_3 = 2130837841;
+			public const int avd_show_password_2 = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int avd_show_password_3 = 2130837842;
 			
 			// aapt resource value: 0x7f02005c
 			public const int card = 2130837596;
@@ -8698,11 +8698,11 @@ namespace Bit.Droid
 			// aapt resource value: 0x7f02013f
 			public const int notification_sm = 2130837823;
 			
-			// aapt resource value: 0x7f02014a
-			public const int notification_template_icon_bg = 2130837834;
-			
 			// aapt resource value: 0x7f02014b
-			public const int notification_template_icon_low_bg = 2130837835;
+			public const int notification_template_icon_bg = 2130837835;
+			
+			// aapt resource value: 0x7f02014c
+			public const int notification_template_icon_low_bg = 2130837836;
 			
 			// aapt resource value: 0x7f020140
 			public const int notification_tile_bg = 2130837824;
@@ -8723,16 +8723,19 @@ namespace Bit.Droid
 			public const int shield = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int splash_screen = 2130837830;
+			public const int slider_thumb = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int tooltip_frame_dark = 2130837831;
+			public const int splash_screen = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int tooltip_frame_light = 2130837832;
+			public const int tooltip_frame_dark = 2130837832;
 			
 			// aapt resource value: 0x7f020149
-			public const int yubikey = 2130837833;
+			public const int tooltip_frame_light = 2130837833;
+			
+			// aapt resource value: 0x7f02014a
+			public const int yubikey = 2130837834;
 			
 			static Drawable()
 			{
