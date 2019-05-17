d038fb900eb1fb5ab26ea939fe715f3a322a7f15
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-17 14:34:29 -0400

    fuzzy autofill prop

--- src/App/Pages/Vault/GroupingsPage/GroupingsPageListItem.cs
+++ src/App/Pages/Vault/GroupingsPage/GroupingsPageListItem.cs
@@ -14,6 +14,7 @@ namespace Bit.App.Pages
         public CipherView Cipher { get; set; }
         public CipherType? Type { get; set; }
         public string ItemCount { get; set; }
+        public bool FuzzyAutofill { get; set; }
 
         public string Name
         {
