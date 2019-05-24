6d49253ee536495f37e7a0722d986d0c46d7fe28
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-23 21:19:45 -0400

    stub out two factor page

---
+++ src/App/Pages/Accounts/TwoFactorPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Bit.App.Pages
+{
+    public partial class TwoFactorPage : BaseContentPage
+    {
+        private TwoFactorPageViewModel _vm;
+
+        public TwoFactorPage()
+        {
+            InitializeComponent();
+            _vm = BindingContext as TwoFactorPageViewModel;
+            _vm.Page = this;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _vm.InitAsync();
+        }
+
+        private void Continue_Clicked(object sender, EventArgs e)
+        {
+            if(DoOnce())
+            {
+
+            }
+        }
+
+        private void Methods_Clicked(object sender, EventArgs e)
+        {
+            if(DoOnce())
+            {
+
+            }
+        }
+    }
+}
---
+++ src/App/Pages/Accounts/TwoFactorPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Bit.App.Abstractions;
+using Bit.App.Resources;
+using Bit.Core.Abstractions;
+using Bit.Core.Exceptions;
+using Bit.Core.Utilities;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bit.App.Pages
+{
+    public class TwoFactorPageViewModel : BaseViewModel
+    {
+        private readonly IDeviceActionService _deviceActionService;
+        private readonly IAuthService _authService;
+        private readonly ISyncService _syncService;
+        private readonly IStorageService _storageService;
+
+        private string _email;
+
+        public TwoFactorPageViewModel()
+        {
+            _deviceActionService = ServiceContainer.Resolve<IDeviceActionService>("deviceActionService");
+            _authService = ServiceContainer.Resolve<IAuthService>("authService");
+            _syncService = ServiceContainer.Resolve<ISyncService>("syncService");
+            _storageService = ServiceContainer.Resolve<IStorageService>("storageService");
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
+        public async Task InitAsync()
+        {
+
+        }
+
+        public async Task SubmitAsync()
+        {
+
+        }
+    }
+}
