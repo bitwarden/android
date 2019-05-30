c7b62c855170dac54fcf9401dbf78e3933d4475b
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-30 14:43:35 -0400

    move some items needed for migration

---
+++ src/App/Migration/SettingsShim.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Bit.App.Migration
+{
+    public class SettingsShim
+    {
+        public bool Contains(string key)
+        {
+            return Xamarin.Essentials.Preferences.ContainsKey(key);
+        }
+
+        public string GetValueOrDefault(string key, string defaultValue)
+        {
+            return Xamarin.Essentials.Preferences.Get(key, defaultValue);
+        }
+
+        public DateTime GetValueOrDefault(string key, DateTime defaultValue)
+        {
+            return Xamarin.Essentials.Preferences.Get(key, defaultValue);
+        }
+
+        public bool GetValueOrDefault(string key, bool defaultValue)
+        {
+            return Xamarin.Essentials.Preferences.Get(key, defaultValue);
+        }
+
+        public int GetValueOrDefault(string key, int defaultValue)
+        {
+            return Xamarin.Essentials.Preferences.Get(key, defaultValue);
+        }
+
+        public long GetValueOrDefault(string key, long defaultValue)
+        {
+            return Xamarin.Essentials.Preferences.Get(key, defaultValue);
+        }
+
+        public void AddOrUpdateValue(string key, string value)
+        {
+            Xamarin.Essentials.Preferences.Set(key, value);
+        }
+
+        public void AddOrUpdateValue(string key, DateTime value)
+        {
+            Xamarin.Essentials.Preferences.Set(key, value);
+        }
+
+        public void AddOrUpdateValue(string key, bool value)
+        {
+            Xamarin.Essentials.Preferences.Set(key, value);
+        }
+
+        public void AddOrUpdateValue(string key, long value)
+        {
+            Xamarin.Essentials.Preferences.Set(key, value);
+        }
+
+        public void AddOrUpdateValue(string key, int value)
+        {
+            Xamarin.Essentials.Preferences.Set(key, value);
+        }
+
+        public void Remove(string key)
+        {
+            Xamarin.Essentials.Preferences.Remove(key);
+        }
+    }
+}
