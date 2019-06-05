dbc750b44ec180819f481ab575254c535bbcf647
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-06-04 23:16:57 -0400

    request focus on email field for hint page

--- src/App/Pages/Accounts/HintPage.xaml.cs
+++ src/App/Pages/Accounts/HintPage.xaml.cs
@@ -13,6 +13,12 @@ namespace Bit.App.Pages
             _vm.Page = this;
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            RequestFocus(_email);
+        }
+
         private async void Submit_Clicked(object sender, EventArgs e)
         {
             if(DoOnce())
