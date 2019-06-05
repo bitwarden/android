6c88d25941190771444310262e132d71b4bb57de
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-06-04 21:28:50 -0400

    add locking to BroadcasterService

--- src/Core/Services/BroadcasterService.cs
+++ src/Core/Services/BroadcasterService.cs
@@ -9,37 +9,47 @@ namespace Bit.App.Services
     public class BroadcasterService : IBroadcasterService
     {
         private readonly Dictionary<string, Action<Message>> _subscribers = new Dictionary<string, Action<Message>>();
+        private object _myLock = new object();
 
         public void Send(Message message, string id = null)
         {
-            if(!string.IsNullOrWhiteSpace(id))
+            lock(_myLock)
             {
-                if(_subscribers.ContainsKey(id))
+                if(!string.IsNullOrWhiteSpace(id))
                 {
-                    Task.Run(() => _subscribers[id].Invoke(message));
+                    if(_subscribers.ContainsKey(id))
+                    {
+                        Task.Run(() => _subscribers[id].Invoke(message));
+                    }
+                    return;
                 }
-                return;
-            }
-            foreach(var sub in _subscribers)
-            {
-                Task.Run(() => sub.Value.Invoke(message));
+                foreach(var sub in _subscribers)
+                {
+                    Task.Run(() => sub.Value.Invoke(message));
+                }
             }
         }
 
         public void Subscribe(string id, Action<Message> messageCallback)
         {
-            if(_subscribers.ContainsKey(id))
+            lock(_myLock)
             {
-                return;
+                if(_subscribers.ContainsKey(id))
+                {
+                    return;
+                }
+                _subscribers.Add(id, messageCallback);
             }
-            _subscribers.Add(id, messageCallback);
         }
 
         public void Unsubscribe(string id)
         {
-            if(_subscribers.ContainsKey(id))
+            lock(_myLock)
             {
-                _subscribers.Remove(id);
+                if(_subscribers.ContainsKey(id))
+                {
+                    _subscribers.Remove(id);
+                }
             }
         }
     }
