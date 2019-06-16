0eb385e49f9cdfbc5ef8bb2e03d7351eee80d522
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-06-15 21:02:38 -0400

    revert com.android.settings from blacklist

--- src/Android/Autofill/AutofillHelpers.cs
+++ src/Android/Autofill/AutofillHelpers.cs
@@ -66,7 +66,6 @@ namespace Bit.Droid.Autofill
             "androidapp://android",
             "androidapp://com.x8bit.bitwarden",
             "androidapp://com.oneplus.applocker",
-            "androidapp://com.android.settings",
         };
 
         public static async Task<List<FilledItem>> GetFillItemsAsync(Parser parser, ICipherService cipherService)
