88b8a192b58815491171b11f2e51ffe39a2ff4c9
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-06-24 16:16:17 -0400

    no listview selection type on non-light theme

---
+++ src/iOS/Renderers/CustomViewCellRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Bit.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(CustomViewCellRenderer))]
+namespace Bit.iOS.Renderers
+{
+    public class CustomViewCellRenderer : ViewCellRenderer
+    {
+        private bool _noSelectionStyle = false;
+
+        public CustomViewCellRenderer()
+        {
+            _noSelectionStyle = (Color)Xamarin.Forms.Application.Current.Resources["BackgroundColor"] != Color.White;
+        }
+
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            if(_noSelectionStyle)
+            {
+                cell.SelectionStyle = UITableViewCellSelectionStyle.None;
+            }
+            return cell;
+        }
+    }
+}
