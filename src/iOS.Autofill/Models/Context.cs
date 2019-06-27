88aea96034dbb2993ebb724729706e9b663d227f
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-06-26 20:52:17 -0400

    add autofill resources

---
+++ src/iOS.Autofill/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿/*
+using AuthenticationServices;
+using Bit.iOS.Core.Models;
+using Foundation;
+
+namespace Bit.iOS.Autofill.Models
+{
+    public class Context : AppExtensionContext
+    {
+        public NSExtensionContext ExtContext { get; set; }
+        public ASCredentialServiceIdentifier[] ServiceIdentifiers { get; set; }
+        public ASPasswordCredentialIdentity CredentialIdentity { get; set; }
+        public bool Configuring { get; set; }
+    }
+}
+*/
