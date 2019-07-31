90c7ec83b19f34c56d01149687ee5c00c27ed020
Tim <tipfom@hotmail.de>
2019-07-31 23:08:27 +0200

    added ISteamGuardService interface

---
+++ src/App/Abstractions/ISteamGuardService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bit.App.Abstractions
+{
+    public interface ISteamGuardService
+    { 
+        string TOTPSecret { get; }
+        string RecoveryCode { get; }
+        void Init(string username, string password);
+        bool CheckEmailCode(string code);
+        bool CheckSMSCode(string code);
+    }
+}
