80a44e3008e6500931872d96982700704815f020
Tim <tipfom@hotmail.de>
2019-07-31 23:14:43 +0200

    added model for a SteamSession

---
+++ src/App/Models/Steam/SteamSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bit.App.Models.Steam
+{
+    class SteamSession
+    {
+        public string SessionID { get; set; }
+        public string SteamLogin { get; set; }
+        public string SteamLoginSecure { get; set; }
+        public string WebCookie { get; set; }
+        public string OAuthToken { get; set; }
+        public ulong SteamID { get; set; }
+    }
+}
