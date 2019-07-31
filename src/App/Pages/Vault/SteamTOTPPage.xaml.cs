399026bb6ac59c2c906fd733210df738dd3b910f
Tim <tipfom@hotmail.de>
2019-07-31 16:48:19 +0200

    new ui to get Steam Secret

---
+++ src/App/Pages/Vault/SteamTOTPPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bit.App.Pages.Vault
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SteamTOTPPage : ContentPage
+    {
+        public SteamTOTPPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
