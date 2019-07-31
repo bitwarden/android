efa26e4284057582d8d4a3ec74a52cb61162b374
Tim <tipfom@hotmail.de>
2019-07-31 23:28:45 +0200

    added model for the SteamGuardData attained

---
+++ src/App/Models/Steam/SteamGuardData.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using Bit.App.Utilities;
+using Newtonsoft.Json;
+
+namespace Bit.App.Models.Steam
+{
+    class SteamGuardData
+    {
+        [JsonProperty("shared_secret")]
+        public string SharedSecret { get; set; }
+
+        [JsonProperty("serial_number")]
+        public string SerialNumber { get; set; }
+
+        [JsonProperty("revocation_code")]
+        public string RevocationCode { get; set; }
+
+        [JsonProperty("uri")]
+        public string URI { get; set; }
+
+        [JsonProperty("server_time")]
+        public long ServerTime { get; set; }
+
+        [JsonProperty("account_name")]
+        public string AccountName { get; set; }
+
+        [JsonProperty("token_gid")]
+        public string TokenGID { get; set; }
+
+        [JsonProperty("identity_secret")]
+        public string IdentitySecret { get; set; }
+
+        [JsonProperty("secret_1")]
+        public string Secret1 { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("device_id")]
+        public string DeviceID { get; set; }
+
+        private static byte[] steamGuardCodeTranslations = new byte[] { 50, 51, 52, 53, 54, 55, 56, 57, 66, 67, 68, 70, 71, 72, 74, 75, 77, 78, 80, 81, 82, 84, 86, 87, 88, 89 };
+
+        public string GenerateSteamGuardCode()
+        {
+            return GenerateSteamGuardCodeForTime(SteamTimeSyncHelper.GetSteamUnixTime());
+        }
+
+        public string GenerateSteamGuardCodeForTime(long time)
+        {
+            if (this.SharedSecret == null || this.SharedSecret.Length == 0)
+            {
+                return "";
+            }
+
+            string sharedSecretUnescaped = Regex.Unescape(this.SharedSecret);
+            byte[] sharedSecretArray = Convert.FromBase64String(sharedSecretUnescaped);
+            byte[] timeArray = new byte[8];
+
+            time /= 30L;
+
+            for (int i = 8; i > 0; i--)
+            {
+                timeArray[i - 1] = (byte)time;
+                time >>= 8;
+            }
+
+            HMACSHA1 hmacGenerator = new HMACSHA1();
+            hmacGenerator.Key = sharedSecretArray;
+            byte[] hashedData = hmacGenerator.ComputeHash(timeArray);
+            byte[] codeArray = new byte[5];
+            try
+            {
+                byte b = (byte)(hashedData[19] & 0xF);
+                int codePoint = (hashedData[b] & 0x7F) << 24 | (hashedData[b + 1] & 0xFF) << 16 | (hashedData[b + 2] & 0xFF) << 8 | (hashedData[b + 3] & 0xFF);
+
+                for (int i = 0; i < 5; ++i)
+                {
+                    codeArray[i] = steamGuardCodeTranslations[codePoint % steamGuardCodeTranslations.Length];
+                    codePoint /= steamGuardCodeTranslations.Length;
+                }
+            }
+            catch (Exception)
+            {
+                return null; //Change later, catch-alls are bad!
+            }
+            return Encoding.UTF8.GetString(codeArray);
+        }
+    }
+}
