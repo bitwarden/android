ce07e416479a8cd0fa2aa38f291cba50c6a43fb4
Tim <tipfom@hotmail.de>
2019-07-31 23:22:42 +0200

    added Steam Endpoint collection

---
+++ src/App/Utilities/SteamAPIEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bit.App.Utilities
+{
+    static class SteamAPIEndpoints
+    {
+        public const string STEAMAPI_BASE = "https://api.steampowered.com";
+        public const string COMMUNITY_BASE = "https://steamcommunity.com";
+        public const string MOBILEAUTH_BASE = STEAMAPI_BASE + "/IMobileAuthService/%s/v0001";
+        public static string MOBILEAUTH_GETWGTOKEN = MOBILEAUTH_BASE.Replace("%s", "GetWGToken");
+        public const string TWO_FACTOR_BASE = STEAMAPI_BASE + "/ITwoFactorService/%s/v0001";
+        public static string TWO_FACTOR_TIME_QUERY = TWO_FACTOR_BASE.Replace("%s", "QueryTime");
+    }
+}
