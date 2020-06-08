dd7a52ba086561b15ac4a7d815fbf181f8e1e81c
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-08 16:10:17 -0400

    print version code

--- .github/scripts/android/increment-version.ps1
+++ .github/scripts/android/increment-version.ps1
@@ -1,6 +1,10 @@
 ﻿$rootPath = $env:GITHUB_WORKSPACE;
 $buildNumber = 3000 + [int]$env:GITHUB_RUN_NUMBER;
 
+Write-Output "########################################"
+Write-Output "##### Setting Version Code $buildNumber"
+Write-Output "########################################"
+
 $androidManifest = $($rootPath + "\src\Android\Properties\AndroidManifest.xml");
 
 $xml=New-Object XML;
