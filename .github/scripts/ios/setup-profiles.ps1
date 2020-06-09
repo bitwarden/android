98757c3f115bfcf127059965c56591a8e5529804
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-09 12:27:40 -0400

    use proper params for copy-item

--- .github/scripts/ios/setup-profiles.ps1
+++ .github/scripts/ios/setup-profiles.ps1
@@ -6,10 +6,13 @@ $bitwardenProfilePath = $($secretsPath + "/dist_bitwarden.mobileprovision");
 $extensionProfilePath = $($secretsPath + "/dist_extension.mobileprovision");
 
 $autofill_uuid = grep UUID -A1 -a $autofillProfilePath | grep -io "[-A-F0-9]\{36\}"
-Copy-Item $autofillProfilePath ~/Library/MobileDevice/Provisioning\ Profiles/$autofill_uuid.mobileprovision
+Copy-Item $autofillProfilePath `
+    -destination ~/Library/MobileDevice/Provisioning\ Profiles/$autofill_uuid.mobileprovision
 
 $bitwarden_uuid = grep UUID -A1 -a $bitwardenProfilePath | grep -io "[-A-F0-9]\{36\}"
-Copy-Item $bitwardenProfilePath ~/Library/MobileDevice/Provisioning\ Profiles/$bitwarden_uuid.mobileprovision
+Copy-Item $bitwardenProfilePath `
+    -destination ~/Library/MobileDevice/Provisioning\ Profiles/$bitwarden_uuid.mobileprovision
 
 $extension_uuid = grep UUID -A1 -a $extensionProfilePath | grep -io "[-A-F0-9]\{36\}"
-Copy-Item $extensionProfilePath ~/Library/MobileDevice/Provisioning\ Profiles/$extension_uuid.mobileprovision
+Copy-Item $extensionProfilePath `
+    -destination ~/Library/MobileDevice/Provisioning\ Profiles/$extension_uuid.mobileprovision
