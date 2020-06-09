707a6ecbaa18a324b1340d3b559f816940337460
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-09 11:37:40 -0400

    setup ios keychain

---
+++ .github/scripts/ios/setup-keychain.ps1
@@ -0,0 +1,11 @@
+$homePath = $env:HOME;
+$secretsPath = $homePath + "\secrets"
+
+$mobileKeyPath = $($secretsPath + "\bitwarden-mobile-key.p12");
+$distCertPath = $($secretsPath + "\iphone-distribution-cert.p12");
+
+security create-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
+security import $mobileKeyPath -k build.keychain -P $env:MOBILE_KEY_PASSWORD
+security import $distCertPath -k build.keychain -P $env:DIST_CERT_PASSWORD
