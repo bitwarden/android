aa2bc40f0319c6e421b2b3b913a629c7b5ce4e43
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-09 11:19:02 -0400

    decrypt ios secrets

--- .github/scripts/ios/decrypt-secrets.ps1
+++ .github/scripts/ios/decrypt-secrets.ps1
@@ -2,3 +2,5 @@
 
 $decryptSecretPath = $($rootPath + ".\.github\scripts\decrypt-secret.ps1");
 
+Invoke-Expression "& `"$decryptSecretPath`" -filename bitwarden-mobile-key.p12.gpg"
+Invoke-Expression "& `"$decryptSecretPath`" -filename iphone-distribution-cert.p12.gpg"
