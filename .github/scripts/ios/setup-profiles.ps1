766b4f797184f2d16e659b9050bff2fdea9ada08
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-09 12:35:39 -0400

    create profiles dir path if it doesnt exist

--- .github/scripts/ios/setup-profiles.ps1
+++ .github/scripts/ios/setup-profiles.ps1
@@ -4,15 +4,18 @@ $secretsPath = $homePath + "/secrets"
 $autofillProfilePath = $($secretsPath + "/dist_autofill.mobileprovision");
 $bitwardenProfilePath = $($secretsPath + "/dist_bitwarden.mobileprovision");
 $extensionProfilePath = $($secretsPath + "/dist_extension.mobileprovision");
+$profilesDirPath = "~/Library/MobileDevice/Provisioning Profiles"
+
+if (!(Test-Path -Path $profilesDirPath))  
+{
+    New-Item -ItemType Directory -Path $profilesDirPath 
+}
 
 $autofill_uuid = grep UUID -A1 -a $autofillProfilePath | grep -io "[-A-F0-9]\{36\}"
-Copy-Item $autofillProfilePath `
-    -destination "~/Library/MobileDevice/Provisioning Profiles/$autofill_uuid.mobileprovision"
+Copy-Item $autofillProfilePath -destination "$profilesDirPath/$autofill_uuid.mobileprovision"
 
 $bitwarden_uuid = grep UUID -A1 -a $bitwardenProfilePath | grep -io "[-A-F0-9]\{36\}"
-Copy-Item $bitwardenProfilePath `
-    -destination "~/Library/MobileDevice/Provisioning Profiles/$bitwarden_uuid.mobileprovision"
+Copy-Item $bitwardenProfilePath -destination "$profilesDirPath/$bitwarden_uuid.mobileprovision"
 
 $extension_uuid = grep UUID -A1 -a $extensionProfilePath | grep -io "[-A-F0-9]\{36\}"
-Copy-Item $extensionProfilePath `
-    -destination "~/Library/MobileDevice/Provisioning Profiles/$extension_uuid.mobileprovision"
+Copy-Item $extensionProfilePath -destination "$profilesDirPath/$extension_uuid.mobileprovision"
