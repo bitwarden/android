8e6c6e04a32ce23e9e45c2335005b937a237ac3e
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-10 10:53:57 -0400

    cleanup export, ipa artifact

--- .github/scripts/ios/export-ipa.ps1
+++ .github/scripts/ios/export-ipa.ps1
@@ -4,21 +4,10 @@
 )
 
 $rootPath = $env:GITHUB_WORKSPACE;
-$resourcesPath = "$rootPath/.github/resources";
-$exportOptionsPath = "$resourcesPath/export-options-$method.plist";
 $homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path
-$archivesPath = "$homePath/Library/Developer/Xcode/Archives";
 
-cd $archivesPath
-cd *
-ls
+$exportOptionsPath = "$rootPath/.github/resources/export-options-$method.plist";
+$archivePath = "$homePath/Library/Developer/Xcode/Archives/*/*.xcarchive";
+$exportPath = "$rootPath/com.8bit.bitwarden.ipa"
 
-xcodebuild -exportArchive -archivePath *.xcarchive -exportPath com.8bit.bitwarden.ipa -exportOptionsPlist $exportOptionsPath
-
-ls
-
-$destPath = "$rootPath/com.8bit.bitwarden.ipa"
-Copy-Item com.8bit.bitwarden.ipa $destPath
-
-cd $rootPath
-ls
+xcodebuild -exportArchive -archivePath $archivePath -exportPath $exportPath -exportOptionsPlist $exportOptionsPath
