caed8c2cf02ca25c94df2f5d9884aedf6236f0ba
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-10 13:13:03 -0400

    explore paths

--- .github/scripts/ios/export-ipa.ps1
+++ .github/scripts/ios/export-ipa.ps1
@@ -9,11 +9,10 @@ $homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path
 $exportOptionsPath = "$rootPath/.github/resources/export-options-$method.plist";
 $archiveBasePath = "$homePath/Library/Developer/Xcode/Archives/*";
 $archivePath = "$archiveBasePath/*.xcarchive";
-$exportPath = "$archiveBasePath/ipa-export";
-$ipaPath = "$exportPath/Bitwarden.ipa";
-$destIpaPath = "$rootPath/Bitwarden.ipa";
+$exportPath = "$rootPath/bitwardenios";
 
 xcodebuild -exportArchive -archivePath $archivePath -exportPath $exportPath -exportOptionsPlist $exportOptionsPath
 
-Copy-Item $ipaPath $destIpaPath
+ls
+cd $exportPath
 ls
