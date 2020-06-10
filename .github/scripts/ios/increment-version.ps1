363f5be8fff4bd68c3e45bb3ea6d0838486f9d94
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-09 21:23:10 -0400

    increment version for ios

---
+++ .github/scripts/ios/increment-version.ps1
@@ -0,0 +1,26 @@
+﻿$rootPath = $env:GITHUB_WORKSPACE;
+$buildNumber = 100 + [int]$env:GITHUB_RUN_NUMBER;
+
+$bitwardenInfo = $($rootPath + "/src/iOS/Info.plist");
+$extensionInfo = $($rootPath + "/src/iOS.Extension/Info.plist");
+$autofillInfo = $($rootPath + "/src/iOS.Autofill/Info.plist");
+
+Write-Output "########################################"
+Write-Output "##### Setting CFBundleVersion $buildNumber"
+Write-Output "########################################"
+
+function Update-Version($file) {
+    $xml=New-Object XML;
+    $xml.Load($file);
+
+    Select-Xml -xml $xml -XPath "//dict/key[. = 'CFBundleVersion']/following-sibling::string[1]" |
+    %{ 
+        $_.Node.InnerXml = $buildNumber
+    }
+
+    $xml.Save($file);
+}
+
+Update-Version $bitwardenInfo
+Update-Version $extensionInfo
+Update-Version $autofillInfo
--- .github/scripts/ios/setup-keychain.ps1
+++ .github/scripts/ios/setup-keychain.ps1
@@ -11,4 +11,3 @@ security set-keychain-settings -lut 1200 build.keychain
 security import $mobileKeyPath -k build.keychain -P $env:MOBILE_KEY_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security import $distCertPath -k build.keychain -P $env:DIST_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $env:KEYCHAIN_PASSWORD build.keychain
-security find-identity -v -p codesigning
