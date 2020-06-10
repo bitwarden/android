df15fa2f0e8a6c9964edbd0c5f4ee60c8b2f1152
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-09 20:47:46 -0400

    move stuff to setup keychain script

--- .github/scripts/ios/setup-keychain.ps1
+++ .github/scripts/ios/setup-keychain.ps1
@@ -7,5 +7,8 @@ $distCertPath = $($secretsPath + "/iphone-distribution-cert.p12");
 security create-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p $env:KEYCHAIN_PASSWORD build.keychain
+security set-keychain-settings -lut 1200 build.keychain
 security import $mobileKeyPath -k build.keychain -P $env:MOBILE_KEY_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
 security import $distCertPath -k build.keychain -P $env:DIST_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $env:KEYCHAIN_PASSWORD build.keychain
+security find-identity -v -p codesigning
