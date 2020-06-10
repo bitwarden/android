10665981502943896439f34cf5021847dc472f88
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-10 13:30:08 -0400

    fix ipa path

--- .github/scripts/ios/deploy-app-store.ps1
+++ .github/scripts/ios/deploy-app-store.ps1
@@ -1,5 +1,5 @@
 $rootPath = $env:GITHUB_WORKSPACE;
-$ipaPath = "$rootPath/Bitwarden.ipa"
+$ipaPath = "$rootPath/bitwarden-export/Bitwarden.ipa"
 
 xcrun altool --upload-app --type ios --file "$ipaPath" `
     --username "$env:APPLE_ID_USERNAME" --password "$env:APPLE_ID_PASSWORD"
--- .github/scripts/ios/export-ipa.ps1
+++ .github/scripts/ios/export-ipa.ps1
@@ -7,12 +7,7 @@ $rootPath = $env:GITHUB_WORKSPACE;
 $homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path
 
 $exportOptionsPath = "$rootPath/.github/resources/export-options-$method.plist";
-$archiveBasePath = "$homePath/Library/Developer/Xcode/Archives/*";
-$archivePath = "$archiveBasePath/*.xcarchive";
-$exportPath = "$rootPath/bitwardenios";
+$archivePath = "$homePath/Library/Developer/Xcode/Archives/*/*.xcarchive";
+$exportPath = "$rootPath/bitwarden-export";
 
 xcodebuild -exportArchive -archivePath $archivePath -exportPath $exportPath -exportOptionsPlist $exportOptionsPath
-
-ls
-cd $exportPath
-ls
