81a30e580e068502d4a28b2ce8bdc880206d176c
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-10 10:02:15 -0400

    export ipa for app store

---
+++ .github/scripts/ios/export-ipa.ps1
@@ -0,0 +1,24 @@
+﻿param (
+  [Parameter(Mandatory=$true)]
+  [string] $method
+)
+
+$rootPath = $env:GITHUB_WORKSPACE;
+$resourcesPath = "$rootPath/.github/resources";
+$exportOptionsPath = "$resourcesPath/export-options-$method.plist";
+$homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path
+$archivesPath = "$homePath/Library/Developer/Xcode/Archives";
+
+cd $archivesPath
+cd *
+ls
+
+xcodebuild -exportArchive -archivePath *.xcarchive -exportPath com.8bit.bitwarden.ipa -exportOptionsPlist $exportOptionsPath
+
+ls
+
+$destPath = "$rootPath/com.8bit.bitwarden.ipa"
+Copy-Item com.8bit.bitwarden.ipa $destPath
+
+cd $rootPath
+ls
