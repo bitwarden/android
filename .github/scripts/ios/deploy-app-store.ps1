8a94623b2b8306da6437150e8fae435b4312c03c
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-10 11:48:02 -0400

    type ios

--- .github/scripts/ios/deploy-app-store.ps1
+++ .github/scripts/ios/deploy-app-store.ps1
@@ -1,4 +1,5 @@
 $rootPath = $env:GITHUB_WORKSPACE;
 $ipaPath = "$rootPath/com.8bit.bitwarden.ipa"
 
-xcrun altool --upload-app --file "$ipaPath" --username "$env:APPLE_ID_USERNAME" --password "$env:APPLE_ID_PASSWORD"
+xcrun altool --upload-app --type ios --file "$ipaPath" `
+    --username "$env:APPLE_ID_USERNAME" --password "$env:APPLE_ID_PASSWORD"
