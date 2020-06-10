6fd24c842fec1d8d5acaa4c60a08e047b3dadec4
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-10 12:24:52 -0400

    get proper ipa for app store

--- .github/scripts/ios/deploy-app-store.ps1
+++ .github/scripts/ios/deploy-app-store.ps1
@@ -1,5 +1,5 @@
 $rootPath = $env:GITHUB_WORKSPACE;
-$ipaPath = "$rootPath/com.8bit.bitwarden.ipa"
+$ipaPath = "$rootPath/Bitwarden.ipa"
 
 xcrun altool --upload-app --type ios --file "$ipaPath" `
     --username "$env:APPLE_ID_USERNAME" --password "$env:APPLE_ID_PASSWORD"
--- .github/scripts/ios/export-ipa.ps1
+++ .github/scripts/ios/export-ipa.ps1
@@ -7,7 +7,12 @@ $rootPath = $env:GITHUB_WORKSPACE;
 $homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path
 
 $exportOptionsPath = "$rootPath/.github/resources/export-options-$method.plist";
-$archivePath = "$homePath/Library/Developer/Xcode/Archives/*/*.xcarchive";
-$exportPath = "$rootPath/com.8bit.bitwarden.ipa"
+$archiveBasePath = "$homePath/Library/Developer/Xcode/Archives/*";
+$archivePath = "$archiveBasePath/*.xcarchive";
+$exportPath = "$archiveBasePath/ipa-export";
+$ipaPath = "$exportPath/Bitwarden.ipa";
+$destIpaPath = "$rootPath/Bitwarden.ipa";
 
 xcodebuild -exportArchive -archivePath $archivePath -exportPath $exportPath -exportOptionsPlist $exportOptionsPath
+
+Copy-Item $ipaPath $destIpaPath
