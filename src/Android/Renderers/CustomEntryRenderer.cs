6df0d3dff7c385be0f4bdc2eeabdcc4a307ac83f
Matt Portune <mportune@bitwarden.com>
2020-06-15 15:43:08 -0400

    Workaround for disabling predictive text in visible password fields

--- src/Android/Renderers/CustomEntryRenderer.cs
+++ src/Android/Renderers/CustomEntryRenderer.cs
@@ -1,5 +1,9 @@
-﻿using Android.Content;
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Graphics;
+using Android.Text;
 using Android.Views.InputMethods;
+using Android.Widget;
 using Bit.Droid.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
@@ -22,6 +26,43 @@ namespace Bit.Droid.Renderers
                     Control.PaddingBottom + 20);
                 Control.ImeOptions = Control.ImeOptions | (ImeAction)ImeFlags.NoPersonalizedLearning |
                     (ImeAction)ImeFlags.NoExtractUi;
+            }
+        }
+
+        // Workaround for failure to disable text prediction on non-password fields
+        // see https://github.com/xamarin/Xamarin.Forms/issues/10857
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            
+            // Check if changed property is "IsPassword", otherwise ignore
+            if (e.PropertyName == Entry.IsPasswordProperty.PropertyName)
+            {
+                // Check if field type is text, otherwise ignore (numeric passwords, etc.)
+                EditText.InputType = Element.Keyboard.ToInputType();
+                if ((EditText.InputType & InputTypes.ClassText) == InputTypes.ClassText)
+                {
+                    if (Element.IsPassword)
+                    {
+                        // Element is a password field, set inputType to TextVariationPassword which disables
+                        // predictive text by default
+                        EditText.InputType = EditText.InputType | InputTypes.TextVariationPassword;
+                    }
+                    else
+                    {
+                        // Element is not a password field, set inputType to TextVariationVisiblePassword to
+                        // disable predictive text while still displaying the content.
+                        EditText.InputType = EditText.InputType | InputTypes.TextVariationVisiblePassword;
+                    }
+                    
+                    // The workaround above forces a reset of the style properties, so we need to re-apply the font.
+                    // see https://xamarin.github.io/bugzilla-archives/33/33666/bug.html
+                    var typeface = Typeface.CreateFromAsset(Context.Assets, "RobotoMono_Regular.ttf");
+                    if (Control is TextView label)
+                    {
+                        label.Typeface = typeface;
+                    }
+                }
             }
         }
     }
