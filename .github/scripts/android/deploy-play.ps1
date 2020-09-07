93cd31018e08c594163ea8a43de18220d957ce74
Matt Portune <59324545+mportune-bw@users.noreply.github.com>
2020-09-06 20:18:44 -0400

    publish to google play internal test track (#1061)
    publish directly to internal test track to skip play store approval process for internal testers (artifact can then be manually promoted to alpha > beta > prod like before).  This should bring the testing experience up to speed with TestFlight for iOS (i.e. push code > ci build > immediate availability)

--- .github/scripts/android/deploy-play.ps1
+++ .github/scripts/android/deploy-play.ps1
@@ -4,6 +4,6 @@ $homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path;
 $publisherPath = $($rootPath + "/store/google/Publisher/bin/Release/netcoreapp2.0/Publisher.dll");
 $credsPath = $($homePath + "/secrets/play_creds.json");
 $aabPath = $($rootPath + "/com.x8bit.bitwarden.aab");
-$track = "alpha";
+$track = "internal";
 
 dotnet $publisherPath $credsPath $aabPath $track
