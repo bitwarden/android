b2acfd295fac851e52bdab0b0a86e6adf4feb194
Contribucious <4764956+contribucious@users.noreply.github.com>
2020-10-15 17:53:57 +0200

    Update support for Ungoogled Chromium browser

--- src/Android/Accessibility/AccessibilityHelpers.cs
+++ src/Android/Accessibility/AccessibilityHelpers.cs
@@ -90,7 +90,9 @@ namespace Bit.Droid.Accessibility
             new Browser("org.mozilla.rocket", "display_url"),
             new Browser("org.torproject.torbrowser", "url_bar_title,mozac_browser_toolbar_url_view"), // 2nd = Anticipation
             new Browser("org.torproject.torbrowser_alpha", "url_bar_title,mozac_browser_toolbar_url_view"), // 2nd = Anticipation
-            new Browser("org.ungoogled.chromium", "url_bar"),
+            new Browser("org.ungoogled.chromium", "url_bar"), // [DEPRECATED]
+            new Browser("org.ungoogled.chromium.extensions.stable", "url_bar"),
+            new Browser("org.ungoogled.chromium.stable", "url_bar"),
 
             // [Section B] Entries only present here
             //
