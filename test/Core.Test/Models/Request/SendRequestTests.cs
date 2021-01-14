07a22587a098e981c283c63e23505857ed2f2dfd
Matt Gibson <mgibson@bitwarden.com>
2021-01-14 16:41:04 -0600

    Test Request generation from Send

---
+++ test/Core.Test/Models/Request/SendRequestTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Bit.Core.Enums;
+using Bit.Core.Models.Domain;
+using Bit.Core.Models.Request;
+using Bit.Core.Test.AutoFixture;
+using Bit.Core.Utilities;
+using Bit.Test.Common;
+using Bit.Test.Common.AutoFixture.Attributes;
+using Xunit;
+
+namespace Bit.Core.Test.Models.Request
+{
+    public class SendRequestTests
+    {
+        [Theory]
+        [InlineCustomAutoData(new[] { typeof(TextSendCustomization) })]
+        [InlineCustomAutoData(new[] { typeof(FileSendCustomization) })]
+        public void SendRequest_FromSend_Success(Send send)
+        {
+            var request = new SendRequest(send);
+
+            TestHelper.AssertPropertyEqual(send, request, "Id", "AccessId", "UserId", "Name", "Notes", "File", "Text", "Key", "AccessCount", "RevisionDate");
+            Assert.Equal(send.Name?.EncryptedString, request.Name);
+            Assert.Equal(send.Notes?.EncryptedString, request.Notes);
+
+            switch (send.Type)
+            {
+                case SendType.File:
+                    // Only sets filename
+                    Assert.Equal(send.File.FileName?.EncryptedString, request.File.FileName);
+                    break;
+                case SendType.Text:
+                    TestHelper.AssertPropertyEqual(send.Text, request?.Text, "Text");
+                    Assert.Equal(send.Text.Text?.EncryptedString, request.Text.Text);
+                    break;
+                default:
+                    throw new Exception("Untested Send type");
+            }
+
+            ServiceContainer.Reset();
+        }
+    }
+}
