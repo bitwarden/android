1532194c3d0d7323de6641007e42c89d8bc8fdb1
Matt Gibson <mgibson@bitwarden.com>
2021-03-22 17:35:18 -0500

    Remove unused helper

--- src/Core/Utilities/CoreHelpers.cs
+++ src/Core/Utilities/CoreHelpers.cs
@@ -249,31 +249,5 @@ namespace Bit.Core.Utilities
         {
             return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(obj));
         }
-
-        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, long size)
-        {
-            T[] bucket = null;
-            var count = 0;
-            foreach (var item in source)
-            {
-                if (bucket == null)
-                {
-                    bucket = new T[size];
-                }
-                bucket[count++] = item;
-                if (count != size)
-                {
-                    continue;
-                }
-                yield return bucket.Select(x => x);
-                bucket = null;
-                count = 0;
-            }
-            // Return the last bucket with all remaining elements
-            if (bucket != null && count > 0)
-            {
-                yield return bucket.Take(count);
-            }
-        }
     }
 }
