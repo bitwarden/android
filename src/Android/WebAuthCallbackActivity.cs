eedfbaca0c2a9c09d6ec3e8a044987514b1d8167
Matt Portune <mportune@bitwarden.com>
2021-05-19 12:32:49 -0400

    fix for activity exporting and workaround for task affinity

--- src/Android/WebAuthCallbackActivity.cs
+++ src/Android/WebAuthCallbackActivity.cs
@@ -3,7 +3,10 @@ using Android.Content.PM;
 
 namespace Bit.Droid
 {
-    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [Activity(
+        Exported = false,
+        NoHistory = true, 
+        LaunchMode = LaunchMode.SingleTop)]
     [IntentFilter(new[] { Android.Content.Intent.ActionView },
         Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
         DataScheme = "bitwarden")]
