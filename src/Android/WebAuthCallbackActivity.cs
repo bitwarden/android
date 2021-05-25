7e7256e7cd11e16f2ffedab61a7a25573ee0d86a
Matt Portune <mportune@bitwarden.com>
2021-05-25 14:19:03 -0400

    replaced blank taskaffinity with singletask launchmode

--- src/Android/WebAuthCallbackActivity.cs
+++ src/Android/WebAuthCallbackActivity.cs
@@ -4,7 +4,6 @@ using Android.Content.PM;
 namespace Bit.Droid
 {
     [Activity(
-        Exported = false,
         NoHistory = true, 
         LaunchMode = LaunchMode.SingleTop)]
     [IntentFilter(new[] { Android.Content.Intent.ActionView },
