18e3eef885874c75627301e50f6ef0443b7570ce
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2021-07-22 15:07:56 -0400

    Move scripts directly into workflows

--- .github/scripts/android/deploy-play.ps1
+++
@@ -1,9 +0,0 @@
-﻿$rootPath = $env:GITHUB_WORKSPACE;
-$homePath = Resolve-Path "~" | Select-Object -ExpandProperty Path;
-
-$publisherPath = $($rootPath + "/store/google/Publisher/bin/Release/netcoreapp2.0/Publisher.dll");
-$credsPath = $($homePath + "/secrets/play_creds.json");
-$aabPath = $($rootPath + "/com.x8bit.bitwarden.aab");
-$track = "internal";
-
-dotnet $publisherPath $credsPath $aabPath $track
