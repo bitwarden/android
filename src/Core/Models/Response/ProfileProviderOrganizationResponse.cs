b6e4b4885462f085c6fb0784693e40df64283765
Thomas Rittson <trittson@bitwarden.com>
2021-09-10 14:20:16 +1000

    Remove leftover code

--- src/Core/Models/Response/ProfileProviderOrganizationResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Bit.Core.Models.Response
-{
-    public class ProfileProviderOrganizationResponse
-    {
-        public string Id { get; set; }
-
-        // Other properties not implemented at this time
-    }
-}
