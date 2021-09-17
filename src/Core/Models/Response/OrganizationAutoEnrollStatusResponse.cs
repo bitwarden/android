51be6e522bab88372c66637bf14c67bd06fa2072
Vincent Salucci <26154748+vincentsalucci@users.noreply.github.com>
2021-09-16 23:01:12 -0500

    [SSO/Auto Enroll] Fixed response object typo (#1542)

--- src/Core/Models/Response/OrganizationAutoEnrollStatusResponse.cs
+++ src/Core/Models/Response/OrganizationAutoEnrollStatusResponse.cs
@@ -3,6 +3,6 @@ namespace Bit.Core.Models.Response
     public class OrganizationAutoEnrollStatusResponse
     {
         public string Id { get; set; }
-        public bool ResetPasswordEnrolled { get; set; }
+        public bool ResetPasswordEnabled { get; set; }
     }
 }
