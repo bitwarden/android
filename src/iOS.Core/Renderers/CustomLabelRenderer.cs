adb8bb4f1bd384e57431cf98b3a4d0f16e528efa
Federico Maccaroni <fedemkr@gmail.com>
2021-12-16 11:36:06 -0300

    Fix by workaround crash on LabelRenderer and when changing themes #1689 (#1690)

--- src/iOS.Core/Renderers/CustomLabelRenderer.cs
+++ src/iOS.Core/Renderers/CustomLabelRenderer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using Bit.iOS.Core.Renderers;
 using Bit.iOS.Core.Utilities;
 using UIKit;
@@ -9,34 +10,49 @@ using Xamarin.Forms.Platform.iOS;
 namespace Bit.iOS.Core.Renderers
 {
     public class CustomLabelRenderer : LabelRenderer
-    {
-        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
-        {
-            base.OnElementChanged(e);
-            if (Control != null && e.NewElement is Label)
-            {
-                UpdateFont();
-            }
-        }
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+			if (Control != null && e.NewElement is Label)
+			{
+				UpdateFont();
+			}
+		}
 
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-            if (e.PropertyName == Label.FontProperty.PropertyName ||
-                e.PropertyName == Label.TextProperty.PropertyName ||
-                e.PropertyName == Label.FormattedTextProperty.PropertyName)
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+            try
             {
-                UpdateFont();
-            }
-        }
+				base.OnElementPropertyChanged(sender, e);
+			}
+			catch (NullReferenceException)
+			{
+				// Do nothing...
+				// There is an issue on Xamarin Forms which throws a null reference
+				// https://appcenter.ms/users/kspearrin/apps/bitwarden/crashes/errors/534094859u/overview
+				// TODO: Maybe something like this https://github.com/matteobortolazzo/HtmlLabelPlugin/pull/113 can be implemented to avoid this
+				// on html labels.
+			}
 
-        private void UpdateFont()
-        {
-            var pointSize = iOSHelpers.GetAccessibleFont<Label>(Element.FontSize);
-            if (pointSize != null)
-            {
-                Control.Font = UIFont.FromDescriptor(Element.Font.ToUIFont().FontDescriptor, pointSize.Value);
-            }
-        }
-    }
+			if (e.PropertyName == Label.FontProperty.PropertyName ||
+				e.PropertyName == Label.TextProperty.PropertyName ||
+				e.PropertyName == Label.FormattedTextProperty.PropertyName)
+			{
+				UpdateFont();
+			}
+		}
+
+		private void UpdateFont()
+		{
+			if (Element is null || Control is null)
+				return;
+
+			var pointSize = iOSHelpers.GetAccessibleFont<Label>(Element.FontSize);
+			if (pointSize != null)
+			{
+				Control.Font = UIFont.FromDescriptor(Element.Font.ToUIFont().FontDescriptor, pointSize.Value);
+			}
+		}
+	}
 }
