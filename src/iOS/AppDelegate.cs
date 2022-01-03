9fdcba386ee96c3897aa1e001d2780f25a57f744
Federico Maccaroni <fedemkr@gmail.com>
2022-01-03 12:46:42 -0300

    Possible fix for iOS crash after long time idling (#1700)

--- src/iOS/AppDelegate.cs
+++ src/iOS/AppDelegate.cs
@@ -201,8 +201,8 @@ namespace Bit.iOS
 
         public override void DidEnterBackground(UIApplication uiApplication)
         {
-            _storageService.SaveAsync(Constants.LastActiveTimeKey, _deviceActionService.GetActiveTime());
-            _messagingService.Send("slept");
+            _storageService?.SaveAsync(Constants.LastActiveTimeKey, _deviceActionService.GetActiveTime());
+            _messagingService?.Send("slept");
             base.DidEnterBackground(uiApplication);
         }
 
@@ -220,7 +220,7 @@ namespace Bit.iOS
 
         public override void WillEnterForeground(UIApplication uiApplication)
         {
-            _messagingService.Send("resumed");
+            _messagingService?.Send("resumed");
             base.WillEnterForeground(uiApplication);
         }
 
