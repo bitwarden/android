2791d4b8ec9395fb58afcd4aa48a126ad24c8536
Federico Maccaroni <fedemkr@gmail.com>
2022-01-18 11:52:08 -0300

    Fixes for iOS push notifications (#1708)
    * WIP Fixes for iOS push notifications
    
    * WIP Fixes for iOS push notifications, fix missed implementation on android
    
    * Fix some issues on the push notifications, changed to Debug Console.WriteLine, and added update on entitlements on the build.yml

--- src/App/Abstractions/IPushNotificationService.cs
+++ src/App/Abstractions/IPushNotificationService.cs
@@ -4,6 +4,7 @@ namespace Bit.App.Abstractions
 {
     public interface IPushNotificationService
     {
+        bool IsRegisteredForPush { get; }
         Task<string> GetTokenAsync();
         Task RegisterAsync();
         Task UnregisterAsync();
