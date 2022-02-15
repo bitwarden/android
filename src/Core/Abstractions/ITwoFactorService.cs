69cb26d99fbcf6733246ac7f7f94dba9e91e080f
Thomas Rittson <trittson@bitwarden.com>
2022-02-15 14:39:15 +1000

    Add LogInCredentials

---
+++ src/Core/Abstractions/ITwoFactorService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bit.Core.Enums;
+using Bit.Core.Models.Domain;
+
+namespace Bit.Core.Abstractions
+{
+    public interface IAuthService
+    {
+        string Email { get; set; }
+        string MasterPasswordHash { get; set; }
+        string Code { get; set; }
+        string CodeVerifier { get; set; }
+        string SsoRedirectUrl { get; set; }
+        TwoFactorProviderType? SelectedTwoFactorProviderType { get; set; }
+        Dictionary<TwoFactorProviderType, TwoFactorProvider> TwoFactorProviders { get; set; }
+        Dictionary<TwoFactorProviderType, Dictionary<string, object>> TwoFactorProvidersData { get; set; }
+
+        TwoFactorProviderType? GetDefaultTwoFactorProvider(bool fido2Supported);
+        bool AuthingWithSso();
+        bool AuthingWithPassword();
+        List<TwoFactorProvider> GetSupportedTwoFactorProviders();
+        Task<AuthResult> LogInAsync(string email, string masterPassword, string captchaToken);
+        Task<AuthResult> LogInSsoAsync(string code, string codeVerifier, string redirectUrl, string orgId);
+        Task<AuthResult> LogInCompleteAsync(string email, string masterPassword, TwoFactorProviderType twoFactorProvider, string twoFactorToken, bool? remember = null);
+        Task<AuthResult> LogInTwoFactorAsync(TwoFactorProviderType twoFactorProvider, string twoFactorToken, bool? remember = null);
+        void LogOut(Action callback);
+        void Init();
+    }
+}
---
+++ src/Core/Models/Domain/logInCredentials.cs
@@ -0,0 +1,42 @@
+﻿using Bit.Core.Models.Request.IdentityToken;
+
+namespace Bit.Core.Models.Domain
+{
+    public class LogInCredentials
+    {
+        public LogInCredentials()
+        {
+        }
+    }
+
+    public class PasswordLogInCredentials : LogInCredentials
+    {
+        public string Email { get; set; }
+        public string MasterPassword { get; set; }
+        public string CaptchaToken { get; set; }
+        public TokenRequestTwoFactor TwoFactor { get; set; }
+
+        public PasswordLogInCredentials(string email, string masterPassword, string captchaToken, TokenRequestTwoFactor twoFactor)
+        {
+            Email = email;
+            MasterPassword = masterPassword;
+            CaptchaToken = captchaToken;
+            TwoFactor = twoFactor;
+        }
+    }
+
+    public class SsoLogInCredentials : LogInCredentials
+    {
+        public string Code { get; set; }
+        public string CodeVerifier { get; set; }
+        public string RedirectUri { get; set; }
+        public TokenRequestTwoFactor TwoFactor { get; set; }
+
+        public SsoLogInCredentials(string code, string codeVerifier, string redirectUri, TokenRequestTwoFactor twoFactor) {
+            Code = code;
+            CodeVerifier = codeVerifier;
+            RedirectUri = redirectUri;
+            TwoFactor = twoFactor;
+        }
+    }
+}
