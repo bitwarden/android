a03f79b38c9e5b4e003fc18e01e8f5abb5dc2e59
Federico Maccaroni <fedemkr@gmail.com>
2022-02-17 17:12:30 -0300

    Fixed some Large Font Accessibility issues on Vault and Send screens for Icons Display #1774

---
+++ src/iOS.Core/Utilities/FontElementExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Bit.iOS.Core.Utilities
+{
+    public static class FontElementExtensions
+    {
+        public static UIFont ToUIFont(this IFontElement fontElement)
+        {
+            var fontSize = fontElement.FontSize;
+            var fontAttributes = fontElement.FontAttributes;
+            var fontFamily = fontElement.FontFamily;
+
+            return fontFamily is null
+                ? Font.SystemFontOfSize(fontSize, fontAttributes).ToUIFont()
+                : Font.OfSize(fontFamily, fontSize).WithAttributes(fontAttributes).ToUIFont();
+        }
+    }
+}
