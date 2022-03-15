3a2a1b527f53887ea2737017708ae8adf7f6749f
Federico Maccaroni <fedemkr@gmail.com>
2022-03-15 11:31:33 -0300

    Removed grouping from Settings to fix a crash on iOS 15.4

--- src/App/Pages/Settings/SettingsPage/SettingsPageListItemSelector.cs
+++ src/App/Pages/Settings/SettingsPage/SettingsPageListItemSelector.cs
@@ -4,21 +4,19 @@ namespace Bit.App.Pages
 {
     public class SettingsPageListItemSelector : DataTemplateSelector
     {
+        public DataTemplate HeaderTemplate { get; set; }
         public DataTemplate RegularTemplate { get; set; }
         public DataTemplate TimePickerTemplate { get; set; }
 
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
+            if (item is SettingsPageHeaderListItem)
+            {
+                return HeaderTemplate;
+            }
             if (item is SettingsPageListItem listItem)
             {
-                if (listItem.ShowTimeInput)
-                {
-                    return TimePickerTemplate;
-                }
-                else
-                {
-                    return RegularTemplate;
-                }
+                return listItem.ShowTimeInput ? TimePickerTemplate : RegularTemplate;
             }
             return null;
         }
