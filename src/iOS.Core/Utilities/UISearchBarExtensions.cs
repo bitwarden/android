37ed5bf0fa00e15aa39a98cffaefbec5b9b6830a
Carlos Gonçalves <cgoncalves@bitwarden.com>
2022-06-07 10:31:45 +0100

    PS-587 - PR corrections

--- src/iOS.Core/Utilities/UISearchBarExtensions.cs
+++ src/iOS.Core/Utilities/UISearchBarExtensions.cs
@@ -4,12 +4,12 @@ namespace Bit.iOS.Core.Utilities
 {
     public static class UISearchBarExtensions
     {
-        public static void ApplyDarkThemesColors(this UISearchBar searchBar)
+        public static void UpdateThemeIfNeeded(this UISearchBar searchBar)
         {
-            var versionParts = UIDevice.CurrentDevice.SystemVersion.Split('.');
-            if (versionParts.Length > 0 && int.TryParse(versionParts[0], out var version))
+            if (!ThemeHelpers.LightTheme)
             {
-                if (version >= 13)
+                searchBar.KeyboardAppearance = UIKeyboardAppearance.Dark;
+                if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
                 {
                     searchBar.SearchTextField.TextColor = UIColor.White;
                     searchBar.SearchTextField.LeftView.TintColor = UIColor.White;
