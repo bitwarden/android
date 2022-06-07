ec71b21264238b8a6ea0c6de8971bdc01947dc26
Federico Maccaroni <fedemkr@gmail.com>
2022-06-07 15:57:53 -0300

    PS-785 Added logs for exceptions on UpdateTemplatedCell and the extra data on each ExtendedCollectionView usage (#1943)

---
+++ src/iOS.Core/Renderers/CollectionView/ExtendedGroupableItemsViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Bit.App.Controls;
+using Foundation;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Bit.iOS.Core.Renderers.CollectionView
+{
+    public class ExtendedGroupableItemsViewController<TItemsView> : GroupableItemsViewController<TItemsView>
+        where TItemsView : ExtendedCollectionView
+    {
+        public ExtendedGroupableItemsViewController(TItemsView groupableItemsView, ItemsViewLayout layout)
+            : base(groupableItemsView, layout)
+        {
+        }
+
+        protected override void UpdateTemplatedCell(TemplatedCell cell, NSIndexPath indexPath)
+        {
+            try
+            {
+                base.UpdateTemplatedCell(cell, indexPath);
+            }
+            catch (Exception ex) when (ItemsView?.ExtraDataForLogging != null)
+            {
+                throw new Exception("Error in ExtendedCollectionView, extra data: " + ItemsView.ExtraDataForLogging, ex);
+            }
+        }
+    }
+}
