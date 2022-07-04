68d4ec6fa2f8028cf14ba8d7003b5b312bc70377
Federico Andrés Maccaroni <fedemkr@gmail.com>
2022-07-04 15:09:37 -0300

    EC-297 Fix possible crash when copying password on cipher item view. Also improved a bit the code of copying commands

--- src/App/Pages/Vault/ViewPageViewModel.cs
+++ src/App/Pages/Vault/ViewPageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Bit.App.Abstractions;
 using Bit.App.Resources;
 using Bit.App.Utilities;
@@ -11,6 +12,7 @@ using Bit.Core.Enums;
 using Bit.Core.Exceptions;
 using Bit.Core.Models.View;
 using Bit.Core.Utilities;
+using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Forms;
 
 namespace Bit.App.Pages
@@ -28,6 +30,7 @@ namespace Bit.App.Pages
         private readonly IPasswordRepromptService _passwordRepromptService;
         private readonly ILocalizeService _localizeService;
         private readonly IClipboardService _clipboardService;
+        private readonly ILogger _logger;
 
         private CipherView _cipher;
         private List<ViewPageFieldViewModel> _fields;
@@ -58,10 +61,11 @@ namespace Bit.App.Pages
             _passwordRepromptService = ServiceContainer.Resolve<IPasswordRepromptService>("passwordRepromptService");
             _localizeService = ServiceContainer.Resolve<ILocalizeService>("localizeService");
             _clipboardService = ServiceContainer.Resolve<IClipboardService>("clipboardService");
+            _logger = ServiceContainer.Resolve<ILogger>("logger");
 
-            CopyCommand = new Command<string>((id) => CopyAsync(id, null));
-            CopyUriCommand = new Command<LoginUriView>(CopyUri);
-            CopyFieldCommand = new Command<FieldView>(CopyField);
+            CopyCommand = new AsyncCommand<string>((id) => CopyAsync(id, null), onException: ex => _logger.Exception(ex), allowsMultipleExecutions: false);
+            CopyUriCommand = new AsyncCommand<LoginUriView>(uriView => CopyAsync("LoginUri", uriView.Uri), onException: ex => _logger.Exception(ex), allowsMultipleExecutions: false);
+            CopyFieldCommand = new AsyncCommand<FieldView>(field => CopyAsync(field.Type == FieldType.Hidden ? "H_FieldValue" : "FieldValue", field.Value), onException: ex => _logger.Exception(ex), allowsMultipleExecutions: false);
             LaunchUriCommand = new Command<LoginUriView>(LaunchUri);
             TogglePasswordCommand = new Command(TogglePassword);
             ToggleCardNumberCommand = new Command(ToggleCardNumber);
@@ -72,9 +76,9 @@ namespace Bit.App.Pages
             PageTitle = AppResources.ViewItem;
         }
 
-        public Command CopyCommand { get; set; }
-        public Command CopyUriCommand { get; set; }
-        public Command CopyFieldCommand { get; set; }
+        public ICommand CopyCommand { get; set; }
+        public ICommand CopyUriCommand { get; set; }
+        public ICommand CopyFieldCommand { get; set; }
         public Command LaunchUriCommand { get; set; }
         public Command TogglePasswordCommand { get; set; }
         public Command ToggleCardNumberCommand { get; set; }
@@ -616,7 +620,7 @@ namespace Bit.App.Pages
             _attachmentFilename = null;
         }
 
-        private async void CopyAsync(string id, string text = null)
+        private async Task CopyAsync(string id, string text = null)
         {
             if (_passwordRepromptService.ProtectedFields.Contains(id) && !await PromptPasswordAsync())
             {
@@ -678,16 +682,6 @@ namespace Bit.App.Pages
                     await _eventService.CollectAsync(Core.Enums.EventType.Cipher_ClientCopiedHiddenField, CipherId);
                 }
             }
-        }
-
-        private void CopyUri(LoginUriView uri)
-        {
-            CopyAsync("LoginUri", uri.Uri);
-        }
-
-        private void CopyField(FieldView field)
-        {
-            CopyAsync(field.Type == Core.Enums.FieldType.Hidden ? "H_FieldValue" : "FieldValue", field.Value);
         }
 
         private void LaunchUri(LoginUriView uri)
