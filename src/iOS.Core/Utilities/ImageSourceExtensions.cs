6c928708bdefc99c1239f63217c05ba4ba8bc01f
Federico Andrés Maccaroni <fedemkr@gmail.com>
2022-07-07 14:24:44 -0300

    EC-308 Fix crash produced by creating avatar image on AccountSwitchingOverlayHelper and also added more logging to see when it happens.

--- src/iOS.Core/Utilities/ImageSourceExtensions.cs
+++ src/iOS.Core/Utilities/ImageSourceExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Bit.Core.Services;
 using UIKit;
 using Xamarin.Forms;
-using Xamarin.Forms.Internals;
 using Xamarin.Forms.Platform.iOS;
 
 namespace Bit.iOS.Core.Utilities
@@ -17,25 +17,29 @@ namespace Bit.iOS.Core.Utilities
         public static async Task<UIImage> GetNativeImageAsync(this ImageSource source, CancellationToken cancellationToken = default(CancellationToken))
         {
             if (source == null || source.IsEmpty)
+            {
                 return null;
+            }
 
             var handler = Xamarin.Forms.Internals.Registrar.Registered.GetHandlerForObject<IImageSourceHandler>(source);
             if (handler == null)
+            {
+                LoggerHelper.LogEvenIfCantBeResolved(new InvalidOperationException("GetNativeImageAsync failed cause IImageSourceHandler couldn't be found"));
                 return null;
+            }
 
             try
             {
                 float scale = (float)UIScreen.MainScreen.Scale;
-
                 return await handler.LoadImageAsync(source, scale: scale, cancelationToken: cancellationToken);
             }
             catch (OperationCanceledException)
             {
-                Log.Warning("Image loading", "Image load cancelled");
+                LoggerHelper.LogEvenIfCantBeResolved(new OperationCanceledException("GetNativeImageAsync was cancelled"));
             }
             catch (Exception ex)
             {
-                Log.Warning("Image loading", $"Image load failed: {ex}");
+                LoggerHelper.LogEvenIfCantBeResolved(new InvalidOperationException("GetNativeImageAsync failed", ex));
             }
 
             return null;
