e52f527eea4a025700bb86afdb9c421ba775f8bf
André Bispo <abispo@bitwarden.com>
2022-07-22 14:59:59 +0100

    [SSG-471] Added UI for the device login request response.

---
+++ src/App/Pages/Accounts/LoginPasswordlessPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Bit.App.Models;
+using Bit.App.Resources;
+using Bit.App.Utilities;
+using Bit.Core.Utilities;
+using Xamarin.Forms;
+
+namespace Bit.App.Pages
+{
+    public partial class LoginPasswordlessPage : Bit.App.Pages.BaseContentPage
+    {
+        public LoginPasswordlessPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Close_Clicked(object sender, System.EventArgs e)
+        {
+            if (DoOnce())
+            {
+                await Navigation.PopModalAsync();
+            }
+        }
+    }
+}
---
+++ src/App/Pages/Accounts/LoginPasswordlessViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Bit.App.Pages
+{
+    public class LoginPasswordlessViewModel : BaseViewModel
+    {
+        
+        public LoginPasswordlessViewModel()
+        {
+            PageTitle = "Log In requested";
+        }
+    }
+}
