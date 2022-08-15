26aca5730892758bafa36a67fbc56c7592ac3108
Andre Rosado <arosado@bitwarden.com>
2022-08-15 17:47:18 +0100

    PS-1009 Added effect to Entry that doesn't allow keyboard with emojis on passphrase separator

---
+++ src/Android/Effects/NoEmojiKeyboardEffect.cs
@@ -0,0 +1,24 @@
+﻿using Bit.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(NoEmojiKeyboardEffect), "NoEmojiKeyboardEffect")]
+namespace Bit.Droid.Effects
+{
+    public class NoEmojiKeyboardEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Element is Entry && Control is FormsEditText editText)
+            {
+                editText.ImeOptions = Android.Views.InputMethods.ImeAction.Done;
+                editText.InputType = Android.Text.InputTypes.ClassText | Android.Text.InputTypes.TextVariationVisiblePassword | Android.Text.InputTypes.TextFlagMultiLine;
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
+
