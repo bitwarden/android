67ad32f75dec4ebb4f401ce9156f13c85ef72f8f
Andre Rosado <arosado@bitwarden.com>
2022-08-16 11:53:57 +0100

    PS-1009 Removed unnecessary ImeOptions setting from NoEmojiKeyboardEffect Improved code

--- src/Android/Effects/NoEmojiKeyboardEffect.cs
+++ src/Android/Effects/NoEmojiKeyboardEffect.cs
@@ -1,17 +1,17 @@
-﻿using Bit.Droid.Effects;
+﻿using Android.Widget;
+using Bit.Droid.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
-[assembly: ExportEffect(typeof(NoEmojiKeyboardEffect), "NoEmojiKeyboardEffect")]
+[assembly: ExportEffect(typeof(NoEmojiKeyboardEffect), nameof(NoEmojiKeyboardEffect))]
 namespace Bit.Droid.Effects
 {
     public class NoEmojiKeyboardEffect : PlatformEffect
     {
         protected override void OnAttached()
         {
-            if (Element is Entry && Control is FormsEditText editText)
+            if (Control != null && Control is EditText editText)
             {
-                editText.ImeOptions = Android.Views.InputMethods.ImeAction.Done;
                 editText.InputType = Android.Text.InputTypes.ClassText | Android.Text.InputTypes.TextVariationVisiblePassword | Android.Text.InputTypes.TextFlagMultiLine;
             }
         }
