3d9555d4208571ec063cd934fbb9014c252ff6d5
aj-rosado <109146700+aj-rosado@users.noreply.github.com>
2022-08-16 18:54:28 +0100

    [PS-1009] Changed keyboard on Passphrase generator to not allow emojis (#2038)
    * PS-1009 Added effect to Entry that doesn't allow keyboard with emojis on passphrase separator
    
    * PS-1009 Removed unnecessary ImeOptions setting from NoEmojiKeyboardEffect
    Improved code
    
    * PS-1009 Removed unnecessary null validation on Android's NoEmojiKeyboardEffect
    
    Co-authored-by: Federico Maccaroni <fedemkr@gmail.com>

---
+++ src/App/Effects/NoEmojiKeyboardEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Bit.App.Effects
+{
+    public class NoEmojiKeyboardEffect : RoutingEffect
+    {
+        public NoEmojiKeyboardEffect()
+            : base("Bitwarden.NoEmojiKeyboardEffect")
+        { }
+    }
+}
