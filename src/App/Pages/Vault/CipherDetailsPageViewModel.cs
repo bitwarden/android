87fb5cf2aeed4aa35a6b80f6d7e3ac50e829d478
André Filipe da Silva Bispo <abispo@bitwarden.com>
2022-09-14 14:45:39 +0100

    [SG-659] Fixed TOTP not showing for free user with classic plans. (#2071)
    * [SG-659] Fixed TOTP not showing for free user with classic plans.
    
    * [SG-659] Ran code format

--- src/App/Pages/Vault/CipherDetailsPageViewModel.cs
+++ src/App/Pages/Vault/CipherDetailsPageViewModel.cs
@@ -202,7 +202,7 @@ namespace Bit.App.Pages
             }
         }
 
-        public bool ShowUpgradePremiumTotpText => !CanAccessPremium && ShowTotp;
+        public bool ShowUpgradePremiumTotpText => !CanAccessPremium && !Cipher.OrganizationUseTotp && ShowTotp;
         public bool ShowUris => IsLogin && Cipher.Login.HasUris;
         public bool ShowIdentityAddress => IsIdentity && (
             !string.IsNullOrWhiteSpace(Cipher.Identity.Address1) ||
@@ -216,7 +216,7 @@ namespace Bit.App.Pages
         public string PasswordVisibilityAccessibilityText => ShowPassword ? AppResources.PasswordIsVisibleTapToHide : AppResources.PasswordIsNotVisibleTapToShow;
         public string TotpCodeFormatted
         {
-            get => _canAccessPremium ? _totpCodeFormatted : string.Empty;
+            get => ShowUpgradePremiumTotpText ? string.Empty : _totpCodeFormatted;
             set => SetProperty(ref _totpCodeFormatted, value,
                 additionalPropertyNames: new string[]
                 {
