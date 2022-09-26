6fe7e9ce1b440b7b5d857c9ee8ae4a42da107448
André Bispo <abispo@bitwarden.com>
2022-09-26 18:27:57 +0100

    Passwordless feature branch PR (#2100)
    * [SG-471] Passwordless device login screen (#2017)
    
    * [SSG-471] Added UI for the device login request response.
    
    * [SG-471] Added text resources and arguments to Page.
    
    * [SG-471] Added properties to speed up page bindings
    
    * [SG-471] Added mock services. Added Accept/reject command binding, navigation and toast messages.
    
    * [SG-471] fixed code styling with dotnet-format
    
    * [SG-471] Fixed back button placement. PR fixes.
    
    * [SG-471] Added new Origin parameter to the page.
    
    * [SG-471] PR Fixes
    
    * [SG-471] PR fixes
    
    * [SG-471] PR Fix: added FireAndForget.
    
    * [SG-471] Moved fire and forget to run on ui thread task.
    
    * [SG-381] Passwordless - Add setting to Mobile (#2037)
    
    * [SG-381] Added settings option to approve passwordless login request. If user has notifications disabled, prompt to go to settings and enable them.
    
    * [SG-381] Update settings pop up texts.
    
    * [SG-381] Added new method to get notifications state on device settings. Added userId to property saved on device to differentiate value between users.
    
    * [SG-381] Added text for the popup on selection.
    
    * [SG-381] PR Fixes
    
    * [SG-408] Implement passwordless api methods (#2055)
    
    * [SG-408] Update notification model.
    
    * [SG-408] removed duplicated resource
    
    * [SG-408] Added implementation to Api Service of new passwordless methods.
    
    * removed qa endpoints
    
    * [SG-408] Changed auth methods implementation, added method call to viewmodel.
    
    * [SG-408] ran code format
    
    * [SG-408] PR fixes
    
    * [SG-472] Add configuration for new notification type (#2056)
    
    * [SG-472] Added methods to present local notification to the user. Configured new notification type for passwordless logins
    
    * [SG-472] Updated code to new api service changes.
    
    * [SG-472] ran dotnet format
    
    * [SG-472] PR Fixes.
    
    * [SG-472] PR Fixes
    
    * [SG-169] End-to-end testing refactor. (#2073)
    
    * [SG-169] Passwordless demo change requests (#2079)
    
    * [SG-169] End-to-end testing refactor.
    
    * [SG-169] Fixed labels. Changed color of Fingerprint phrase. Waited for app to be in foreground to launch passwordless modal to fix Android issues.
    
    * [SG-169] Anchored buttons to the bottom of the screen.
    
    * [SG-169] Changed device type from enum to string.
    
    * [SG-169] PR fixes
    
    * [SG-169] PR fixes
    
    * [SG-169] Added comment on static variable
    
    (cherry picked from commit f9a32e4abcedd490a518214a94e7aa3ab20315df)

---
+++ src/App/Pages/Accounts/LoginPasswordlessViewModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Bit.App.Abstractions;
+using Bit.App.Resources;
+using Bit.App.Utilities;
+using Bit.Core;
+using Bit.Core.Abstractions;
+using Bit.Core.Enums;
+using Bit.Core.Utilities;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Bit.App.Pages
+{
+    public class LoginPasswordlessViewModel : BaseViewModel
+    {
+        private IDeviceActionService _deviceActionService;
+        private IAuthService _authService;
+        private IPlatformUtilsService _platformUtilsService;
+        private ILogger _logger;
+        private LoginPasswordlessDetails _resquest;
+
+        public LoginPasswordlessViewModel()
+        {
+            _deviceActionService = ServiceContainer.Resolve<IDeviceActionService>("deviceActionService");
+            _platformUtilsService = ServiceContainer.Resolve<IPlatformUtilsService>("platformUtilsService");
+            _authService = ServiceContainer.Resolve<IAuthService>("authService");
+            _logger = ServiceContainer.Resolve<ILogger>("logger");
+
+            PageTitle = AppResources.LogInRequested;
+
+            AcceptRequestCommand = new AsyncCommand(() => PasswordlessLoginAsync(true),
+                onException: ex => HandleException(ex),
+                allowsMultipleExecutions: false);
+            RejectRequestCommand = new AsyncCommand(() => PasswordlessLoginAsync(false),
+                onException: ex => HandleException(ex),
+                allowsMultipleExecutions: false);
+        }
+
+        public ICommand AcceptRequestCommand { get; }
+
+        public ICommand RejectRequestCommand { get; }
+
+        public string LogInAttemptByLabel => LoginRequest != null ? string.Format(AppResources.LogInAttemptByXOnY, LoginRequest.Email, LoginRequest.Origin) : string.Empty;
+
+        public string TimeOfRequestText => CreateRequestDate(LoginRequest?.RequestDate);
+
+        public bool ShowIpAddress => !string.IsNullOrEmpty(LoginRequest?.IpAddress);
+
+        public LoginPasswordlessDetails LoginRequest
+        {
+            get => _resquest;
+            set
+            {
+                SetProperty(ref _resquest, value, additionalPropertyNames: new string[]
+                    {
+                        nameof(LogInAttemptByLabel),
+                        nameof(TimeOfRequestText),
+                        nameof(ShowIpAddress),
+                    });
+            }
+        }
+
+        private async Task PasswordlessLoginAsync(bool approveRequest)
+        {
+            await _deviceActionService.ShowLoadingAsync(AppResources.Loading);
+            await _authService.PasswordlessLoginAsync(LoginRequest.Id, LoginRequest.PubKey, approveRequest);
+            await _deviceActionService.HideLoadingAsync();
+            await Page.Navigation.PopModalAsync();
+            _platformUtilsService.ShowToast("info", null, approveRequest ? AppResources.LogInAccepted : AppResources.LogInDenied);
+        }
+
+        private string CreateRequestDate(DateTime? requestDate)
+        {
+            if (!requestDate.HasValue)
+            {
+                return string.Empty;
+            }
+
+            var minutesSinceRequest = requestDate.Value.ToUniversalTime().Minute - DateTime.UtcNow.Minute;
+            if (minutesSinceRequest < 5)
+            {
+                return AppResources.JustNow;
+            }
+            if (minutesSinceRequest < 59)
+            {
+                return string.Format(AppResources.XMinutesAgo, minutesSinceRequest);
+            }
+
+            return requestDate.Value.ToShortTimeString();
+        }
+
+        private void HandleException(Exception ex)
+        {
+            Xamarin.Essentials.MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                await _deviceActionService.HideLoadingAsync();
+                await _platformUtilsService.ShowDialogAsync(AppResources.GenericErrorMessage);
+            }).FireAndForget();
+            _logger.Exception(ex);
+        }
+    }
+
+    public class LoginPasswordlessDetails
+    {
+        public string Id { get; set; }
+
+        public string Key { get; set; }
+
+        public string PubKey { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Email { get; set; }
+
+        public string FingerprintPhrase { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public string DeviceType { get; set; }
+
+        public string IpAddress { get; set; }
+    }
+}
--- src/iOS.Core/Services/DeviceActionService.cs
+++ src/iOS.Core/Services/DeviceActionService.cs
@@ -643,5 +643,11 @@ namespace Bit.iOS.Core.Services
                 _deviceActionService.PickedDocument(url);
             }
         }
+
+        public void OpenAppSettings()
+        {
+            var url = new NSUrl(UIApplication.OpenSettingsUrlString);
+            UIApplication.SharedApplication.OpenUrl(url);
+        }
     }
 }
