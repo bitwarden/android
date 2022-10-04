3161f5539a6e5dc7b7cd3763109e83498c486826
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-10-05 00:01:06 +0300

    Use `Rfc2898DeriveBytes` for PBKDF2 and move CryptoPrimitiveService to Core.

--- src/Core/Services/CryptoPrimitiveService.cs
+++ src/Core/Services/CryptoPrimitiveService.cs
@@ -1,37 +1,33 @@
 ﻿using Bit.Core.Abstractions;
 using Bit.Core.Enums;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Digests;
-using Org.BouncyCastle.Crypto.Generators;
-using Org.BouncyCastle.Crypto.Parameters;
 using System;
+using System.Security.Cryptography;
 
-namespace Bit.Droid.Services
+namespace Bit.Core.Services
 {
     public class CryptoPrimitiveService : ICryptoPrimitiveService
     {
         public byte[] Pbkdf2(byte[] password, byte[] salt, CryptoHashAlgorithm algorithm, int iterations)
         {
-            int keySize = 256;
-            IDigest digest = null;
+            int keySize;
+            HashAlgorithmName digest;
             if (algorithm == CryptoHashAlgorithm.Sha256)
             {
                 keySize = 256;
-                digest = new Sha256Digest();
+                digest = HashAlgorithmName.SHA256;
             }
             else if (algorithm == CryptoHashAlgorithm.Sha512)
             {
                 keySize = 512;
-                digest = new Sha512Digest();
+                digest = HashAlgorithmName.SHA512;
             }
             else
             {
                 throw new ArgumentException("Unsupported PBKDF2 algorithm.");
             }
 
-            var generator = new Pkcs5S2ParametersGenerator(digest);
-            generator.Init(password, salt, iterations);
-            return ((KeyParameter)generator.GenerateDerivedMacParameters(keySize)).GetKey();
+            var generator = new Rfc2898DeriveBytes(password, salt, iterations, digest);
+            return generator.GetBytes(keySize);
         }
     }
 }
