43c1fdef00ee7b63a4e2817db0748aa175c259fc
André Bispo <abispo@bitwarden.com>
2022-10-04 11:38:21 +0100

    [SG-702] Notification data model

---
+++ src/App/Models/NotificationData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Bit.App.Models
+{
+    public abstract class BaseNotificationData
+    {
+        public abstract string Type { get; }
+
+    }
+
+    public class PasswordlessNotificationData : BaseNotificationData
+    {
+        public const string TYPE = "passwordlessNotificationData";
+
+        public override string Type => TYPE;
+
+        public int TimeoutInMinutes { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string NotificationId { get; set; }
+    }
+
+}
+
