4c2f5c05e5d564c1269a5c6b0bf7fd3471cdfe45
aj-rosado <109146700+aj-rosado@users.noreply.github.com>
2022-10-19 10:57:01 +0100

    [PS-1188] Add new Solarized Dark Theme (#2141)
    * PS-1188 Add new Solarized Dark Theme

---
+++ src/App/Styles/SolarizedDark.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Bit.App.Styles
+{
+    public partial class SolarizedDark : ResourceDictionary, IThemeResourceDictionary
+    {
+        public SolarizedDark()
+        {
+            InitializeComponent();
+        }
+    }
+}
