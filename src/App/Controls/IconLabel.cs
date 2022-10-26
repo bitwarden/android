3cde6091c77552e22c72289eaa350e192fc993e3
André Bispo <abispo@bitwarden.com>
2022-10-26 16:26:28 +0100

    [SG-166] Add Renderer for IconLabel control. Add RemoveFontPadding bool property.

--- src/App/Controls/IconLabel.cs
+++ src/App/Controls/IconLabel.cs
@@ -6,6 +6,15 @@ namespace Bit.App.Controls
     {
         public bool ShouldUpdateFontSizeDynamicallyForAccesibility { get; set; }
 
+        public static readonly BindableProperty RemoveFontPaddingProperty = BindableProperty.Create(
+            nameof(IncludeFontPadding), typeof(bool?), typeof(IconLabel), defaultValue: true);
+
+        public bool? IncludeFontPadding
+        {
+            get => GetValue(RemoveFontPaddingProperty) as bool?;
+            set => SetValue(RemoveFontPaddingProperty, value);
+        }
+
         public IconLabel()
         {
             switch (Device.RuntimePlatform)
