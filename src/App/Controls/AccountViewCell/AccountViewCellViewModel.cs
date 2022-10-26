5deba15373ce8d8451e6d0580fac407c0c45a2cb
mp-bw <59324545+mp-bw@users.noreply.github.com>
2022-10-26 12:34:54 -0400

    Updated avatar color selection logic (#2151)
    * updated avatar color selection logic
    
    * tweaks
    
    * more tweaks
    
    * formatting

--- src/App/Controls/AccountViewCell/AccountViewCellViewModel.cs
+++ src/App/Controls/AccountViewCell/AccountViewCellViewModel.cs
@@ -14,7 +14,7 @@ namespace Bit.App.Controls
         {
             AccountView = accountView;
             AvatarImageSource = ServiceContainer.Resolve<IAvatarImageSourcePool>("avatarImageSourcePool")
-                                                ?.GetOrCreateAvatar(AccountView.Name, AccountView.Email);
+                ?.GetOrCreateAvatar(AccountView.UserId, AccountView.Name, AccountView.Email);
         }
 
         public AccountView AccountView
