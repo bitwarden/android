fa8e5c0dbac086237fd84dac8cfa10923af49f00
André Bispo <abispo@bitwarden.com>
2022-10-26 19:36:54 +0100

    [SG-166] Remove IconLabel Android renderer. Add RemoveFontPadding effect.

---
+++ src/Android/Effects/RemoveFontPaddingEffect.cs
@@ -0,0 +1,25 @@
+﻿using Android.Graphics.Drawables;
+using Android.Widget;
+using Bit.Droid.Effects;
+using Bit.Droid.Utilities;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(RemoveFontPaddingEffect), "RemoveFontPaddingEffect")]
+namespace Bit.Droid.Effects
+{
+    public class RemoveFontPaddingEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Element is Label label && Control is TextView textView)
+            {
+                textView.SetIncludeFontPadding(false);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
