015d4864d22d46011a741878e107ffb2754be273
André Bispo <abispo@bitwarden.com>
2022-10-26 16:27:59 +0100

    [SG-166] Add new control. Add styles for the control.

--- src/App/Controls/IconLabelButton/IconLabelButton.xaml.cs
+++ src/App/Controls/IconLabelButton/IconLabelButton.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -20,6 +21,15 @@ namespace Bit.App.Controls
 
         public static readonly BindableProperty ButtonCommandProperty = BindableProperty.Create(
             nameof(ButtonCommand), typeof(Command), typeof(IconLabelButton));
+
+        public static readonly BindableProperty IconLabelColorProperty = BindableProperty.Create(
+            nameof(IconLabelColor), typeof(Color), typeof(IconLabelButton), Color.White);
+
+        public static readonly BindableProperty IconLabelBackgroundColorProperty = BindableProperty.Create(
+            nameof(IconLabelBackgroundColor), typeof(Color), typeof(IconLabelButton), Color.White);
+
+        public static readonly BindableProperty IconLabelBorderColorProperty = BindableProperty.Create(
+            nameof(IconLabelBorderColor), typeof(Color), typeof(IconLabelButton), Color.White);
 
         public IconLabelButton()
         {
@@ -42,6 +52,30 @@ namespace Bit.App.Controls
         {
             get => GetValue(ButtonCommandProperty) as ICommand;
             set => SetValue(ButtonCommandProperty, value);
+        }
+
+        public Color IconLabelColor
+        {
+            get { return (Color)GetValue(IconLabelColorProperty); }
+            set { SetValue(IconLabelColorProperty, value); }
+        }
+
+        public Color IconLabelBackgroundColor
+        {
+            get { return (Color)GetValue(IconLabelBackgroundColorProperty); }
+            set { SetValue(IconLabelBackgroundColorProperty, value); }
+        }
+
+        public Color IconLabelBorderColor
+        {
+            get { return (Color)GetValue(IconLabelBorderColorProperty); }
+            set { SetValue(IconLabelBorderColorProperty, value); }
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            this.InvalidateLayout();
         }
     }
 }
