15522caabf6ae1f2448aaae1576a5b8e9da892c2
André Bispo <abispo@bitwarden.com>
2022-10-28 15:33:35 +0100

    [SG-166] Fix layout for IconLabelButton

--- src/App/Controls/IconLabelButton/IconLabelButton.xaml.cs
+++ src/App/Controls/IconLabelButton/IconLabelButton.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
