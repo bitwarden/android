6362f5baa9d5ab046da4a3ca391d7d899b93d1b9
Federico Andrés Maccaroni <fedemkr@gmail.com>
2022-11-07 16:52:46 -0300

    EC-614 Implemented watch ciphers list UI

--- src/watchOS/bitwarden/bitwarden WatchKit Extension/Services/Mocks/CipherServiceMock.swift
+++ src/watchOS/bitwarden/bitwarden WatchKit Extension/Services/Mocks/CipherServiceMock.swift
@@ -14,10 +14,13 @@ class CipherServiceMock: CipherServiceProtocol{
     private var ciphers = [Cipher]()
     
     init() {
+        //ciphers = []
+            
         ciphers = [
-            Cipher(id: "1", name: "Site 1", organizationUseTotp: true, login: Login(username: "user1", totp: "555 444")),
-            Cipher(id: "2", name: "Site 2", organizationUseTotp: true, login: Login(username: "user2", totp: "567 435")),
-            Cipher(id: "3", name: "Really long name for a site that is used for a totp", organizationUseTotp: true, login: Login(username: "user3", totp: "123 456"))
+            Cipher(id: "1", name: "Site 1", organizationUseTotp: true, login: Login(username: "jadenroberts@bitwarden.com", totp: "555 444")),
+            Cipher(id: "2", name: "No user", organizationUseTotp: true, login: Login(username: nil, totp: "555 444")),
+            Cipher(id: "3", name: "Site 2", organizationUseTotp: true, login: Login(username: "longtestemail000000@fastmailasdfasdf.com", totp: "567 435")),
+            Cipher(id: "4", name: "Really long name for a site that is used for a totp", organizationUseTotp: true, login: Login(username: "user3", totp: "123 456"))
         ]
     }
     
