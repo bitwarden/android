a613405619b2770e0b0ac8db3ae5bb50f4dd0f4b
Federico Andrés Maccaroni <fedemkr@gmail.com>
2022-11-16 10:57:30 -0300

    EC-617 Added state view on watch app and some state helpers and wired it on the CipherListView. Also added some images

---
+++ src/watchOS/bitwarden/bitwarden WatchKit Extension/Views/BWStateView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct BWStateView: View {
+    @ObservedObject var viewModel:BWStateViewModel
+    
+    init(_ state: BWState) {
+        viewModel = BWStateViewModel(state)
+    }
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Spacer()
+            Image("BitwardenImagetype")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: .infinity, height: 35)
+                .padding(.leading, 15)
+                .padding(.trailing, 15)
+                .padding(.top, 5)
+            Spacer()
+            Text(LocalizedStringKey(viewModel.text))
+                .font(.title3)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+            if viewModel.isLoading {
+                ProgressView()
+                    .frame(width: 20, height: 20)
+            }
+        }
+    }
+}
+
+struct BWStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        BWStateView(.needSetup)
+    }
+}
