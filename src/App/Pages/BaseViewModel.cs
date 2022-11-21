e18013c57aebda482bdf729a624d308d13c88c2f
André Bispo <abispo@bitwarden.com>
2022-11-21 14:25:33 +0000

    [SG-834] Add generic handle exception method to BaseViewModel

--- src/App/Pages/BaseViewModel.cs
+++ src/App/Pages/BaseViewModel.cs
@@ -5,6 +5,7 @@ using Bit.App.Resources;
 using Bit.Core.Abstractions;
 using Bit.Core.Utilities;
 using Xamarin.Forms;
+using Bit.Core.Services;
 
 namespace Bit.App.Pages
 {
@@ -12,6 +13,16 @@ namespace Bit.App.Pages
     {
         private string _pageTitle = string.Empty;
         private AvatarImageSource _avatar;
+        private readonly IDeviceActionService _deviceActionService;
+        private readonly IPlatformUtilsService _platformUtilsService;
+        private readonly ILogger _logger;
+
+        protected BaseViewModel()
+        {
+            _deviceActionService = ServiceContainer.Resolve<IDeviceActionService>();
+            _platformUtilsService = ServiceContainer.Resolve<IPlatformUtilsService>();
+            _logger = ServiceContainer.Resolve<ILogger>();
+        }
 
         public string PageTitle
         {
@@ -27,14 +38,14 @@ namespace Bit.App.Pages
 
         public ContentPage Page { get; set; }
 
-        protected void HandleException(Exception ex, IDeviceActionService deviceActionService, IPlatformUtilsService platformUtilsService, ILogger logger)
+        protected void HandleException(Exception ex)
         {
             Xamarin.Essentials.MainThread.InvokeOnMainThreadAsync(async () =>
             {
-                await deviceActionService.HideLoadingAsync();
-                await platformUtilsService.ShowDialogAsync(AppResources.GenericErrorMessage);
+                await _deviceActionService.HideLoadingAsync();
+                await _platformUtilsService.ShowDialogAsync(AppResources.GenericErrorMessage);
             }).FireAndForget();
-            logger.Exception(ex);
+            _logger.Exception(ex);
         }
     }
 }
