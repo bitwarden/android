6af19f8e5c0cfa23bb0c43175e6e76c06ec91398
Federico Andrés Maccaroni <fedemkr@gmail.com>
2022-12-01 20:24:12 -0300

    EC-786 Commented need unlock state

--- src/watchOS/bitwarden/bitwarden WatchKit Extension/Utilities/BWState.swift
+++ src/watchOS/bitwarden/bitwarden WatchKit Extension/Utilities/BWState.swift
@@ -3,7 +3,7 @@ import Foundation
 enum BWState : Int, Codable {
     case valid = 0
     case needLogin = 1
-    case needUnlock = 2
+//    case needUnlock = 2
     case needPremium = 3
     case needSetup = 4
     case need2FAItem = 5
