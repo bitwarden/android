6973a0b71ca0cde59e01d9dc5c26825cbfa432d8
mp-bw <59324545+mp-bw@users.noreply.github.com>
2022-12-05 12:49:34 -0500

    [BEEEP] Support for automatic TOTP token copy via external autofill (Android) (#2220)
    * Android: Support for automatic TOTP copy via external autofill
    
    * update iOS autofill interface
    
    * additional tweaks

--- src/Android/Receivers/ClearClipboardAlarmReceiver.cs
+++ src/Android/Receivers/ClearClipboardAlarmReceiver.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Content;
+using Android.OS;
 
 namespace Bit.Droid.Receivers
 {
@@ -8,7 +9,17 @@ namespace Bit.Droid.Receivers
         public override void OnReceive(Context context, Intent intent)
         {
             var clipboardManager = context.GetSystemService(Context.ClipboardService) as ClipboardManager;
-            clipboardManager.PrimaryClip = ClipData.NewPlainText("bitwarden", " ");
+            if (clipboardManager == null)
+            {
+                return;
+            }
+            // ClearPrimaryClip is supported down to API 28 with mixed results, so we're requiring 33+ instead
+            if ((int)Build.VERSION.SdkInt < 33)
+            {
+                clipboardManager.PrimaryClip = ClipData.NewPlainText("bitwarden", " ");
+                return;
+            }
+            clipboardManager.ClearPrimaryClip();
         }
     }
 }
