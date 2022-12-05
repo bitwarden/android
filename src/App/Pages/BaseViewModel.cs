59b3458fe174e857464e50d56abcf52241820b7e
André Bispo <abispo@bitwarden.com>
2022-12-05 17:36:08 +0000

    [SG-872] Add method to handle exception in BaseViewModel

--- src/App/Pages/BaseViewModel.cs
+++ src/App/Pages/BaseViewModel.cs
@@ -1,4 +1,9 @@
-﻿using Bit.App.Controls;
+﻿using Bit.App.Abstractions;
+using System;
+using Bit.App.Controls;
+using Bit.App.Resources;
+using Bit.Core.Abstractions;
+using Bit.Core.Services;
 using Bit.Core.Utilities;
 using Xamarin.Forms;
 
@@ -8,6 +13,9 @@ namespace Bit.App.Pages
     {
         private string _pageTitle = string.Empty;
         private AvatarImageSource _avatar;
+        private LazyResolve<IDeviceActionService> _deviceActionService = new LazyResolve<IDeviceActionService>();
+        private LazyResolve<IPlatformUtilsService> _platformUtilsService = new LazyResolve<IPlatformUtilsService>();
+        private LazyResolve<ILogger> _logger = new LazyResolve<ILogger>();
 
         public string PageTitle
         {
@@ -22,5 +30,16 @@ namespace Bit.App.Pages
         }
 
         public ContentPage Page { get; set; }
+
+        protected void HandleException(Exception ex, string message = null)
+        {
+            Xamarin.Essentials.MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                await _deviceActionService.Value.HideLoadingAsync();
+                await _platformUtilsService.Value.ShowDialogAsync(message ?? AppResources.GenericErrorMessage);
+            }).FireAndForget();
+            _logger.Value.Exception(ex);
+        }
+
     }
 }
