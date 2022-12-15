50d469ca71ebeeac71627af9807c44693ca29860
André Bispo <abispo@bitwarden.com>
2022-12-15 22:22:19 +0000

    [SG-886] remove import

--- src/Core/Services/PasswordGenerationService.cs
+++ src/Core/Services/PasswordGenerationService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Bit.Core.Abstractions;
