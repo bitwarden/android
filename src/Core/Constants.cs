246a1ccba5b5e443be121704198cfb2a74620315
Matt Portune <59324545+mp-bw@users.noreply.github.com>
2023-01-06 13:21:24 -0500

    fix my own bad advice + tweaks

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -102,6 +102,5 @@
         public static string PushLastRegistrationDateKey(string userId) => $"pushLastRegistrationDate_{userId}";
         public static string PushCurrentTokenKey(string userId) => $"pushCurrentToken_{userId}";
         public static string ShouldConnectToWatchKey(string userId) => $"shouldConnectToWatch_{userId}";
-        public static string AvatarColorKey(string userId) => $"avatarColor_{userId}";
     }
 }
