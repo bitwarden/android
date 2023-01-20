9613019d4dda9d52973c540b3a6c44097bcf02b1
Bernd Schoolmann <mail@quexten.com>
2023-01-20 22:03:16 +0100

    [PS-2249] Implement Argon2 (#2293)
    * Implement Argon2
    
    * Fix incorrect argon2 type on iOS
    
    * Switch argon2 implementation to native bindings
    
    * Change argon2 to save iterations instead of memory as 'kdfIterations'
    
    * Remove mistakenly added import
    
    * Remove unused library

--- src/Android/Services/CryptoPrimitiveService.cs
+++ src/Android/Services/CryptoPrimitiveService.cs
@@ -5,6 +5,8 @@ using Org.BouncyCastle.Crypto.Digests;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Parameters;
 using System;
+using System.Runtime.InteropServices;
+using Java.Lang;
 
 namespace Bit.Droid.Services
 {
@@ -33,5 +35,15 @@ namespace Bit.Droid.Services
             generator.Init(password, salt, iterations);
             return ((KeyParameter)generator.GenerateDerivedMacParameters(keySize)).GetKey();
         }
+        public byte[] Argon2id(byte[] password, byte[] salt, int iterations, int memory, int parallelism)
+        {
+            JavaSystem.LoadLibrary("argon2");
+            var hash = new byte[32];
+            argon2id_hash_raw(iterations, memory, parallelism, password, password.Length, password, salt.Length, hash, 32);
+            return hash.ToArray();
+        }
+
+        [DllImport("argon2", EntryPoint = "argon2id_hash_raw")]
+        internal static extern int argon2id_hash_raw(int timeCost, int memoryCost, int parallelism, byte[] pwd, int pwdlen, byte[] salt, int saltlen, ref byte hash, int hashlen);
     }
 }
