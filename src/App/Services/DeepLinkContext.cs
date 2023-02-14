4896618e1c974c53b72870d6e1ee75edd101ada7
Federico Maccaroni <fedemkr@gmail.com>
2023-02-14 19:22:01 -0300

    [EC-980] Added iOS otpauth handler (#2370)
    * EC-980 added Bitwarden as otpauth scheme handler
    
    * EC-980 Fix format

---
+++ src/App/Services/DeepLinkContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Bit.App.Abstractions;
+using Bit.Core;
+
+namespace Bit.App.Services
+{
+    public class DeepLinkContext : IDeepLinkContext
+    {
+        private Uri _uri;
+
+        public Uri CurrentUri => _uri;
+
+        public bool HandleUri(Uri uri)
+        {
+            if (uri.Scheme == Constants.OtpAuthScheme)
+            {
+                _uri = uri;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            _uri = null;
+        }
+    }
+}
