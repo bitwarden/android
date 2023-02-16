09f42f97db9b4eb383b3be2bda46faff93d0862c
Jacob Fink <jfink@bitwarden.com>
2023-02-16 10:22:57 -0500

    [EC-1045] missed a const

--- src/Core/Services/VaultTimeoutService.cs
+++ src/Core/Services/VaultTimeoutService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Bit.Core.Abstractions;
@@ -247,7 +247,7 @@ namespace Bit.Core.Services
             {
                 var policy = (await _policyService.GetAll(PolicyType.MaximumVaultTimeout, userId)).First();
                 // Remove negative values, and ensure it's smaller than maximum allowed value according to policy
-                var policyTimeout = _policyService.GetPolicyInt(policy, "minutes");
+                var policyTimeout = _policyService.GetPolicyInt(policy, PolicyService.TIMEOUT_POLICY_MINUTES);
                 if (!policyTimeout.HasValue)
                 {
                     return vaultTimeout;
