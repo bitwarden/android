fe3c04b4269238a12f6ceaa6b46622a2b5dfd459
André Bispo <abispo@bitwarden.com>
2023-03-23 16:10:09 +0000

    [PM-1351] Add config cache for 10 mins during session or app restart.

--- src/Core/Services/ConfigService.cs
+++ src/Core/Services/ConfigService.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bit.Core.Abstractions;
 using Bit.Core.Models.Domain;
 using Bit.Core.Models.Response;
+using Bit.Core.Models.View;
 
 namespace Bit.Core.Services
 {
     public class ConfigService : IConfigService
     {
+        private ConfigResponse _configs;
+        private DateTime _lastUpdate;
+        private const int UPDATE_INTERVAL_MINS = 10;
         private readonly IApiService _apiService;
 
         public ConfigService(IApiService apiService)
@@ -17,7 +22,13 @@ namespace Bit.Core.Services
 
         public async Task<ConfigResponse> GetAllAsync()
         {
-            return await _apiService.GetAllConfigsAsync();
+            if(_configs == null || _lastUpdate == null || _lastUpdate.AddMinutes(UPDATE_INTERVAL_MINS) < DateTime.Now)
+            {
+                _configs = await _apiService.GetAllConfigsAsync();
+                _lastUpdate = DateTime.Now;
+            }
+
+            return _configs;
         }
     }
 }
