55d17ae1cafdf12a7a92afe4a901a0814adcd399
Daniel James Smith <djsmith@web.de>
2023-03-24 00:09:21 +0100

    Remove Mir because of https://github.com/bitwarden/clients/pull/5011

--- src/App/Pages/Vault/CipherAddEditPageViewModel.cs
+++ src/App/Pages/Vault/CipherAddEditPageViewModel.cs
@@ -122,7 +122,6 @@ namespace Bit.App.Pages
                 new KeyValuePair<string, string>("Maestro", "Maestro"),
                 new KeyValuePair<string, string>("UnionPay", "UnionPay"),
                 new KeyValuePair<string, string>("RuPay", "RuPay"),
-                new KeyValuePair<string, string>("Mir", "Mir"),
                 new KeyValuePair<string, string>(AppResources.Other, "Other")
             };
             CardExpMonthOptions = new List<KeyValuePair<string, string>>
