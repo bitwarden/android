6b71e7a61486fc4f2e9045b18c0057c74b5e3e5d
Federico Maccaroni <fedemkr@gmail.com>
2023-06-21 20:24:55 -0300

    PM-192 Refactor Forwarded email providers to use better patterns and code reuse.

--- src/App/Resources/AppResources.Designer.cs
+++ src/App/Resources/AppResources.Designer.cs
@@ -3263,6 +3263,24 @@ namespace Bit.App.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Invalid API key.
+        /// </summary>
+        public static string InvalidAPIKey {
+            get {
+                return ResourceManager.GetString("InvalidAPIKey", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid API token.
+        /// </summary>
+        public static string InvalidAPIToken {
+            get {
+                return ResourceManager.GetString("InvalidAPIToken", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Invalid email address..
         /// </summary>
         public static string InvalidEmail {
---
+++ src/Core/Services/EmailForwarders/BaseForwarder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Bit.Core.Abstractions;
+using Bit.Core.Exceptions;
+using Newtonsoft.Json.Linq;
+
+namespace Bit.Core.Services.EmailForwarders
+{
+    public abstract class BaseForwarder<T>
+        where T : ForwarderOptions
+    {
+        protected abstract string RequestUri { get; }
+
+        public async Task<string> GenerateAsync(IApiService apiService, T options)
+        {
+            if (!CanGenerate(options))
+            {
+                return Constants.DefaultUsernameGenerated;
+            }
+
+            using (var requestMessage = new HttpRequestMessage())
+            {
+                requestMessage.Version = new Version(1, 0);
+                requestMessage.Method = HttpMethod.Post;
+                requestMessage.RequestUri = new Uri(RequestUri);
+                requestMessage.Headers.Add("Accept", "application/json");
+
+                ConfigureHeaders(requestMessage.Headers, options);
+                requestMessage.Content = await GetContentAsync(apiService, options);
+
+                try
+                {
+                    var response = await apiService.SendAsync(requestMessage);
+
+                    var responseJsonString = await response.Content.ReadAsStringAsync();
+
+                    return HandleResponse(JObject.Parse(responseJsonString));
+                }
+                catch (ApiException ex)
+                {
+                    if (IsRequestSecretInvalid(ex))
+                    {
+                        throw new ForwardedEmailInvalidSecretException(ex);
+                    }
+
+                    throw;
+                }
+            }
+        }
+
+        protected virtual bool CanGenerate(T options) => !string.IsNullOrWhiteSpace(options.ApiKey);
+
+        protected abstract void ConfigureHeaders(HttpRequestHeaders headers, T options);
+
+        protected abstract Task<HttpContent> GetContentAsync(IApiService apiService, T options);
+
+        protected abstract string HandleResponse(JObject result);
+
+        protected virtual bool IsRequestSecretInvalid(ApiException ex) => ex.Error?.StatusCode == System.Net.HttpStatusCode.Unauthorized;
+    }
+}
