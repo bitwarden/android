72e67bd6f207466d5d64ebd4d21a9512272bb47e
ifernandezdiaz <ifernandez+github@bitwarden.com>
2023-06-29 15:37:08 -0300

    [PM-2691] Adding AutomationIDs for Vault Page sections (#2580)
    * Adding IDs for Vault Page sections
    
    * Removing extra spaces
    
    * Adding Matt's comments
    
    * Fixing Filters Id bug
    
    * Adding Fede's suggestions
    
    * Fixing Settings Ids issues
    
    * Fixing AutomationIds issues with RecyclerViews + implementing AutomationId helper class
    
    * Adding Fede's suggestion
    
    * Adding latest Fede's suggestions

---
+++ src/App/Utilities/Automation/AutomationIdsHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bit.App.Utilities.Automation
+{
+    public static class AutomationIdsHelper
+    {
+        public static string ToEnglishTitleCase(string name)
+        {
+            return new CultureInfo("en-US", false)
+                    .TextInfo
+                    .ToTitleCase(name)
+                    .Replace(" ", String.Empty)
+                    .Replace("-", String.Empty);
+        }
+
+        public static string AddSuffixFor(string text, SuffixType type)
+        {
+            return $"{text}{Enum.GetName(typeof(SuffixType), type)}";
+        }
+    }
+}
+
