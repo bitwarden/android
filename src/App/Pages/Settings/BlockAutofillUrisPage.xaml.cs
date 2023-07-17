e3982797947dd91dff17cbcfe3f67c9971e16232
Federico Maccaroni <fedemkr@gmail.com>
2023-07-17 14:41:38 -0300

    PM-2320 Improved autofill block uris placeholder colors on different themes

--- src/App/Pages/Settings/BlockAutofillUrisPage.xaml.cs
+++ src/App/Pages/Settings/BlockAutofillUrisPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using Bit.Core.Utilities;
+﻿using System.Threading.Tasks;
+using Bit.App.Styles;
+using Bit.App.Utilities;
+using Bit.Core.Utilities;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace Bit.App.Pages
 {
-    public partial class BlockAutofillUrisPage : BaseContentPage
+    public partial class BlockAutofillUrisPage : BaseContentPage, IThemeDirtablePage
     {
         private readonly BlockAutofillUrisPageViewModel _vm;
 
@@ -19,6 +24,21 @@ namespace Bit.App.Pages
             base.OnAppearing();
 
             _vm.InitAsync().FireAndForget(_ => Navigation.PopAsync());
+
+            UpdatePlaceholder();
+        }
+
+        public override async Task UpdateOnThemeChanged()
+        {
+            await base.UpdateOnThemeChanged();
+
+            UpdatePlaceholder();
+        }
+
+        private void UpdatePlaceholder()
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+                _emptyUrisPlaceholder.Source = ImageSource.FromFile(ThemeManager.UsingLightTheme ? "empty_uris_placeholder" : "empty_uris_placeholder_dark"));
         }
     }
 }
