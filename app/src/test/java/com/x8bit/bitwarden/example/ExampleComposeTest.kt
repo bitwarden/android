f67d4ccad35d610eef026fdb934c9271c7559780
Brian Yencho <brian@livefront.com>
2023-08-29 13:07:55 -0500

    Add BaseComposeTest for facilitating compose-layer testing (#14)

--- app/src/test/java/com/x8bit/bitwarden/example/ExampleComposeTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/example/ExampleComposeTest.kt
@@ -1,30 +1,17 @@
 package com.x8bit.bitwarden.example
 
 import androidx.compose.material3.Button
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
-import dagger.hilt.android.testing.HiltTestApplication
+import com.x8bit.bitwarden.example.ui.BaseComposeTest
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
-import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
 /**
- * Example showing that Compose tests using "junit" imports and Roboelectric work.
+ * Example showing that Compose tests using "junit" imports and Robolectric work.
  */
-@Config(
-    application = HiltTestApplication::class,
-    sdk = [Config.NEWEST_SDK],
-)
-@RunWith(RobolectricTestRunner::class)
-class ExampleComposeTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
+class ExampleComposeTest : BaseComposeTest() {
     @Test
     fun `the onClick callback should be correctly triggered when performing a click`() {
         var isClicked = false
