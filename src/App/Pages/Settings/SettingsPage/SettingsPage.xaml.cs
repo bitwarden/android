086159f177acbd5ee6bca73d82b1e72e57aab874
Federico Maccaroni <fedemkr@gmail.com>
2023-08-29 19:34:53 -0300

    PM-3693 Remove old Settings approach

--- src/App/Pages/Settings/SettingsPage/SettingsPage.xaml.cs
+++ src/App/Pages/Settings/SettingsPage/SettingsPage.xaml.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
-using Bit.App.Controls;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Bit.App.Pages
 {
     public partial class SettingsPage : BaseContentPage
     {
         private readonly TabsPage _tabsPage;
-        private SettingsPageViewModel _vm;
 
         public SettingsPage(TabsPage tabsPage)
         {
             _tabsPage = tabsPage;
             InitializeComponent();
-            _vm = BindingContext as SettingsPageViewModel;
-            _vm.Page = this;
+            var vm = BindingContext as SettingsPageViewModel;
+            vm.Page = this;
         }
 
         protected override bool OnBackButtonPressed()
@@ -28,70 +22,6 @@ namespace Bit.App.Pages
                 return true;
             }
             return base.OnBackButtonPressed();
-        }
-    }
-
-    public partial class SettingsPage2 : BaseContentPage
-    {
-        private readonly TabsPage _tabsPage;
-        private SettingsPageViewModel2 _vm;
-
-        public SettingsPage2(TabsPage tabsPage)
-        {
-            _tabsPage = tabsPage;
-            //InitializeComponent();
-            _vm = BindingContext as SettingsPageViewModel2;
-            _vm.Page = this;
-        }
-
-        public async Task InitAsync()
-        {
-            await _vm.InitAsync();
-        }
-
-        public void BuildList()
-        {
-            _vm.BuildList();
-        }
-
-        protected override bool OnBackButtonPressed()
-        {
-            if (Device.RuntimePlatform == Device.Android && _tabsPage != null)
-            {
-                _tabsPage.ResetToVaultPage();
-                return true;
-            }
-            return base.OnBackButtonPressed();
-        }
-
-        void ActivateTimePicker(object sender, EventArgs args)
-        {
-            var stackLayout = (ExtendedStackLayout)sender;
-            SettingsPageListItemOld item = (SettingsPageListItemOld)stackLayout.BindingContext;
-            if (item.ShowTimeInput)
-            {
-                var timePicker = stackLayout.Children.Where(x => x is TimePicker).FirstOrDefault();
-                ((TimePicker)timePicker)?.Focus();
-            }
-        }
-
-        async void OnTimePickerPropertyChanged(object sender, PropertyChangedEventArgs args)
-        {
-            var s = (TimePicker)sender;
-            var time = s.Time.TotalMinutes;
-            if (s.IsFocused && args.PropertyName == "Time")
-            {
-                await _vm.VaultTimeoutAsync(false, (int)time);
-            }
-        }
-
-        private void RowSelected(object sender, SelectionChangedEventArgs e)
-        {
-            ((ExtendedCollectionView)sender).SelectedItem = null;
-            if (e.CurrentSelection?.FirstOrDefault() is SettingsPageListItemOld item)
-            {
-                _vm?.ExecuteSettingItemCommand.Execute(item);
-            }
         }
     }
 }
--- src/App/Pages/Settings/SettingsPage/SettingsPageListItem.cs
+++ src/App/Pages/Settings/SettingsPage/SettingsPageListItem.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Bit.App.Resources;
-using Bit.App.Utilities;
 using Bit.App.Utilities.Automation;
-using Xamarin.Forms;
 
 namespace Bit.App.Pages
 {
     public class SettingsPageListItem
     {
-        private string _nameResourceKey;
+        private readonly string _nameResourceKey;
 
         public SettingsPageListItem(string nameResourceKey, Func<Task> executeAsync)
         {
@@ -26,49 +24,6 @@ namespace Bit.App.Pages
             get
             {
                 return AutomationIdsHelper.AddSuffixFor(AutomationIdsHelper.ToEnglishTitleCase(_nameResourceKey), SuffixType.Cell);
-            }
-        }
-    }
-
-    [Obsolete("Remove when Settings Reorganization is DONE")]
-    public class SettingsPageListItemOld : ISettingsPageListItem
-    {
-        public string Icon { get; set; }
-        public string Name { get; set; }
-        public string SubLabel { get; set; }
-        public TimeSpan? Time { get; set; }
-        public bool UseFrame { get; set; }
-        public Func<Task> ExecuteAsync { get; set; }
-
-        public bool SubLabelTextEnabled => SubLabel == AppResources.On;
-        public string LineBreakMode => SubLabel == null ? "TailTruncation" : "";
-        public bool ShowSubLabel => SubLabel.Length != 0;
-        public bool ShowTimeInput => Time != null;
-        public Color SubLabelColor => SubLabelTextEnabled ?
-            ThemeManager.GetResourceColor("SuccessColor") :
-            ThemeManager.GetResourceColor("MutedColor");
-
-        public string AutomationIdSettingName
-        {
-            get
-            {
-                return AutomationIdsHelper.AddSuffixFor(
-                    UseFrame ? "EnabledPolicy"
-                             : AutomationIdsHelper.ToEnglishTitleCase(Name)
-                    , SuffixType.Cell);
-            }
-        }
-
-        public string AutomationIdSettingStatus
-        {
-            get
-            {
-                if (UseFrame)
-                {
-                    return null;
-                }
-
-                return AutomationIdsHelper.AddSuffixFor(AutomationIdsHelper.ToEnglishTitleCase(Name), SuffixType.SettingValue);
             }
         }
     }
