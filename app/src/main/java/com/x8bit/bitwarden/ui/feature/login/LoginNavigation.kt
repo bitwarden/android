24c7dade1e36c15feaf8334138d3ff34ebe74d86
Caleb Derosier <125901828+caleb-livefront@users.noreply.github.com>
2023-08-31 08:41:56 -0500

    Add initial Landing screen & Login nav graph (#19)

---
+++ app/src/main/java/com/x8bit/bitwarden/ui/feature/login/LoginNavigation.kt
@@ -0,0 +1,40 @@
+package com.x8bit.bitwarden.ui.feature.login
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.navigation
+import com.x8bit.bitwarden.ui.feature.createaccount.createAccountDestinations
+import com.x8bit.bitwarden.ui.feature.createaccount.navigateToCreateAccount
+import com.x8bit.bitwarden.ui.feature.landing.LANDING_ROUTE
+import com.x8bit.bitwarden.ui.feature.landing.landingDestination
+
+const val LOGIN_ROUTE: String = "login"
+
+/**
+ * Add login destinations to the nav graph.
+ */
+fun NavGraphBuilder.loginDestinations(navController: NavHostController) {
+    navigation(
+        startDestination = LANDING_ROUTE,
+        route = LOGIN_ROUTE,
+    ) {
+        createAccountDestinations()
+        landingDestination(
+            onNavigateToCreateAccount = { navController.navigateToCreateAccount() },
+        )
+    }
+}
+
+/**
+ * Navigate to the login screen. Note this will only work if login destination was added
+ * via [loginDestinations].
+ */
+fun NavController.navigateToLoginAsRoot() {
+    navigate(LANDING_ROUTE) {
+        // When changing root navigation state, pop everything else off the back stack:
+        popUpTo(graph.id) {
+            inclusive = true
+        }
+    }
+}
