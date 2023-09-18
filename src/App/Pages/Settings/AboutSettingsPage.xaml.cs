64ffc7df974fbac9fcd5d470569ce8d6be5249a2
Federico Maccaroni <fedemkr@gmail.com>
2023-09-18 11:42:24 -0300

    PM-3839 Fix allow screen capture and submit crash logs to init their state when the page appears (#2760)

--- src/App/Pages/Settings/AboutSettingsPage.xaml.cs
+++ src/App/Pages/Settings/AboutSettingsPage.xaml.cs
@@ -1,12 +1,37 @@
-﻿namespace Bit.App.Pages
+﻿using System;
+using Bit.App.Resources;
+using Bit.Core.Abstractions;
+using Bit.Core.Services;
+using Bit.Core.Utilities;
+
+namespace Bit.App.Pages
 {
     public partial class AboutSettingsPage : BaseContentPage
     {
+        private readonly AboutSettingsPageViewModel _vm;
+
         public AboutSettingsPage()
         {
             InitializeComponent();
-            var vm = BindingContext as AboutSettingsPageViewModel;
-            vm.Page = this;
+            _vm = BindingContext as AboutSettingsPageViewModel;
+            _vm.Page = this;
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                await _vm.InitAsync();
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.LogEvenIfCantBeResolved(ex);
+                ServiceContainer.Resolve<IPlatformUtilsService>().ShowToast(null, null, AppResources.AnErrorHasOccurred);
+
+                Navigation.PopAsync().FireAndForget();
+            }
         }
     }
 }
