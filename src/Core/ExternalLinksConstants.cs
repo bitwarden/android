a4cbc2f283b8952302374ba92ee0d9642e5ffd18
Federico Maccaroni <fedemkr@gmail.com>
2023-09-22 11:55:50 -0300

    [PM-3834] Fix import items link (#2782)
    * PM-3834 Fix import items link
    
    * PM-3834 Fix import items link, removed old link.

--- src/Core/ExternalLinksConstants.cs
+++ src/Core/ExternalLinksConstants.cs
@@ -2,7 +2,6 @@
 {
     public static class ExternalLinksConstants
     {
-        public const string HELP_IMPORT_DATA = "https://bitwarden.com/help/import-data/";
         public const string HELP_CENTER = "https://bitwarden.com/help/";
         public const string HELP_ABOUT_ORGANIZATIONS = "https://bitwarden.com/help/about-organizations/";
         public const string HELP_FINGERPRINT_PHRASE = "https://bitwarden.com/help/fingerprint-phrase/";
