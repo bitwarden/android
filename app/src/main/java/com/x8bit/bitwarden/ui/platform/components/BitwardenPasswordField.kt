9d9ee380708325a3432b3d16b4eadc8633132e5e
Brian Yencho <brian@livefront.com>
2023-10-02 15:37:23 -0500

    Add BitwardenPasswordField (#82)

---
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenPasswordField.kt
@@ -0,0 +1,119 @@
+package com.x8bit.bitwarden.ui.platform.components
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import com.x8bit.bitwarden.R
+
+/**
+ * Represents a Bitwarden-styled password field that manages the state of a show/hide indicator
+ * internally.
+ *
+ * @param label Label for the text field.
+ * @param value Current next on the text field.
+ * @param onValueChange Callback that is triggered when the password changes.
+ * @param modifier Modifier for the composable.
+ * @param initialShowPassword The initial state of the show/hide password control. A value of
+ * `false` (the default) indicates that that password should begin in the hidden state.
+ */
+@Composable
+fun BitwardenPasswordField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    initialShowPassword: Boolean = false,
+) {
+    var showPassword by rememberSaveable { mutableStateOf(initialShowPassword) }
+    OutlinedTextField(
+        modifier = modifier,
+        textStyle = MaterialTheme.typography.bodyLarge,
+        label = { Text(text = label) },
+        value = value,
+        onValueChange = onValueChange,
+        visualTransformation = if (showPassword) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            IconButton(
+                onClick = { showPassword = !showPassword },
+            ) {
+                if (showPassword) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_visibility_off),
+                        contentDescription = stringResource(id = R.string.hide),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                } else {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_visibility),
+                        contentDescription = stringResource(id = R.string.show),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+            }
+        },
+    )
+}
+
+@Preview
+@Composable
+private fun BitwardenPasswordField_preview_withInput_hidePassword() {
+    BitwardenPasswordField(
+        label = "Label",
+        value = "Password",
+        onValueChange = {},
+        initialShowPassword = false,
+    )
+}
+
+@Preview
+@Composable
+private fun BitwardenPasswordField_preview_withInput_showPassword() {
+    BitwardenPasswordField(
+        label = "Label",
+        value = "Password",
+        onValueChange = {},
+        initialShowPassword = true,
+    )
+}
+
+@Preview
+@Composable
+private fun BitwardenPasswordField_preview_withoutInput_hidePassword() {
+    BitwardenPasswordField(
+        label = "Label",
+        value = "",
+        onValueChange = {},
+        initialShowPassword = false,
+    )
+}
+
+@Preview
+@Composable
+private fun BitwardenPasswordField_preview_withoutInput_showPassword() {
+    BitwardenPasswordField(
+        label = "Label",
+        value = "",
+        onValueChange = {},
+        initialShowPassword = true,
+    )
+}
