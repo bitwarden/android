a785ab0ba7b222aa406cb151154544cc37182873
Andrew Haisting <142518658+ahaisting-livefront@users.noreply.github.com>
2023-10-06 13:48:31 -0500

    BIT-753 Remove mention of "retrofit" from service level docs (#98)

--- app/src/main/java/com/x8bit/bitwarden/data/auth/datasource/network/service/AccountsService.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/auth/datasource/network/service/AccountsService.kt
@@ -3,7 +3,7 @@ package com.x8bit.bitwarden.data.auth.datasource.network.service
 import com.x8bit.bitwarden.data.auth.datasource.network.model.PreLoginResponseJson
 
 /**
- * Wraps raw retrofit accounts API in a cleaner interface.
+ * Provides an API for querying accounts endpoints.
  */
 interface AccountsService {
 
