bb9c260160ebe064b9e227cec120c82a6c79ef7f
Brian Yencho <brian@livefront.com>
2023-10-12 11:22:52 -0500

    Update version name to 1.0.0 (#108)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
