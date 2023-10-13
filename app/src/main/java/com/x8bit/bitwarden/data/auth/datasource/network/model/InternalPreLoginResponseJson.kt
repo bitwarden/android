5ecb8fbb2cd012d999dfe421b0bc94c1d5dc0c80
Andrew Haisting <142518658+ahaisting-livefront@users.noreply.github.com>
2023-10-13 14:05:55 -0500

    BIT-329 Implement parsing and usage of kdf params (#112)

---
+++ app/src/main/java/com/x8bit/bitwarden/data/auth/datasource/network/model/InternalPreLoginResponseJson.kt
@@ -0,0 +1,21 @@
+package com.x8bit.bitwarden.data.auth.datasource.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Response body for pre login. This internal model is only used for as a surrogate serializer.
+ *
+ * See [PreLoginResponseJson] for exposed model.
+ */
+@Serializable
+data class InternalPreLoginResponseJson(
+    @SerialName("kdf")
+    val kdfType: Int,
+    @SerialName("kdfIterations")
+    val kdfIterations: UInt,
+    @SerialName("kdfMemory")
+    val kdfMemory: UInt? = null,
+    @SerialName("kdfParallelism")
+    val kdfParallelism: UInt? = null,
+)
