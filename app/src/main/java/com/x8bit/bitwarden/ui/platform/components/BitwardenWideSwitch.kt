69009c3d6f23b0c4564db47bd625287020faaa45
joshua-livefront <139182194+joshua-livefront@users.noreply.github.com>
2023-10-17 10:27:06 -0400

    BIT-870: Updating the BitwardenWideSwitch ripple (#119)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenWideSwitch.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenWideSwitch.kt
@@ -1,14 +1,18 @@
 package com.x8bit.bitwarden.ui.platform.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
@@ -34,10 +38,16 @@ fun BitwardenWideSwitch(
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .semantics(mergeDescendants = true) { }
+        modifier = Modifier
             .fillMaxWidth()
-            .wrapContentHeight(),
+            .wrapContentHeight()
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(color = MaterialTheme.colorScheme.primary),
+                onClick = { onCheckedChange?.invoke(!isChecked) },
+            )
+            .semantics(mergeDescendants = true) { }
+            .then(modifier),
     ) {
         Text(
             text = label,
@@ -49,7 +59,7 @@ fun BitwardenWideSwitch(
             modifier = Modifier
                 .height(56.dp),
             checked = isChecked,
-            onCheckedChange = onCheckedChange,
+            onCheckedChange = null,
         )
     }
 }
