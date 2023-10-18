77f693e15993501a84f09019ec547afd41d16b61
Andrew Haisting <142518658+ahaisting-livefront@users.noreply.github.com>
2023-10-18 13:29:48 -0500

    BIT-868 Keep password fields on create account in sync (#125)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenPasswordField.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenPasswordField.kt
@@ -21,6 +21,62 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.x8bit.bitwarden.R
 
 /**
+ * Represents a Bitwarden-styled password field that hoists show/hide password state to the caller.
+ *
+ * See overloaded [BitwardenPasswordField] for self managed show/hide state.
+ *
+ * @param label Label for the text field.
+ * @param value Current next on the text field.
+ * @param showPassword Whether or not password should be shown.
+ * @param showPasswordChange Lambda that is called when user request show/hide be toggled.
+ * @param onValueChange Callback that is triggered when the password changes.
+ * @param modifier Modifier for the composable.
+ */
+@Composable
+fun BitwardenPasswordField(
+    label: String,
+    value: String,
+    showPassword: Boolean,
+    showPasswordChange: (Boolean) -> Unit,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        textStyle = MaterialTheme.typography.bodyLarge,
+        label = { Text(text = label) },
+        value = value,
+        onValueChange = onValueChange,
+        visualTransformation = if (showPassword) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            IconButton(
+                onClick = { showPasswordChange.invoke(!showPassword) },
+            ) {
+                if (showPassword) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_visibility_off),
+                        contentDescription = stringResource(id = R.string.hide),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                } else {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_visibility),
+                        contentDescription = stringResource(id = R.string.show),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+            }
+        },
+    )
+}
+
+/**
  * Represents a Bitwarden-styled password field that manages the state of a show/hide indicator
  * internally.
  *
@@ -40,38 +96,13 @@ fun BitwardenPasswordField(
     initialShowPassword: Boolean = false,
 ) {
     var showPassword by rememberSaveable { mutableStateOf(initialShowPassword) }
-    OutlinedTextField(
+    BitwardenPasswordField(
         modifier = modifier,
-        textStyle = MaterialTheme.typography.bodyLarge,
-        label = { Text(text = label) },
+        label = label,
         value = value,
+        showPassword = showPassword,
+        showPasswordChange = { showPassword = !showPassword },
         onValueChange = onValueChange,
-        visualTransformation = if (showPassword) {
-            VisualTransformation.None
-        } else {
-            PasswordVisualTransformation()
-        },
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        trailingIcon = {
-            IconButton(
-                onClick = { showPassword = !showPassword },
-            ) {
-                if (showPassword) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_visibility_off),
-                        contentDescription = stringResource(id = R.string.hide),
-                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                    )
-                } else {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_visibility),
-                        contentDescription = stringResource(id = R.string.show),
-                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                    )
-                }
-            }
-        },
     )
 }
 
