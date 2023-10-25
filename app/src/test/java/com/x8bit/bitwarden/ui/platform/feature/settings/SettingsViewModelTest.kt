62fe0ea90182f253e3dc06a2c8c3dd7f17ee3258
Brian Yencho <brian@livefront.com>
2023-10-25 09:59:49 -0500

    BIT-150: Add more comprehensive list of settings rows. (#155)
    Co-authored-by: David Perez <david@livefront.com>

--- app/src/test/java/com/x8bit/bitwarden/ui/platform/feature/settings/SettingsViewModelTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/platform/feature/settings/SettingsViewModelTest.kt
@@ -9,11 +9,56 @@ import org.junit.jupiter.api.Test
 class SettingsViewModelTest : BaseViewModelTest() {
 
     @Test
-    fun `on AccountSecurityClick should emit NavigateAccountSecurity`() = runTest {
+    fun `on SettingsClick with ABOUT should emit nothing`() = runTest {
         val viewModel = SettingsViewModel()
         viewModel.eventFlow.test {
-            viewModel.trySendAction(SettingsAction.AccountSecurityClick)
+            viewModel.trySendAction(SettingsAction.SettingsClick(Settings.ABOUT))
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `on SettingsClick with ACCOUNT_SECURITY should emit NavigateAccountSecurity`() = runTest {
+        val viewModel = SettingsViewModel()
+        viewModel.eventFlow.test {
+            viewModel.trySendAction(SettingsAction.SettingsClick(Settings.ACCOUNT_SECURITY))
             assertEquals(SettingsEvent.NavigateAccountSecurity, awaitItem())
+        }
+    }
+
+    @Test
+    fun `on SettingsClick with APPEARANCE should emit nothing`() = runTest {
+        val viewModel = SettingsViewModel()
+        viewModel.eventFlow.test {
+            viewModel.trySendAction(SettingsAction.SettingsClick(Settings.APPEARANCE))
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `on SettingsClick with AUTO_FILL should emit nothing`() = runTest {
+        val viewModel = SettingsViewModel()
+        viewModel.eventFlow.test {
+            viewModel.trySendAction(SettingsAction.SettingsClick(Settings.AUTO_FILL))
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `on SettingsClick with OTHER should emit nothing`() = runTest {
+        val viewModel = SettingsViewModel()
+        viewModel.eventFlow.test {
+            viewModel.trySendAction(SettingsAction.SettingsClick(Settings.OTHER))
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `on SettingsClick with VAULT should emit nothing`() = runTest {
+        val viewModel = SettingsViewModel()
+        viewModel.eventFlow.test {
+            viewModel.trySendAction(SettingsAction.SettingsClick(Settings.VAULT))
+            expectNoEvents()
         }
     }
 }
