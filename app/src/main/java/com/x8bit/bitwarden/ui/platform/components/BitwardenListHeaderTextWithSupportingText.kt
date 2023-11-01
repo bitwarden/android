e06e36b1beeb2d39692de326f6faff5ee8f472dd
joshua-livefront <139182194+joshua-livefront@users.noreply.github.com>
2023-11-01 16:36:09 -0400

    BIT-534: Bitwarden shared composables and icons for my vault list screen (#192)

---
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenListHeaderTextWithSupportingText.kt
@@ -0,0 +1,59 @@
+package com.x8bit.bitwarden.ui.platform.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
+
+/**
+ * Represents a Bitwarden-styled label text.
+ *
+ * @param label The text content for the label.
+ * @param supportingLabel The text for the supporting label.
+ * @param modifier The [Modifier] to be applied to the label.
+ */
+@Composable
+fun BitwardenListHeaderTextWithSupportLabel(
+    label: String,
+    supportingLabel: String,
+    modifier: Modifier,
+) {
+    Row(
+        modifier = modifier
+            .padding(
+                top = 12.dp,
+                bottom = 4.dp,
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+
+        Text(
+            text = supportingLabel,
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BitwardenListHeaderTextWithSupportLabel_preview() {
+    BitwardenTheme {
+        BitwardenListHeaderTextWithSupportLabel(
+            label = "Sample Label",
+            supportingLabel = "0",
+            modifier = Modifier,
+        )
+    }
+}
