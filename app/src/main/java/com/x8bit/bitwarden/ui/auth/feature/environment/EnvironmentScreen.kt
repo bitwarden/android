e066c885a9397275cb93f1e6991f9dd7f2e66ab1
David Perez <david@livefront.com>
2023-11-06 11:17:35 -0600

    Handle window insets on the Environment screen. (#211)

--- app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/environment/EnvironmentScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/environment/EnvironmentScreen.kt
@@ -4,15 +4,21 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -60,6 +66,9 @@ fun EnvironmentScreen(
         modifier = Modifier
             .fillMaxSize()
             .nestedScroll(scrollBehavior.nestedScrollConnection),
+        contentWindowInsets = ScaffoldDefaults
+            .contentWindowInsets
+            .exclude(WindowInsets.navigationBars),
         topBar = {
             BitwardenTopAppBar(
                 title = stringResource(id = R.string.settings),
@@ -85,6 +94,7 @@ fun EnvironmentScreen(
                 .padding(innerPadding)
                 .fillMaxSize()
                 .background(color = MaterialTheme.colorScheme.surface)
+                .imePadding()
                 .verticalScroll(rememberScrollState()),
         ) {
             BitwardenListHeaderText(
@@ -175,6 +185,8 @@ fun EnvironmentScreen(
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp),
             )
+
+            Spacer(modifier = Modifier.navigationBarsPadding())
         }
     }
 }
