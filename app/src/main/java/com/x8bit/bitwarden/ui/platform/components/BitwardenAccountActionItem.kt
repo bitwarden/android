eb0e04c8248446b82306ce1e6ac7fcb0d95f49f6
David Perez <david@livefront.com>
2023-11-08 11:59:24 -0600

    Fix minor TalkBack issues in VaultScreen (#225)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenAccountActionItem.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenAccountActionItem.kt
@@ -1,11 +1,9 @@
 package com.x8bit.bitwarden.ui.platform.components
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -36,16 +34,12 @@ fun BitwardenAccountActionItem(
     val iconPainter = painterResource(id = R.drawable.ic_account_initials_container)
     val contentDescription = stringResource(id = R.string.account)
 
-    Box(
-        contentAlignment = Alignment.Center,
-    ) {
-        IconButton(onClick = onClick) {
-            Icon(
-                painter = iconPainter,
-                contentDescription = contentDescription,
-                tint = color,
-            )
-        }
+    IconButton(onClick = onClick) {
+        Icon(
+            painter = iconPainter,
+            contentDescription = contentDescription,
+            tint = color,
+        )
         Text(
             text = initials,
             style = TextStyle(
--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenListHeaderTextWithSupportingText.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenListHeaderTextWithSupportingText.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
@@ -29,7 +30,8 @@ fun BitwardenListHeaderTextWithSupportLabel(
             .padding(
                 top = 12.dp,
                 bottom = 4.dp,
-            ),
+            )
+            .semantics(mergeDescendants = true) { },
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Text(
