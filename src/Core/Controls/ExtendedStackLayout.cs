27306fe3533411e5c10b6633d61c56d71255ed16
Dinis Vieira <dinisvieira@outlook.com>
2023-11-13 09:59:54 +0000

    PM-3349 PM-3350 Added the partial MAUI Community Toolkit implementation for TouchEffect. This is a temporary solution until they finalize this and add it to their nuget package. This allows implementing the LongPressCommand in AccountSwitchingOverlay and also have the "Ripple effect" animation when touching an item in Android

--- src/Core/Controls/ExtendedStackLayout.cs
+++ src/Core/Controls/ExtendedStackLayout.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.Maui.Controls;
-using Microsoft.Maui;
+﻿using CommunityToolkit.Maui.Behaviors;
 
 namespace Bit.App.Controls
 {
     public class ExtendedStackLayout : StackLayout
     {
+        public ExtendedStackLayout()
+        {
+            // Add Android Ripple effect. Eventually we should be able to replace this with the Maui Community Toolkit implementation. (https://github.com/CommunityToolkit/Maui/issues/86)
+            // [MAUI-Migration] When this TouchBehavior is replaced we can delete the existing TouchBehavior support files (which is all the files and folders inside "Core.Behaviors.PlatformBehaviors.MCTTouch.*")
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                var touchBehavior = new TouchBehavior()
+                {
+                    NativeAnimation = true
+                };
+                Behaviors.Add(touchBehavior);
+            }
+        }
     }
 }
