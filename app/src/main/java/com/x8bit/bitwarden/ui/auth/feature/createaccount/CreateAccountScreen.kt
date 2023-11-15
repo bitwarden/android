3e6ce662d8b26b15527fe9a321666dcc8a03f9cf
David Perez <david@livefront.com>
2023-11-15 08:47:19 -0600

    Add IME padding to create account screen (#248)

--- app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/createaccount/CreateAccountScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/createaccount/CreateAccountScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -193,6 +194,7 @@ fun CreateAccountScreen(
             modifier = Modifier
                 .semantics { testTagsAsResourceId = true }
                 .padding(innerPadding)
+                .imePadding()
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState()),
         ) {
