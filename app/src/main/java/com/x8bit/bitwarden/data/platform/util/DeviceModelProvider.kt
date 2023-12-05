f8fefae3b81f6889f4063166a3a34c0737fda5c6
Brian Yencho <brian@livefront.com>
2023-12-04 18:40:37 -0600

    Add OmitFromCoverage annotation to exclude special cases (#318)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/util/DeviceModelProvider.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/util/DeviceModelProvider.kt
@@ -1,10 +1,12 @@
 package com.x8bit.bitwarden.data.platform.util
 
 import android.os.Build
+import com.x8bit.bitwarden.data.platform.annotation.OmitFromCoverage
 
 /**
- * Provides device model string. Useful for mocking static [Build.model] call tests.
+ * Provides device model string. Useful for mocking static [Build.MODEL] call tests.
  */
+@OmitFromCoverage
 class DeviceModelProvider {
 
     /**
