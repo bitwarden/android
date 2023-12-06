ba62eaf6d09f5fca0b9479dd4c3dc2525d26d7a6
David Perez <david@livefront.com>
2023-12-06 16:46:04 -0600

    Add helper method for applying visual transformations to any string (#342)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/base/util/StringExtensions.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/base/util/StringExtensions.kt
@@ -1,9 +1,11 @@
 package com.x8bit.bitwarden.ui.platform.base.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.core.graphics.toColorInt
 import java.net.URI
 import java.util.Locale
@@ -64,6 +66,18 @@ fun String?.orNullIfBlank(): String? = this?.takeUnless { it.isBlank() }
 fun String.lowercaseWithCurrentLocal(): String {
     return lowercase(LocalContext.current.resources.configuration.locales[0])
 }
+
+/**
+ * A helper method to apply the [visualTransformation] to the given string and and returns the
+ * transformed [AnnotatedString].
+ */
+@Composable
+fun String.withVisualTransformation(
+    visualTransformation: VisualTransformation,
+): AnnotatedString =
+    remember(key1 = this) {
+        visualTransformation.filter(toAnnotatedString()).text
+    }
 
 /**
  * Returns the [String] as an [AnnotatedString].
