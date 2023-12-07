446b0f9da473a5f9ed879f0c16d738d67f4088ee
David Perez <david@livefront.com>
2023-12-07 09:17:22 -0600

    Migrate VaultAddItemScreen to use a LazyColumn (#345)

---
+++ app/src/main/java/com/x8bit/bitwarden/ui/vault/feature/additem/AddEditSecureNotesItems.kt
@@ -0,0 +1,170 @@
+package com.x8bit.bitwarden.ui.vault.feature.additem
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.x8bit.bitwarden.R
+import com.x8bit.bitwarden.ui.platform.components.BitwardenFilledTonalButton
+import com.x8bit.bitwarden.ui.platform.components.BitwardenListHeaderText
+import com.x8bit.bitwarden.ui.platform.components.BitwardenMultiSelectButton
+import com.x8bit.bitwarden.ui.platform.components.BitwardenSwitch
+import com.x8bit.bitwarden.ui.platform.components.BitwardenSwitchWithActions
+import com.x8bit.bitwarden.ui.platform.components.BitwardenTextField
+import kotlinx.collections.immutable.toImmutableList
+
+/**
+ * The UI for adding and editing a secure notes cipher.
+ */
+@Suppress("LongMethod")
+fun LazyListScope.addEditSecureNotesItems(
+    state: VaultAddItemState.ItemType.SecureNotes,
+    secureNotesTypeHandlers: VaultAddSecureNotesItemTypeHandlers,
+) {
+    item {
+        Spacer(modifier = Modifier.height(8.dp))
+        BitwardenTextField(
+            label = stringResource(id = R.string.name),
+            value = state.name,
+            onValueChange = secureNotesTypeHandlers.onNameTextChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(24.dp))
+        BitwardenListHeaderText(
+            label = stringResource(id = R.string.miscellaneous),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(8.dp))
+        BitwardenMultiSelectButton(
+            label = stringResource(id = R.string.folder),
+            options = state.availableFolders.map { it.invoke() }.toImmutableList(),
+            selectedOption = state.folderName.invoke(),
+            onOptionSelected = secureNotesTypeHandlers.onFolderTextChange,
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(16.dp))
+        BitwardenSwitch(
+            label = stringResource(id = R.string.favorite),
+            isChecked = state.favorite,
+            onCheckedChange = secureNotesTypeHandlers.onToggleFavorite,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(16.dp))
+        BitwardenSwitchWithActions(
+            label = stringResource(id = R.string.password_prompt),
+            isChecked = state.masterPasswordReprompt,
+            onCheckedChange = secureNotesTypeHandlers.onToggleMasterPasswordReprompt,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            actions = {
+                IconButton(onClick = secureNotesTypeHandlers.onTooltipClick) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_tooltip),
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        contentDescription = stringResource(
+                            id = R.string.master_password_re_prompt_help,
+                        ),
+                    )
+                }
+            },
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(24.dp))
+        BitwardenListHeaderText(
+            label = stringResource(id = R.string.notes),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(8.dp))
+        BitwardenTextField(
+            singleLine = false,
+            label = stringResource(id = R.string.notes),
+            value = state.notes,
+            onValueChange = secureNotesTypeHandlers.onNotesTextChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(24.dp))
+        BitwardenListHeaderText(
+            label = stringResource(id = R.string.custom_fields),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(16.dp))
+        BitwardenFilledTonalButton(
+            label = stringResource(id = R.string.new_custom_field),
+            onClick = secureNotesTypeHandlers.onAddNewCustomFieldClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(24.dp))
+        BitwardenListHeaderText(
+            label = stringResource(id = R.string.ownership),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(8.dp))
+        BitwardenMultiSelectButton(
+            label = stringResource(id = R.string.who_owns_this_item),
+            options = state.availableOwners.toImmutableList(),
+            selectedOption = state.ownership,
+            onOptionSelected = secureNotesTypeHandlers.onOwnershipTextChange,
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+        )
+    }
+
+    item {
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
