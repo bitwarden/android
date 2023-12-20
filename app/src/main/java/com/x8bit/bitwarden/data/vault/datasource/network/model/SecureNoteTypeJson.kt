b2692a563786f8abef6002b8b563b9eac6438ff2
Brian Yencho <brian@livefront.com>
2023-12-20 17:15:21 -0600

    Ensure BaseEnumeratedIntSerializer subclasses are not obfuscated (#428)

--- app/src/main/java/com/x8bit/bitwarden/data/vault/datasource/network/model/SecureNoteTypeJson.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/vault/datasource/network/model/SecureNoteTypeJson.kt
@@ -1,5 +1,6 @@
 package com.x8bit.bitwarden.data.vault.datasource.network.model
 
+import androidx.annotation.Keep
 import com.x8bit.bitwarden.data.platform.datasource.network.serializer.BaseEnumeratedIntSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -16,5 +17,6 @@ enum class SecureNoteTypeJson {
     GENERIC,
 }
 
+@Keep
 private class SecureNoteTypeSerializer :
     BaseEnumeratedIntSerializer<SecureNoteTypeJson>(SecureNoteTypeJson.values())
