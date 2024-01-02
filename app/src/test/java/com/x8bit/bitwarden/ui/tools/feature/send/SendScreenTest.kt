7a8da67944372c0332b2d72aacbc81f2ab4d5601
David Perez <david@livefront.com>
2024-01-02 13:34:35 -0600

    Add the overflow menu to the send screen (#469)

--- app/src/test/java/com/x8bit/bitwarden/ui/tools/feature/send/SendScreenTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/tools/feature/send/SendScreenTest.kt
@@ -1,13 +1,19 @@
 package com.x8bit.bitwarden.ui.tools.feature.send
 
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.hasAnyAncestor
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.isPopup
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import com.x8bit.bitwarden.data.platform.repository.util.bufferedMutableSharedFlow
 import com.x8bit.bitwarden.ui.platform.base.BaseComposeTest
+import com.x8bit.bitwarden.ui.platform.base.util.IntentHandler
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
 import org.junit.Test
@@ -15,9 +21,9 @@ import org.junit.Test
 class SendScreenTest : BaseComposeTest() {
 
     private var onNavigateToNewSendCalled = false
-    private val mutableEventFlow = MutableSharedFlow<SendEvent>(
-        extraBufferCapacity = Int.MAX_VALUE,
-    )
+
+    private val intentHandler = mockk<IntentHandler>()
+    private val mutableEventFlow = bufferedMutableSharedFlow<SendEvent>()
     private val mutableStateFlow = MutableStateFlow(DEFAULT_STATE)
     private val viewModel = mockk<SendViewModel>(relaxed = true) {
         every { eventFlow } returns mutableEventFlow
@@ -30,7 +36,76 @@ class SendScreenTest : BaseComposeTest() {
             SendScreen(
                 viewModel = viewModel,
                 onNavigateNewSend = { onNavigateToNewSendCalled = true },
+                intentHandler = intentHandler,
             )
+        }
+    }
+
+    @Test
+    fun `on overflow item click should display menu`() {
+        composeTestRule
+            .onNodeWithContentDescription(label = "More")
+            .performClick()
+
+        composeTestRule
+            .onAllNodesWithText(text = "Sync")
+            .filterToOne(hasAnyAncestor(isPopup()))
+            .isDisplayed()
+        composeTestRule
+            .onAllNodesWithText(text = "Lock")
+            .filterToOne(hasAnyAncestor(isPopup()))
+            .isDisplayed()
+        composeTestRule
+            .onAllNodesWithText(text = "About Send")
+            .filterToOne(hasAnyAncestor(isPopup()))
+            .isDisplayed()
+    }
+
+    @Test
+    fun `on sync click should send SyncClick`() {
+        composeTestRule
+            .onNodeWithContentDescription(label = "More")
+            .performClick()
+
+        composeTestRule
+            .onAllNodesWithText(text = "Sync")
+            .filterToOne(hasAnyAncestor(isPopup()))
+            .performClick()
+
+        verify {
+            viewModel.trySendAction(SendAction.SyncClick)
+        }
+    }
+
+    @Test
+    fun `on lock click should send LockClick`() {
+        composeTestRule
+            .onNodeWithContentDescription(label = "More")
+            .performClick()
+
+        composeTestRule
+            .onAllNodesWithText(text = "Lock")
+            .filterToOne(hasAnyAncestor(isPopup()))
+            .performClick()
+
+        verify {
+            viewModel.trySendAction(SendAction.LockClick)
+        }
+    }
+
+    @Test
+    fun `on about send click should send AboutSendClick`() {
+        composeTestRule
+            .onNodeWithContentDescription(label = "More")
+            .performClick()
+
+        composeTestRule
+            .onAllNodesWithText(text = "About Send")
+            .filterToOne(hasAnyAncestor(isPopup()))
+            .performClick()
+
+        verify {
+            viewModel.trySendAction(SendAction.AboutSendClick)
         }
     }
 
--- app/src/test/java/com/x8bit/bitwarden/ui/tools/feature/send/SendViewModelTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/tools/feature/send/SendViewModelTest.kt
@@ -2,32 +2,47 @@ package com.x8bit.bitwarden.ui.tools.feature.send
 
 import androidx.lifecycle.SavedStateHandle
 import app.cash.turbine.test
+import com.x8bit.bitwarden.data.vault.repository.VaultRepository
 import com.x8bit.bitwarden.ui.platform.base.BaseViewModelTest
 import com.x8bit.bitwarden.ui.platform.base.util.asText
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SendViewModelTest : BaseViewModelTest() {
 
+    private val vaultRepo: VaultRepository = mockk()
+
     @Test
     fun `initial state should be Empty`() {
-        val viewModel = SendViewModel(SavedStateHandle())
+        val viewModel = createViewModel()
         assertEquals(SendState.Empty, viewModel.stateFlow.value)
     }
 
     @Test
     fun `initial state should read from saved state when present`() {
         val savedState = mockk<SendState>()
-        val handle = SavedStateHandle(mapOf("state" to savedState))
-        val viewModel = SendViewModel(handle)
+        val viewModel = createViewModel(state = savedState)
         assertEquals(savedState, viewModel.stateFlow.value)
     }
 
     @Test
+    fun `AboutSendClick should emit NavigateToAboutSend`() = runTest {
+        val viewModel = createViewModel()
+        viewModel.eventFlow.test {
+            viewModel.trySendAction(SendAction.AboutSendClick)
+            assertEquals(SendEvent.NavigateToAboutSend, awaitItem())
+        }
+    }
+
+    @Test
     fun `AddSendClick should emit NavigateNewSend`() = runTest {
-        val viewModel = SendViewModel(SavedStateHandle())
+        val viewModel = createViewModel()
         viewModel.eventFlow.test {
             viewModel.trySendAction(SendAction.AddSendClick)
             assertEquals(SendEvent.NavigateNewSend, awaitItem())
@@ -35,11 +50,45 @@ class SendViewModelTest : BaseViewModelTest() {
     }
 
     @Test
+    fun `LockClick should lock the vault`() {
+        val viewModel = createViewModel()
+        every { vaultRepo.lockVaultForCurrentUser() } just runs
+
+        viewModel.trySendAction(SendAction.LockClick)
+
+        verify {
+            vaultRepo.lockVaultForCurrentUser()
+        }
+    }
+
+    @Test
     fun `SearchClick should emit ShowToast`() = runTest {
-        val viewModel = SendViewModel(SavedStateHandle())
+        val viewModel = createViewModel()
         viewModel.eventFlow.test {
             viewModel.trySendAction(SendAction.SearchClick)
             assertEquals(SendEvent.ShowToast("Search Not Implemented".asText()), awaitItem())
         }
     }
+
+    @Test
+    fun `SyncClick should call sync`() {
+        val viewModel = createViewModel()
+        every { vaultRepo.sync() } just runs
+
+        viewModel.trySendAction(SendAction.SyncClick)
+
+        verify {
+            vaultRepo.sync()
+        }
+    }
+
+    private fun createViewModel(
+        state: SendState? = null,
+        vaultRepository: VaultRepository = vaultRepo,
+    ): SendViewModel = SendViewModel(
+        savedStateHandle = SavedStateHandle().apply {
+            set("state", state)
+        },
+        vaultRepo = vaultRepository,
+    )
 }
