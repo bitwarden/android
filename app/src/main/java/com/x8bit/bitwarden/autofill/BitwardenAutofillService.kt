738cd76c7df8fe820582a7b87ccb41e61bca3af5
Lucas Kivi <125697099+lucas-livefront@users.noreply.github.com>
2024-01-02 13:39:50 -0600

    BIT-1293: add autofill service shell (#468)

---
+++ app/src/main/java/com/x8bit/bitwarden/autofill/BitwardenAutofillService.kt
@@ -0,0 +1,31 @@
+package com.x8bit.bitwarden.autofill
+
+import android.os.CancellationSignal
+import android.service.autofill.AutofillService
+import android.service.autofill.FillCallback
+import android.service.autofill.FillRequest
+import android.service.autofill.SaveCallback
+import android.service.autofill.SaveRequest
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * The [AutofillService] implementation for the app. This fulfills autofill requests from other
+ * applications.
+ */
+@AndroidEntryPoint
+class BitwardenAutofillService : AutofillService() {
+    override fun onFillRequest(
+        request: FillRequest,
+        cancellationSignal: CancellationSignal,
+        fillCallback: FillCallback,
+    ) {
+        // TODO: parse request and perform dummy autofill (BIT-1314)
+    }
+
+    override fun onSaveRequest(
+        saverRequest: SaveRequest,
+        saveCallback: SaveCallback,
+    ) {
+        // TODO: add save request behavior (BIT-1299)
+    }
+}
---
+++ app/src/test/java/com/x8bit/bitwarden/autofill/BitwardenAutofillServiceTests.kt
@@ -0,0 +1,54 @@
+package com.x8bit.bitwarden.autofill
+
+import android.os.CancellationSignal
+import android.service.autofill.FillCallback
+import android.service.autofill.FillRequest
+import android.service.autofill.SaveCallback
+import android.service.autofill.SaveRequest
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class BitwardenAutofillServiceTests {
+    private lateinit var bitwardenAutofillService: BitwardenAutofillService
+
+    @BeforeEach
+    fun setup() {
+        bitwardenAutofillService = BitwardenAutofillService()
+    }
+
+    @Nested
+    inner class OnFillRequest {
+        @Test
+        fun `nothing happens`() {
+            // Setup
+            val cancellationSignal: CancellationSignal = mockk()
+            val fillCallback: FillCallback = mockk()
+            val fillRequest: FillRequest = mockk()
+
+            // Test
+            bitwardenAutofillService.onFillRequest(
+                cancellationSignal = cancellationSignal,
+                fillCallback = fillCallback,
+                request = fillRequest,
+            )
+        }
+    }
+
+    @Nested
+    inner class OnSaveRequest {
+        @Test
+        fun `nothing happens`() {
+            // Setup
+            val saverRequest: SaveRequest = mockk()
+            val saveCallback: SaveCallback = mockk()
+
+            // Test
+            bitwardenAutofillService.onSaveRequest(
+                saveCallback = saveCallback,
+                saverRequest = saverRequest,
+            )
+        }
+    }
+}
