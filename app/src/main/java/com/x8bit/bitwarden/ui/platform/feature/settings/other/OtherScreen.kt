b24c2ba7e74ee4cb06896d8444ae4c58f5aecb0f
Caleb Derosier <125901828+caleb-livefront@users.noreply.github.com>
2024-01-04 19:24:12 -0700

    BIT-930: Add UI for Other screen (#477)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/other/OtherScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/other/OtherScreen.kt
@@ -1,34 +1,55 @@
 package com.x8bit.bitwarden.ui.platform.feature.settings.other
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.x8bit.bitwarden.R
 import com.x8bit.bitwarden.ui.platform.base.util.EventsEffect
+import com.x8bit.bitwarden.ui.platform.components.BitwardenFilledTonalButton
 import com.x8bit.bitwarden.ui.platform.components.BitwardenScaffold
+import com.x8bit.bitwarden.ui.platform.components.BitwardenSelectionDialog
+import com.x8bit.bitwarden.ui.platform.components.BitwardenSelectionRow
+import com.x8bit.bitwarden.ui.platform.components.BitwardenTextRow
 import com.x8bit.bitwarden.ui.platform.components.BitwardenTopAppBar
+import com.x8bit.bitwarden.ui.platform.components.BitwardenTwoButtonDialog
+import com.x8bit.bitwarden.ui.platform.components.BitwardenWideSwitch
 
 /**
  * Displays the other screen.
  */
+@Suppress("LongMethod")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OtherScreen(
     onNavigateBack: () -> Unit,
     viewModel: OtherViewModel = hiltViewModel(),
 ) {
+    val state by viewModel.stateFlow.collectAsState()
     EventsEffect(viewModel = viewModel) { event ->
         when (event) {
             OtherEvent.NavigateBack -> onNavigateBack.invoke()
@@ -58,7 +79,148 @@ fun OtherScreen(
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState()),
         ) {
-            // TODO: BIT-930 Display Other UI
+            BitwardenWideSwitch(
+                label = stringResource(id = R.string.enable_sync_on_refresh),
+                description = stringResource(id = R.string.enable_sync_on_refresh_description),
+                isChecked = state.allowSyncOnRefresh,
+                onCheckedChange = remember(viewModel) {
+                    { viewModel.trySendAction(OtherAction.AllowSyncToggle(it)) }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            BitwardenFilledTonalButton(
+                onClick = remember(viewModel) {
+                    { viewModel.trySendAction(OtherAction.SyncNowButtonClick) }
+                },
+                label = stringResource(id = R.string.sync_now),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.last_sync),
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(start = 16.dp, end = 2.dp),
+                )
+                Text(
+                    text = state.lastSyncTime,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            ClearClipboardFrequencyRow(
+                currentSelection = state.clearClipboardFrequency,
+                onFrequencySelection = remember(viewModel) {
+                    { viewModel.trySendAction(OtherAction.ClearClipboardFrequencyChange(it)) }
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            ScreenCaptureRow(
+                currentValue = state.allowScreenCapture,
+                onValueChange = remember(viewModel) {
+                    { viewModel.trySendAction(OtherAction.AllowScreenCaptureToggle(it)) }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            )
+        }
+    }
+}
+
+@Composable
+private fun ScreenCaptureRow(
+    currentValue: Boolean,
+    onValueChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var shouldShowScreenCaptureConfirmDialog by remember { mutableStateOf(false) }
+
+    BitwardenWideSwitch(
+        label = stringResource(id = R.string.allow_screen_capture),
+        isChecked = currentValue,
+        onCheckedChange = {
+            if (currentValue) {
+                onValueChange(false)
+            } else {
+                shouldShowScreenCaptureConfirmDialog = true
+            }
+        },
+        modifier = modifier,
+    )
+
+    if (shouldShowScreenCaptureConfirmDialog) {
+        BitwardenTwoButtonDialog(
+            title = stringResource(R.string.allow_screen_capture),
+            message = stringResource(R.string.are_you_sure_you_want_to_enable_screen_capture),
+            confirmButtonText = stringResource(R.string.yes),
+            dismissButtonText = stringResource(id = R.string.cancel),
+            onConfirmClick = {
+                onValueChange(true)
+                shouldShowScreenCaptureConfirmDialog = false
+            },
+            onDismissClick = { shouldShowScreenCaptureConfirmDialog = false },
+            onDismissRequest = { shouldShowScreenCaptureConfirmDialog = false },
+        )
+    }
+}
+
+@Composable
+private fun ClearClipboardFrequencyRow(
+    currentSelection: OtherState.ClearClipboardFrequency,
+    onFrequencySelection: (OtherState.ClearClipboardFrequency) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var shouldShowClearClipboardDialog by remember { mutableStateOf(false) }
+
+    BitwardenTextRow(
+        text = stringResource(id = R.string.clear_clipboard),
+        description = stringResource(id = R.string.clear_clipboard_description),
+        onClick = { shouldShowClearClipboardDialog = true },
+        modifier = modifier,
+    ) {
+        Text(
+            text = currentSelection.text(),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+    }
+
+    if (shouldShowClearClipboardDialog) {
+        BitwardenSelectionDialog(
+            title = stringResource(id = R.string.clear_clipboard),
+            onDismissRequest = { shouldShowClearClipboardDialog = false },
+        ) {
+            OtherState.ClearClipboardFrequency.entries.forEach { option ->
+                BitwardenSelectionRow(
+                    text = option.text,
+                    isSelected = option == currentSelection,
+                    onClick = {
+                        shouldShowClearClipboardDialog = false
+                        onFrequencySelection(
+                            OtherState.ClearClipboardFrequency.entries.first { it == option },
+                        )
+                    },
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/other/OtherViewModel.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/other/OtherViewModel.kt
@@ -1,18 +1,95 @@
 package com.x8bit.bitwarden.ui.platform.feature.settings.other
 
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import com.x8bit.bitwarden.R
+import com.x8bit.bitwarden.data.vault.repository.VaultRepository
 import com.x8bit.bitwarden.ui.platform.base.BaseViewModel
+import com.x8bit.bitwarden.ui.platform.base.util.Text
+import com.x8bit.bitwarden.ui.platform.base.util.asText
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
+
+private const val KEY_STATE = "state"
 
 /**
  * View model for the other screen.
  */
 @HiltViewModel
-class OtherViewModel @Inject constructor() : BaseViewModel<Unit, OtherEvent, OtherAction>(
-    initialState = Unit,
+class OtherViewModel @Inject constructor(
+    private val vaultRepo: VaultRepository,
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<OtherState, OtherEvent, OtherAction>(
+    initialState = savedStateHandle[KEY_STATE]
+        ?: OtherState(
+            allowScreenCapture = false,
+            allowSyncOnRefresh = false,
+            clearClipboardFrequency = OtherState.ClearClipboardFrequency.DEFAULT,
+            lastSyncTime = "5/14/2023 4:52 PM",
+        ),
 ) {
     override fun handleAction(action: OtherAction): Unit = when (action) {
-        OtherAction.BackClick -> sendEvent(OtherEvent.NavigateBack)
+        is OtherAction.AllowScreenCaptureToggle -> handleAllowScreenCaptureToggled(action)
+        is OtherAction.AllowSyncToggle -> handleAllowSyncToggled(action)
+        OtherAction.BackClick -> handleBackClicked()
+        is OtherAction.ClearClipboardFrequencyChange -> handleClearClipboardFrequencyChanged(action)
+        OtherAction.SyncNowButtonClick -> handleSyncNowButtonClicked()
+    }
+
+    private fun handleAllowScreenCaptureToggled(action: OtherAction.AllowScreenCaptureToggle) {
+        // TODO BIT-805 implement screen capture setting
+        mutableStateFlow.update { it.copy(allowScreenCapture = action.isScreenCaptureEnabled) }
+    }
+
+    private fun handleAllowSyncToggled(action: OtherAction.AllowSyncToggle) {
+        // TODO BIT-461 hook up to pull-to-refresh feature
+        mutableStateFlow.update { it.copy(allowSyncOnRefresh = action.isSyncEnabled) }
+    }
+
+    private fun handleBackClicked() {
+        sendEvent(OtherEvent.NavigateBack)
+    }
+
+    private fun handleClearClipboardFrequencyChanged(
+        action: OtherAction.ClearClipboardFrequencyChange,
+    ) {
+        // TODO BIT-1283 implement clear clipboard setting
+        mutableStateFlow.update {
+            it.copy(
+                clearClipboardFrequency = action.clearClipboardFrequency,
+            )
+        }
+    }
+
+    private fun handleSyncNowButtonClicked() {
+        // TODO BIT-1282 add full support and visual feedback
+        vaultRepo.sync()
+    }
+}
+
+/**
+ * Models the state of the Other screen.
+ */
+@Parcelize
+data class OtherState(
+    val allowScreenCapture: Boolean,
+    val allowSyncOnRefresh: Boolean,
+    val clearClipboardFrequency: ClearClipboardFrequency,
+    val lastSyncTime: String,
+) : Parcelable {
+    /**
+     * Represents the different frequencies with which the user clipboard can be cleared.
+     */
+    enum class ClearClipboardFrequency(val text: Text) {
+        DEFAULT(text = R.string.never.asText()),
+        TEN_SECONDS(text = R.string.ten_seconds.asText()),
+        TWENTY_SECONDS(text = R.string.twenty_seconds.asText()),
+        THIRTY_SECONDS(text = R.string.thirty_seconds.asText()),
+        ONE_MINUTE(text = R.string.one_minute.asText()),
+        TWO_MINUTES(text = R.string.two_minutes.asText()),
+        FIVE_MINUTES(text = R.string.five_minutes.asText()),
     }
 }
 
@@ -31,7 +108,33 @@ sealed class OtherEvent {
  */
 sealed class OtherAction {
     /**
+     * Indicates that the user toggled the Allow screen capture switch to [isScreenCaptureEnabled].
+     */
+    data class AllowScreenCaptureToggle(
+        val isScreenCaptureEnabled: Boolean,
+    ) : OtherAction()
+
+    /**
+     * Indicates that the user toggled the Allow sync on refresh switch to [isSyncEnabled].
+     */
+    data class AllowSyncToggle(
+        val isSyncEnabled: Boolean,
+    ) : OtherAction()
+
+    /**
      * User clicked back button.
      */
     data object BackClick : OtherAction()
+
+    /**
+     * Indicates that the user changed the clear clipboard frequency.
+     */
+    data class ClearClipboardFrequencyChange(
+        val clearClipboardFrequency: OtherState.ClearClipboardFrequency,
+    ) : OtherAction()
+
+    /**
+     * Indicates that the user clicked the Sync Now button.
+     */
+    data object SyncNowButtonClick : OtherAction()
 }
