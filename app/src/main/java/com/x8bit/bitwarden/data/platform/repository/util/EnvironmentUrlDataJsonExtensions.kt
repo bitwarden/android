0a3377d98a3c8ee4a9cff11e8abcc1040b7b1490
Brian Yencho <brian@livefront.com>
2024-01-05 18:36:57 -0600

    Consolidate Environment(UrlDataJson)Extensions files in data layer (#509)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/repository/util/EnvironmentUrlDataJsonExtensions.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/repository/util/EnvironmentUrlDataJsonExtensions.kt
@@ -22,3 +22,20 @@ val EnvironmentUrlDataJson.labelOrBaseUrlHost: String
                 .orEmpty()
         }
     }
+
+/**
+ * Converts a raw [EnvironmentUrlDataJson] to an externally-consumable [Environment].
+ */
+fun EnvironmentUrlDataJson.toEnvironmentUrls(): Environment =
+    when (this) {
+        Environment.Us.environmentUrlData -> Environment.Us
+        Environment.Eu.environmentUrlData -> Environment.Eu
+        else -> Environment.SelfHosted(environmentUrlData = this)
+    }
+
+/**
+ * Converts a nullable [EnvironmentUrlDataJson] to an [Environment], where `null` values default to
+ * the US environment.
+ */
+fun EnvironmentUrlDataJson?.toEnvironmentUrlsOrDefault(): Environment =
+    this?.toEnvironmentUrls() ?: Environment.Us
