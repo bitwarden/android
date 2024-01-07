64709590851dbec37847ca6c07ec402316b378ca
Brian Yencho <brian@livefront.com>
2024-01-07 13:31:04 -0600

    Add VaultTimeoutAction handling to SettingsRepository (#521)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/repository/SettingsRepository.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/repository/SettingsRepository.kt
@@ -1,6 +1,7 @@
 package com.x8bit.bitwarden.data.platform.repository
 
 import com.x8bit.bitwarden.data.platform.repository.model.VaultTimeout
+import com.x8bit.bitwarden.data.platform.repository.model.VaultTimeoutAction
 import kotlinx.coroutines.flow.StateFlow
 
 /**
@@ -16,4 +17,23 @@ interface SettingsRepository {
      * Stores the given [vaultTimeout] for the given [userId].
      */
     fun storeVaultTimeout(userId: String, vaultTimeout: VaultTimeout)
+
+    /**
+     * Gets updates for the [VaultTimeoutAction] associated with the given [userId].
+     *
+     * Note that in cases where no value has been set, a default will be returned. Use
+     * [isVaultTimeoutActionSet] to see if there is an actual value set.
+     */
+    fun getVaultTimeoutActionStateFlow(userId: String): StateFlow<VaultTimeoutAction>
+
+    /**
+     * Returns `true` if a [VaultTimeoutAction] is set for the given [userId], and `false`
+     * otherwise.
+     */
+    fun isVaultTimeoutActionSet(userId: String): Boolean
+
+    /**
+     * Stores the given [VaultTimeoutAction] for the given [userId].
+     */
+    fun storeVaultTimeoutAction(userId: String, vaultTimeoutAction: VaultTimeoutAction?)
 }
