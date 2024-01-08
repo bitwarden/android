d2225ef5f822e3bcafd12f5447a19ce238930645
André Bispo <abispo@bitwarden.com>
2024-01-08 13:47:03 +0000

    [PM-5633] Ignore ArgumentOutOfRangeException to collect more data about the crash

--- src/iOS.Core/Renderers/CollectionView/ExtendedGroupableItemsViewController.cs
+++ src/iOS.Core/Renderers/CollectionView/ExtendedGroupableItemsViewController.cs
@@ -38,10 +38,11 @@ namespace Bit.iOS.Core.Renderers.CollectionView
                     // Do nothing in here, this is temporary to get more info about the crash, if the logger fails, we want to get the info
                     // by crashing with the original exception and not the logger one
                 }
-                if (ex is IndexOutOfRangeException)
+                if (ex is IndexOutOfRangeException || ex is ArgumentOutOfRangeException)
                 {
                     return;
                 }
+
                 throw colEx;
             }
         }
