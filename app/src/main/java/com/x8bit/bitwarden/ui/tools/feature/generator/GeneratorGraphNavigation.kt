82b174168ec588d78ce8ac071be7b6a942256673
David Perez <david@livefront.com>
2024-01-15 08:47:31 -0600

    Create a navigation graph for the generator tab (#617)

---
+++ app/src/main/java/com/x8bit/bitwarden/ui/tools/feature/generator/GeneratorGraphNavigation.kt
@@ -0,0 +1,31 @@
+package com.x8bit.bitwarden.ui.tools.feature.generator
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.navigation
+
+private const val GENERATOR_GRAPH_ROUTE: String = "generator_graph"
+
+/**
+ * Add generator destination to the root nav graph.
+ */
+fun NavGraphBuilder.generatorGraph(
+    onNavigateToPasswordHistory: () -> Unit,
+) {
+    navigation(
+        route = GENERATOR_GRAPH_ROUTE,
+        startDestination = GENERATOR_ROUTE,
+    ) {
+        generatorDestination(
+            onNavigateToPasswordHistory = onNavigateToPasswordHistory,
+        )
+    }
+}
+
+/**
+ * Navigate to the generator graph.
+ */
+fun NavController.navigateToGeneratorGraph(navOptions: NavOptions? = null) {
+    navigate(GENERATOR_GRAPH_ROUTE, navOptions)
+}
