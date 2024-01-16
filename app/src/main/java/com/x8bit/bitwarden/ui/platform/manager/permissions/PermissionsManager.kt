87ea0da23fdc01e115377efc26bd4f3f65f97166
David Perez <david@livefront.com>
2024-01-16 09:54:35 -0600

    Add method to request multiple permissions at once (#633)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/manager/permissions/PermissionsManager.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/manager/permissions/PermissionsManager.kt
@@ -17,9 +17,24 @@ interface PermissionsManager {
     fun getLauncher(onResult: (Boolean) -> Unit): ManagedActivityResultLauncher<String, Boolean>
 
     /**
+     * Method for creating and returning a permissions launcher that can request multiple
+     * permissions at once.
+     */
+    @Composable
+    fun getPermissionsLauncher(
+        onResult: (Map<String, Boolean>) -> Unit,
+    ): ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>
+
+    /**
      * Method for checking whether the permission is granted.
      */
     fun checkPermission(permission: String): Boolean
+
+    /**
+     * Method for checking whether the permissions are granted. This returns `true` only if all
+     * permissions have been granted, `false` otherwise.
+     */
+    fun checkPermissions(permissions: Array<String>): Boolean
 
     /**
      * Method for checking if an informative UI should be shown the user.
