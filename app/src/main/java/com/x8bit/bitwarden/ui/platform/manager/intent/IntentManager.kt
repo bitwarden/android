cb9e8b469b10a23838e52826cf6090ea822062a3
David Perez <david@livefront.com>
2024-01-27 21:30:51 -0600

    Interfaces provided via Local Composition should be immutable (#822)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/manager/intent/IntentManager.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/manager/intent/IntentManager.kt
@@ -6,12 +6,14 @@ import android.os.Parcelable
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.result.ActivityResult
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import kotlinx.parcelize.Parcelize
 
 /**
  * A manager class for simplifying the handling of Android Intents within a given context.
  */
 @Suppress("TooManyFunctions")
+@Immutable
 interface IntentManager {
     /**
      * Start an activity using the provided [Intent].
