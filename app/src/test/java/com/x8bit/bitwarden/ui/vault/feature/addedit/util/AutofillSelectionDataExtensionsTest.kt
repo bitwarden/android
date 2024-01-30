085fa0153d7a862d99bad7a1f2f2235fbb1c9753
Brian Yencho <brian@livefront.com>
2024-01-29 23:27:46 -0600

    Fix rebasing issue with Login URIs (#866)

--- app/src/test/java/com/x8bit/bitwarden/ui/vault/feature/addedit/util/AutofillSelectionDataExtensionsTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/vault/feature/addedit/util/AutofillSelectionDataExtensionsTest.kt
@@ -2,10 +2,26 @@ package com.x8bit.bitwarden.ui.vault.feature.addedit.util
 
 import com.x8bit.bitwarden.data.autofill.model.AutofillSelectionData
 import com.x8bit.bitwarden.ui.vault.feature.addedit.VaultAddEditState
+import com.x8bit.bitwarden.ui.vault.feature.addedit.model.UriItem
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.UUID
 
 class AutofillSelectionDataExtensionsTest {
+    @BeforeEach
+    fun setUp() {
+        mockkStatic(UUID::randomUUID)
+    }
+
+    @BeforeEach
+    fun tearDown() {
+        unmockkStatic(UUID::randomUUID)
+    }
+
     @Test
     fun `toDefaultAddTypeContent for a Card type should return the correct Content`() {
         assertEquals(
@@ -23,13 +39,20 @@ class AutofillSelectionDataExtensionsTest {
 
     @Test
     fun `toDefaultAddTypeContent for a Login type should return the correct Content`() {
+        every { UUID.randomUUID().toString() } returns "uuid"
         assertEquals(
             VaultAddEditState.ViewState.Content(
                 common = VaultAddEditState.ViewState.Content.Common(
                     name = "www.test.com",
                 ),
                 type = VaultAddEditState.ViewState.Content.ItemType.Login(
-                    uri = "https://www.test.com",
+                    uriList = listOf(
+                        UriItem(
+                            id = "uuid",
+                            uri = "https://www.test.com",
+                            match = null,
+                        ),
+                    ),
                 ),
             ),
             AutofillSelectionData(
