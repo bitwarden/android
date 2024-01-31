2127dcbb1d1703ae652c91211ab3e63af66f28fd
Oleg Semenenko <146032743+oleg-livefront@users.noreply.github.com>
2024-01-31 14:26:59 -0600

    BIT-1570 Make the name value appear in the dialog (#907)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenTextEntryDialog.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenTextEntryDialog.kt
@@ -4,10 +4,15 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
 import com.x8bit.bitwarden.R
 
@@ -18,6 +23,8 @@ import com.x8bit.bitwarden.R
  * @param textFieldLabel Label for the text field.
  * @param onConfirmClick Called when the confirm button is clicked.
  * @param onDismissRequest Called when the user attempts to dismiss the dialog.
+ * @param autoFocus When set to true, the view will request focus after the first recomposition.
+ * @param initialText The text that will be visible at the start of text entry.
  */
 @Composable
 fun BitwardenTextEntryDialog(
@@ -25,8 +32,12 @@ fun BitwardenTextEntryDialog(
     textFieldLabel: String,
     onConfirmClick: (String) -> Unit,
     onDismissRequest: () -> Unit,
+    autoFocus: Boolean = false,
+    initialText: String? = null,
 ) {
-    var text by remember { mutableStateOf("") }
+    var text by remember { mutableStateOf(initialText.orEmpty()) }
+    val focusRequester = remember { FocusRequester() }
+    var shouldRequestFocus by remember { mutableStateOf(false) }
 
     AlertDialog(
         onDismissRequest = onDismissRequest,
@@ -55,8 +66,19 @@ fun BitwardenTextEntryDialog(
                 label = textFieldLabel,
                 value = text,
                 onValueChange = { text = it },
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .onGloballyPositioned {
+                        shouldRequestFocus = true
+                    },
             )
         },
         containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
     )
+
+    if (autoFocus && shouldRequestFocus) {
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
+    }
 }
