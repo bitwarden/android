9d7e762c5dbc073ffe6e29ba5ec4456f2ab1ca16
David Perez <david@livefront.com>
2024-02-05 16:56:21 -0600

    Remove the usage of the deprecated AlertDialog (#959)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenLoadingDialog.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/BitwardenLoadingDialog.kt
@@ -1,17 +1,15 @@
 package com.x8bit.bitwarden.ui.platform.components
 
 import android.os.Parcelable
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +17,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.x8bit.bitwarden.ui.platform.base.util.Text
 import com.x8bit.bitwarden.ui.platform.base.util.asText
 import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
@@ -29,7 +29,6 @@ import kotlinx.parcelize.Parcelize
  *
  * @param visibilityState the [LoadingDialogState] used to populate the dialog.
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BitwardenLoadingDialog(
     visibilityState: LoadingDialogState,
@@ -37,19 +36,24 @@ fun BitwardenLoadingDialog(
     when (visibilityState) {
         is LoadingDialogState.Hidden -> Unit
         is LoadingDialogState.Shown -> {
-            AlertDialog(
+            Dialog(
                 onDismissRequest = {},
+                properties = DialogProperties(
+                    dismissOnBackPress = false,
+                    dismissOnClickOutside = false,
+                ),
             ) {
                 Card(
                     shape = RoundedCornerShape(28.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                    ),
                     modifier = Modifier
                         .fillMaxWidth()
                         .wrapContentHeight(),
                 ) {
                     Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(MaterialTheme.colorScheme.surfaceContainerHigh),
+                        modifier = Modifier.fillMaxWidth(),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
