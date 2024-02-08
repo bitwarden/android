a7b58e9c198e5e233a40a39a68571e33e64f7c11
David Perez <david@livefront.com>
2024-02-08 17:10:25 -0600

    BIT-1729: Update generator policy UI in realtime (#990)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/manager/util/PolicyManagerExtensions.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/manager/util/PolicyManagerExtensions.kt
@@ -4,12 +4,33 @@ import com.x8bit.bitwarden.data.auth.repository.model.PolicyInformation
 import com.x8bit.bitwarden.data.auth.repository.util.policyInformation
 import com.x8bit.bitwarden.data.platform.manager.PolicyManager
 import com.x8bit.bitwarden.data.vault.datasource.network.model.PolicyTypeJson
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 /**
  * Get a list of active policies with the data decoded to the specified type.
  */
-inline fun <reified T : PolicyInformation> PolicyManager.getActivePolicies(): List<T> {
-    val type = when (T::class.java) {
+inline fun <reified T : PolicyInformation> PolicyManager.getActivePolicies(): List<T> =
+    this
+        .getActivePolicies(type = getPolicyTypeJson<T>())
+        .mapNotNull { it.policyInformation as? T }
+
+/**
+ * Gets a flow of all the active policies with the data decoded to the specified type.
+ */
+inline fun <reified T : PolicyInformation> PolicyManager.getActivePoliciesFlow(): Flow<List<T>> =
+    this
+        .getActivePoliciesFlow(type = getPolicyTypeJson<T>())
+        .map { policies ->
+            policies.mapNotNull { policy -> policy.policyInformation as? T }
+        }
+
+/**
+ * Helper method for mapping a specific [PolicyInformation] type to its [PolicyTypeJson]
+ * counterpart.
+ */
+inline fun <reified T : PolicyInformation> getPolicyTypeJson(): PolicyTypeJson =
+    when (T::class.java) {
         PolicyInformation.MasterPassword::class.java -> PolicyTypeJson.MASTER_PASSWORD
         PolicyInformation.PasswordGenerator::class.java -> PolicyTypeJson.PASSWORD_GENERATOR
         PolicyInformation.SendOptions::class.java -> PolicyTypeJson.SEND_OPTIONS
@@ -18,11 +39,7 @@ inline fun <reified T : PolicyInformation> PolicyManager.getActivePolicies(): Li
         else -> {
             throw IllegalStateException(
                 "Looks like you are missing a branch in your when statement. Update " +
-                    "getActivePolicies() to handle all PolicyInformation implementations.",
+                    "getPolicyTypeJson() to handle all PolicyInformation implementations.",
             )
         }
     }
-    return this
-        .getActivePolicies(type = type)
-        .mapNotNull { it.policyInformation as? T }
-}
