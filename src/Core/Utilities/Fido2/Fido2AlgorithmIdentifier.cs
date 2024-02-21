527aa4a9b46cd33c0e36e24fb7f8ab832153a39c
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-02-21 11:47:02 +0100

    [PM-5731] fix: formatting

--- src/Core/Utilities/Fido2/Fido2AlgorithmIdentifier.cs
+++ src/Core/Utilities/Fido2/Fido2AlgorithmIdentifier.cs
@@ -1,5 +1,7 @@
-namespace Bit.Core.Utilities.Fido2 {
-    public enum Fido2AlgorithmIdentifier : int {
+namespace Bit.Core.Utilities.Fido2 
+{
+    public enum Fido2AlgorithmIdentifier : int
+    {
         ES256 = -7,
         RS256 = -257,
     }
