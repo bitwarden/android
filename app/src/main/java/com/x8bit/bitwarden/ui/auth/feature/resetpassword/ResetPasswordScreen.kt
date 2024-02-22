951d64a6524fb4121ecefe105f9744d68e34cbf9
David Perez <david@livefront.com>
2024-02-22 12:41:34 -0600

    BIT-1943: ResetPasswordScreen was missing navigation bar padding (#1052)

--- app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/resetpassword/ResetPasswordScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/resetpassword/ResetPasswordScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -241,6 +242,6 @@ private fun ResetPasswordScreeContent(
                 .padding(horizontal = 16.dp),
         )
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.navigationBarsPadding())
     }
 }
