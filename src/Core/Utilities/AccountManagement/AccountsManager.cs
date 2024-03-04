304e583978eb05fb55d6b9490808688a28d9c6a9
Federico Maccaroni <fedemkr@gmail.com>
2024-03-04 10:07:34 -0300

    PM-6539 Remove unnecessary using

--- src/Core/Utilities/AccountManagement/AccountsManager.cs
+++ src/Core/Utilities/AccountManagement/AccountsManager.cs
@@ -4,7 +4,6 @@ using Bit.Core.Abstractions;
 using Bit.Core.Enums;
 using Bit.Core.Models.Domain;
 using Bit.Core.Resources.Localization;
-using Bit.Core.Services;
 using Bit.Core.Utilities;
 
 namespace Bit.App.Utilities.AccountManagement
