00d61323b978015ed409ee856fc277c80aaed800
David Perez <david@livefront.com>
2024-03-12 16:49:37 -0500

    BIT-1990: Add test tags for all rows in setting UI (#1129)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/SettingsScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/SettingsScreen.kt
@@ -26,6 +26,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -88,6 +90,7 @@ fun SettingsScreen(
                         { viewModel.trySendAction(SettingsAction.SettingsClick(it)) }
                     },
                     modifier = Modifier
+                        .semantics { testTag = it.testTag }
                         .padding(horizontal = 16.dp)
                         .fillMaxWidth(),
                 )
