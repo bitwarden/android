9241cd640b1c4ce954f46e7b08a0084c2d957b50
Ramsey Smith <142836716+ramsey-livefront@users.noreply.github.com>
2024-03-19 09:17:34 -0700

    BIT-1955: Autofill Brings User to the Incorrect Page on Pre Android 11 (#1157)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/util/IntentExtensions.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/util/IntentExtensions.kt
@@ -4,6 +4,7 @@ package com.x8bit.bitwarden.data.platform.util
 
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.os.Parcelable
 import com.x8bit.bitwarden.data.platform.annotation.OmitFromCoverage
 
@@ -20,4 +21,19 @@ inline fun <reified T> Intent.getSafeParcelableExtra(name: String): T? =
     } else {
         @Suppress("DEPRECATION")
         getParcelableExtra(name)
+    }
+
+/**
+ * A means of retrieving a [Parcelable] from a [Bundle] using the given [name] in a manner that
+ * is safe across SDK versions.
+ */
+inline fun <reified T> Bundle.getSafeParcelableExtra(name: String): T? =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelable(
+            name,
+            T::class.java,
+        )
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelable(name)
     }
