ffa268e0f8a00d263478aae9d0236fa6b741b2bb
Caleb Derosier <125901828+caleb-livefront@users.noreply.github.com>
2024-03-28 16:34:47 -0600

    BIT-2159: Update .json (Encrypted) text to .json (Password Protected) (#1182)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/util/ExportVaultFormatExtensions.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/util/ExportVaultFormatExtensions.kt
@@ -1,15 +1,20 @@
 package com.x8bit.bitwarden.ui.platform.util
 
+import com.x8bit.bitwarden.R
+import com.x8bit.bitwarden.ui.platform.base.util.Text
+import com.x8bit.bitwarden.ui.platform.base.util.asText
 import com.x8bit.bitwarden.ui.platform.feature.settings.exportvault.model.ExportVaultFormat
 
 /**
  *  Provides a human-readable label for the export format.
  */
-val ExportVaultFormat.displayLabel: String
+val ExportVaultFormat.displayLabel: Text
     get() = when (this) {
-        ExportVaultFormat.JSON -> ".json"
-        ExportVaultFormat.CSV -> ".csv"
-        ExportVaultFormat.JSON_ENCRYPTED -> ".json (Encrypted)"
+        ExportVaultFormat.JSON -> R.string.json_extension.asText()
+        ExportVaultFormat.CSV -> R.string.csv_extension.asText()
+        ExportVaultFormat.JSON_ENCRYPTED -> R.string.json_extension_formatted.asText(
+            R.string.password_protected.asText(),
+        )
     }
 
 /**
--- app/src/test/java/com/x8bit/bitwarden/ui/platform/util/ExportVaultFormatExtensionTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/platform/util/ExportVaultFormatExtensionTest.kt
@@ -1,5 +1,7 @@
 package com.x8bit.bitwarden.ui.platform.util
 
+import com.x8bit.bitwarden.R
+import com.x8bit.bitwarden.ui.platform.base.util.asText
 import com.x8bit.bitwarden.ui.platform.feature.settings.exportvault.model.ExportVaultFormat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -8,9 +10,11 @@ class ExportVaultFormatExtensionTest {
     @Test
     fun `displayLabel should return the correct value for each type`() {
         mapOf(
-            ExportVaultFormat.JSON to ".json",
-            ExportVaultFormat.CSV to ".csv",
-            ExportVaultFormat.JSON_ENCRYPTED to ".json (Encrypted)",
+            ExportVaultFormat.JSON to R.string.json_extension.asText(),
+            ExportVaultFormat.CSV to R.string.csv_extension.asText(),
+            ExportVaultFormat.JSON_ENCRYPTED to R.string.json_extension_formatted.asText(
+                R.string.password_protected.asText(),
+            ),
         )
             .forEach { (type, label) ->
                 assertEquals(
