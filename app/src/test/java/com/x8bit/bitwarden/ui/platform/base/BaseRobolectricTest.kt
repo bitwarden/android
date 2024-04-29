9a6ac3fca121c022c158a130b158123bf05126c1
David Perez <david@livefront.com>
2024-04-29 11:46:49 -0500

    Apply the RobolectricTestRunner to the BaseRobolectricTest (#1316)

--- app/src/test/java/com/x8bit/bitwarden/ui/platform/base/BaseRobolectricTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/platform/base/BaseRobolectricTest.kt
@@ -1,8 +1,8 @@
 package com.x8bit.bitwarden.ui.platform.base
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltTestApplication
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowLog
 
@@ -13,7 +13,7 @@ import org.robolectric.shadows.ShadowLog
     application = HiltTestApplication::class,
     sdk = [Config.NEWEST_SDK],
 )
-@RunWith(AndroidJUnit4::class)
+@RunWith(RobolectricTestRunner::class)
 abstract class BaseRobolectricTest {
     init {
         ShadowLog.stream = System.out
