5d5c5809d12f44b517d7b08980d4060779f0592f
David Perez <david@livefront.com>
2024-05-28 17:55:58 -0500

    Add suppression for unstable API (#1398)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ val userProperties = Properties().apply {
     }
 }
 
-
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
