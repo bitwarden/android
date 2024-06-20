d97be3aceb261edb23aa06e3b19198d2c42a10f8
Matt Bishop <mbishop@bitwarden.com>
2024-06-20 14:16:26 -0400

    Additional scanning

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.kotlinx.kover) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.sonarqube) apply false
 }
