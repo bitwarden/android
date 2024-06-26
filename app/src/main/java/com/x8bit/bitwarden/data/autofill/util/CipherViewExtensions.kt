5a49024670d5c165691a539904cf5c81bf66d42e
Patrick Honkonen <phonkonen@bitwarden.com>
2024-06-26 12:43:18 -0400

    Document ext function

--- app/src/main/java/com/x8bit/bitwarden/data/autofill/util/CipherViewExtensions.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/autofill/util/CipherViewExtensions.kt
@@ -46,6 +46,9 @@ fun CipherView.toAutofillCipherProvider(): AutofillCipherProvider =
         }
     }
 
+/**
+ * Returns true when the cipher is not deleted and contains at least one FIDO 2 credential.
+ */
 val CipherView.isActiveWithFido2Credentials: Boolean
     get() = deletedDate == null &&
         type == CipherType.LOGIN &&
