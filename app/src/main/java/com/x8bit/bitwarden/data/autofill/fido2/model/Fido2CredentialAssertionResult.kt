c6cdc0ca7fa0d6e8a18a2ad8c4a45066b2b33c57
Patrick Honkonen <phonkonen@bitwarden.com>
2024-07-23 15:30:43 -0400

    Complete FIDO 2 assertion with appropriate response

---
+++ app/src/main/java/com/x8bit/bitwarden/data/autofill/fido2/model/Fido2CredentialAssertionResult.kt
@@ -0,0 +1,22 @@
+package com.x8bit.bitwarden.data.autofill.fido2.model
+
+/**
+ * Represents possible outcomes of a FIDO 2 credential assertion request.
+ */
+sealed class Fido2CredentialAssertionResult {
+
+    /**
+     * Indicates the assertion request completed and [responseJson] was successfully generated.
+     */
+    data class Success(val responseJson: String) : Fido2CredentialAssertionResult()
+
+    /**
+     * Indicates there was an error and the assertion was not successful.
+     */
+    data object Error : Fido2CredentialAssertionResult()
+
+    /**
+     * Indicates assertion was cancelled by the user.
+     */
+    data object Cancelled : Fido2CredentialAssertionResult()
+}
