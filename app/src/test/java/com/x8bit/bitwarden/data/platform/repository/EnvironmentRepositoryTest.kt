dd21b10e75b5e9ba39e49c77ee6d489033bcd535
André Bispo <abispo@bitwarden.com>
2024-07-29 16:37:42 +0100

    [PM-9875] Update EnvironmentRepository tests

--- app/src/test/java/com/x8bit/bitwarden/data/platform/repository/EnvironmentRepositoryTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/platform/repository/EnvironmentRepositoryTest.kt
@@ -7,10 +7,16 @@ import com.x8bit.bitwarden.data.auth.datasource.disk.model.UserStateJson
 import com.x8bit.bitwarden.data.auth.datasource.disk.util.FakeAuthDiskSource
 import com.x8bit.bitwarden.data.platform.base.FakeDispatcherManager
 import com.x8bit.bitwarden.data.platform.datasource.disk.EnvironmentDiskSource
+import com.x8bit.bitwarden.data.platform.datasource.disk.util.FakeConfigDiskSource
+import com.x8bit.bitwarden.data.platform.datasource.network.model.ConfigResponseJson
+import com.x8bit.bitwarden.data.platform.datasource.network.model.ConfigResponseJson.EnvironmentJson
+import com.x8bit.bitwarden.data.platform.datasource.network.model.ConfigResponseJson.ServerJson
 import com.x8bit.bitwarden.data.platform.manager.dispatcher.DispatcherManager
 import com.x8bit.bitwarden.data.platform.repository.model.Environment
 import com.x8bit.bitwarden.data.platform.repository.util.bufferedMutableSharedFlow
 import com.x8bit.bitwarden.data.platform.repository.util.toEnvironmentUrls
+import com.x8bit.bitwarden.data.platform.util.asSuccess
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
@@ -27,12 +33,21 @@ import org.junit.jupiter.api.Test
 class EnvironmentRepositoryTest {
 
     private val dispatcherManager: DispatcherManager = FakeDispatcherManager()
+    private val fakeConfigDiskSource = FakeConfigDiskSource()
+    private val serverConfigRepository = ServerConfigRepositoryImpl(
+        configService = mockk {
+            coEvery { getConfig() } returns CONFIG_RESPONSE_JSON.asSuccess()
+        },
+        configDiskSource = fakeConfigDiskSource,
+        dispatcherManager = dispatcherManager
+    )
 
     private val fakeEnvironmentDiskSource = FakeEnvironmentDiskSource()
     private val fakeAuthDiskSource = FakeAuthDiskSource()
 
     private val repository = EnvironmentRepositoryImpl(
         environmentDiskSource = fakeEnvironmentDiskSource,
+        serverConfigRepository = serverConfigRepository,
         authDiskSource = fakeAuthDiskSource,
         dispatcherManager = dispatcherManager,
     )
@@ -147,6 +162,81 @@ class EnvironmentRepositoryTest {
         }
     }
 
+    @Test
+    fun `changes to the active user should fetch new server configurations`() {
+        assertEquals(
+            Environment.Us,
+            repository.environment,
+        )
+        assertEquals(
+            null,
+            fakeConfigDiskSource.serverConfig,
+        )
+
+        // Updating the environment for the active user to a non-null value triggers an update
+        // in the saved environment.
+        fakeAuthDiskSource.userState = getMockUserState(
+            environmentForActiveUser = EnvironmentUrlDataJson.DEFAULT_EU,
+        )
+        assertEquals(
+            Environment.Eu,
+            repository.environment,
+        )
+        assertEquals(
+            EnvironmentUrlDataJson.DEFAULT_EU,
+            fakeEnvironmentDiskSource.preAuthEnvironmentUrlData,
+        )
+        assertEquals(
+            fakeConfigDiskSource.serverConfig!!.serverData,
+            CONFIG_RESPONSE_JSON
+        )
+
+        // Updating the environment for the active user to a null value leaves the current
+        // environment unchanged.
+        fakeAuthDiskSource.userState = getMockUserState(
+            environmentForActiveUser = null,
+        )
+        assertEquals(
+            Environment.Eu,
+            repository.environment,
+        )
+        assertEquals(
+            EnvironmentUrlDataJson.DEFAULT_EU,
+            fakeEnvironmentDiskSource.preAuthEnvironmentUrlData,
+        )
+        assertEquals(
+            fakeConfigDiskSource.serverConfig!!.serverData,
+            CONFIG_RESPONSE_JSON
+        )
+    }
+
+    @Test
+    fun `new server configurations should be fetched when environment changes`() = runTest {
+        val environmentUrlDataJson = mockk<EnvironmentUrlDataJson>()
+        val environment = mockk<Environment> {
+            every { environmentUrlData } returns environmentUrlDataJson
+        }
+        every { environmentUrlDataJson.toEnvironmentUrls() } returns environment
+
+        // The initial values
+        assertEquals(
+            Environment.Us,
+            repository.environment,
+        )
+        assertEquals(
+            fakeConfigDiskSource.serverConfig,
+            null,
+        )
+
+        // Updating the property updates server configurations
+        repository.environment = environment
+
+        assertEquals(
+            fakeConfigDiskSource.serverConfig!!.serverData,
+            CONFIG_RESPONSE_JSON,
+        )
+    }
+
     private fun getMockUserState(
         environmentForActiveUser: EnvironmentUrlDataJson?,
     ): UserStateJson =
@@ -178,3 +268,22 @@ private class FakeEnvironmentDiskSource : EnvironmentDiskSource {
     private val mutablePreAuthEnvironmentUrlDataFlow =
         bufferedMutableSharedFlow<EnvironmentUrlDataJson?>(replay = 1)
 }
+
+private val CONFIG_RESPONSE_JSON = ConfigResponseJson(
+    type = null,
+    version = "2024.7.0",
+    gitHash = "25cf6119-dirty",
+    server = ServerJson(
+        name = "example",
+        url = "https://localhost:8080",
+    ),
+    environment = EnvironmentJson(
+        cloudRegion = null,
+        vaultUrl = "https://localhost:8080",
+        apiUrl = "http://localhost:4000",
+        identityUrl = "http://localhost:33656",
+        notificationsUrl = "http://localhost:61840",
+        ssoUrl = "http://localhost:51822",
+    ),
+    featureStates = mapOf("duo-redirect" to true, "flexible-collections-v-1" to false)
+)
