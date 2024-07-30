646566edd884f5d119b310ef465649d958083833
André Bispo <abispo@bitwarden.com>
2024-07-30 20:23:33 +0100

    [PM-9875] Server configurations  (#3645)

---
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/datasource/disk/ConfigDiskSource.kt
@@ -0,0 +1,21 @@
+package com.x8bit.bitwarden.data.platform.datasource.disk
+
+import com.x8bit.bitwarden.data.platform.datasource.disk.model.ServerConfig
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Primary access point for server configuration-related disk information.
+ */
+interface ConfigDiskSource {
+
+    /**
+     * The currently persisted [ServerConfig] (or `null` if not set).
+     */
+    var serverConfig: ServerConfig?
+
+    /**
+     * Emits updates that track [ServerConfig]. This will replay the last known value,
+     * if any.
+     */
+    val serverConfigFlow: Flow<ServerConfig?>
+}
