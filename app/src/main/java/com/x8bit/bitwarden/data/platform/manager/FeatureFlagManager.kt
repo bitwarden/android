31f6c14a8768addc91c8997f443bff0bd4cdf51f
David Perez <david@livefront.com>
2024-08-07 09:49:13 -0500

    Add option to retrieve feature flag synchronously

--- app/src/main/java/com/x8bit/bitwarden/data/platform/manager/FeatureFlagManager.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/manager/FeatureFlagManager.kt
@@ -20,11 +20,19 @@ interface FeatureFlagManager {
 
     /**
      * Get value for feature flag with [key] and returns it as generic type [T].
-     * If no value is found the the given [key] its default value will be returned.
+     * If no value is found the given [key] its default value will be returned.
      * Cached flags can be invalidated with [forceRefresh]
      */
     suspend fun <T : Any> getFeatureFlag(
         key: FlagKey<T>,
         forceRefresh: Boolean,
+    ): T
+
+    /**
+     * Gets the value for feature flag with [key] and returns it as generic type [T].
+     * If no value is found the given [key] its [FlagKey.defaultValue] will be returned.
+     */
+    fun <T : Any> getFeatureFlag(
+        key: FlagKey<T>,
     ): T
 }
