551f94864412e18d64f7f092b87b9dd8fb7dae7d
David Perez <david@livefront.com>
2024-08-13 11:34:33 -0500

    PM-10835: Make config request after environment update (#3720)

--- app/src/test/java/com/x8bit/bitwarden/data/platform/manager/NetworkConfigManagerTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/platform/manager/NetworkConfigManagerTest.kt
@@ -8,27 +8,39 @@ import com.x8bit.bitwarden.data.platform.datasource.network.interceptor.AuthToke
 import com.x8bit.bitwarden.data.platform.datasource.network.interceptor.BaseUrlInterceptors
 import com.x8bit.bitwarden.data.platform.manager.dispatcher.DispatcherManager
 import com.x8bit.bitwarden.data.platform.repository.EnvironmentRepository
+import com.x8bit.bitwarden.data.platform.repository.ServerConfigRepository
 import com.x8bit.bitwarden.data.platform.repository.model.Environment
+import com.x8bit.bitwarden.data.util.advanceTimeByAndRunCurrent
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class NetworkConfigManagerTest {
 
-    private val dispatcherManager: DispatcherManager = FakeDispatcherManager()
+    private val testDispatcher = UnconfinedTestDispatcher()
+    private val dispatcherManager: DispatcherManager = FakeDispatcherManager(
+        unconfined = testDispatcher,
+    )
     private val mutableAuthStateFlow = MutableStateFlow<AuthState>(AuthState.Uninitialized)
     private val mutableEnvironmentStateFlow = MutableStateFlow<Environment>(Environment.Us)
 
     private val authRepository: AuthRepository = mockk {
         every { authStateFlow } returns mutableAuthStateFlow
     }
-
     private val environmentRepository: EnvironmentRepository = mockk {
         every { environmentStateFlow } returns mutableEnvironmentStateFlow
+    }
+    private val serverConfigRepository: ServerConfigRepository = mockk {
+        coEvery { getServerConfig(forceRefresh = true) } returns null
     }
 
     private val refreshAuthenticator = RefreshAuthenticator()
@@ -43,6 +55,7 @@ class NetworkConfigManagerTest {
             authRepository = authRepository,
             authTokenInterceptor = authTokenInterceptor,
             environmentRepository = environmentRepository,
+            serverConfigRepository = serverConfigRepository,
             baseUrlInterceptors = baseUrlInterceptors,
             refreshAuthenticator = refreshAuthenticator,
             dispatcherManager = dispatcherManager,
@@ -85,5 +98,9 @@ class NetworkConfigManagerTest {
             Environment.Eu,
             baseUrlInterceptors.environment,
         )
+        testDispatcher.advanceTimeByAndRunCurrent(delayTimeMillis = 500L)
+        coVerify(exactly = 1) {
+            serverConfigRepository.getServerConfig(forceRefresh = true)
+        }
     }
 }
