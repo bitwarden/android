2bed4986a1607b0ded18f79e8d89d1bd677f7278
David Perez <david@livefront.com>
2024-08-14 09:23:13 -0500

    PM-10855: Update the minimum SDK to API 29 (Android 10) (#3723)

--- app/src/main/java/com/x8bit/bitwarden/data/autofill/builder/SaveInfoBuilder.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/autofill/builder/SaveInfoBuilder.kt
@@ -2,7 +2,6 @@ package com.x8bit.bitwarden.data.autofill.builder
 
 import android.service.autofill.FillRequest
 import android.service.autofill.SaveInfo
-import com.x8bit.bitwarden.data.autofill.model.AutofillAppInfo
 import com.x8bit.bitwarden.data.autofill.model.AutofillPartition
 
 /**
@@ -12,13 +11,11 @@ interface SaveInfoBuilder {
     /**
      * Build a save info out the provided data. If that isn't possible, return null.
      *
-     * @param autofillAppInfo App data that is required for building the [SaveInfo].
      * @param autofillPartition The portion of the processed [FillRequest] that will be filled.
      * @param fillRequest The [FillRequest] that initiated the autofill flow.
      * @param packageName The package name that was extracted from the [FillRequest].
      */
     fun build(
-        autofillAppInfo: AutofillAppInfo,
         autofillPartition: AutofillPartition,
         fillRequest: FillRequest,
         packageName: String?,
