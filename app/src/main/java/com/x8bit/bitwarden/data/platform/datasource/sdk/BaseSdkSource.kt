524b9e9a08f857ba3a277d9ea4098bae20cabbc0
David Perez <david@livefront.com>
2024-08-14 16:10:19 -0500

    Add logging for SDK functionality in debug only (#3738)

---
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/datasource/sdk/BaseSdkSource.kt
@@ -0,0 +1,35 @@
+package com.x8bit.bitwarden.data.platform.datasource.sdk
+
+import android.util.Log
+import com.bitwarden.sdk.Client
+import com.x8bit.bitwarden.BuildConfig
+import com.x8bit.bitwarden.data.platform.manager.SdkClientManager
+
+/**
+ * Base class for simplifying sdk interactions.
+ */
+@Suppress("UnnecessaryAbstractClass")
+abstract class BaseSdkSource(
+    protected val sdkClientManager: SdkClientManager,
+) {
+    /**
+     * Helper function to retrieve the [Client] associated with the given [userId].
+     */
+    protected suspend fun getClient(
+        userId: String? = null,
+    ): Client = sdkClientManager.getOrCreateClient(userId = userId)
+
+    /**
+     * Invokes the [block] with `this` value as its receiver and returns its result if it was
+     * successful and catches any exception that was thrown from the `block` and wrapping it as a
+     * failure.
+     */
+    protected inline fun <T, R> T.runCatchingWithLogs(
+        block: T.() -> R,
+    ): Result<R> = runCatching(block = block)
+        .onFailure {
+            if (BuildConfig.DEBUG) {
+                Log.w(this@BaseSdkSource::class.java.simpleName, it)
+            }
+        }
+}
