86dabea39ffa389ab1d7b1e460e6c9ae98087823
Dave Severns <149429124+dseverns-livefront@users.noreply.github.com>
2024-08-20 18:17:03 -0400

    PM-11192 update check email screen to new design (#3788)

--- app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/checkemail/CheckEmailScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/checkemail/CheckEmailScreen.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -23,7 +23,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
@@ -31,23 +30,22 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.x8bit.bitwarden.R
 import com.x8bit.bitwarden.ui.platform.base.util.EventsEffect
 import com.x8bit.bitwarden.ui.platform.base.util.createAnnotatedString
+import com.x8bit.bitwarden.ui.platform.base.util.standardHorizontalMargin
 import com.x8bit.bitwarden.ui.platform.components.appbar.BitwardenTopAppBar
 import com.x8bit.bitwarden.ui.platform.components.button.BitwardenFilledButton
+import com.x8bit.bitwarden.ui.platform.components.button.BitwardenTextButton
 import com.x8bit.bitwarden.ui.platform.components.scaffold.BitwardenScaffold
 import com.x8bit.bitwarden.ui.platform.components.util.rememberVectorPainter
 import com.x8bit.bitwarden.ui.platform.composition.LocalIntentManager
 import com.x8bit.bitwarden.ui.platform.manager.intent.IntentManager
-
-/**
- * Constant string to be used in string annotation tag field
- */
-private const val TAG_URL = "URL"
+import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
 
 /**
  * Top level composable for the check email screen.
@@ -57,7 +55,6 @@ private const val TAG_URL = "URL"
 @Composable
 fun CheckEmailScreen(
     onNavigateBack: () -> Unit,
-    onNavigateBackToLanding: () -> Unit,
     intentManager: IntentManager = LocalIntentManager.current,
     viewModel: CheckEmailViewModel = hiltViewModel(),
 ) {
@@ -71,10 +68,6 @@ fun CheckEmailScreen(
             is CheckEmailEvent.NavigateToEmailApp -> {
                 intentManager.startDefaultEmailApplication()
             }
-
-            is CheckEmailEvent.NavigateBackToLanding -> {
-                onNavigateBackToLanding.invoke()
-            }
         }
     }
 
@@ -87,10 +80,10 @@ fun CheckEmailScreen(
             BitwardenTopAppBar(
                 title = stringResource(id = R.string.create_account),
                 scrollBehavior = scrollBehavior,
-                navigationIcon = rememberVectorPainter(id = R.drawable.ic_close),
-                navigationIconContentDescription = stringResource(id = R.string.close),
+                navigationIcon = rememberVectorPainter(id = R.drawable.ic_back),
+                navigationIconContentDescription = stringResource(id = R.string.back),
                 onNavigationIconClick = remember(viewModel) {
-                    { viewModel.trySendAction(CheckEmailAction.CloseClick) }
+                    { viewModel.trySendAction(CheckEmailAction.BackClick) }
                 },
             )
         },
@@ -102,105 +95,118 @@ fun CheckEmailScreen(
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState()),
         ) {
-            Spacer(modifier = Modifier.height(32.dp))
-            Image(
-                painter = rememberVectorPainter(id = R.drawable.email_check),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-                contentDescription = null,
-                contentScale = ContentScale.FillHeight,
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .height(112.dp)
-                    .fillMaxWidth(),
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            Text(
-                text = stringResource(id = R.string.check_your_email),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .wrapContentHeight()
-                    .fillMaxWidth(),
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            @Suppress("MaxLineLength")
-            val descriptionAnnotatedString = createAnnotatedString(
-                mainString = stringResource(
-                    id = R.string.follow_the_instructions_in_the_email_sent_to_x_to_continue_creating_your_account,
-                    state.email,
-                ),
-                highlights = listOf(state.email),
-                highlightStyle = SpanStyle(
-                    color = MaterialTheme.colorScheme.onSurface,
-                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-                    fontWeight = FontWeight.Bold,
-                ),
-                tag = "EMAIL",
-            )
-            Text(
-                text = descriptionAnnotatedString,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            BitwardenFilledButton(
-                label = stringResource(id = R.string.open_email_app),
-                onClick = remember(viewModel) {
-                    { viewModel.trySendAction(CheckEmailAction.OpenEmailClick) }
+            CheckEmailContent(
+                email = state.email,
+                onOpenEmailAppClick = remember(viewModel) {
+                    {
+                        viewModel.trySendAction(CheckEmailAction.OpenEmailClick)
+                    }
                 },
-                modifier = Modifier
-                    .testTag("OpenEmailApp")
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
+                onChangeEmailClick = remember(viewModel) {
+                    {
+                        viewModel.trySendAction(CheckEmailAction.ChangeEmailClick)
+                    }
+                },
+                modifier = Modifier.standardHorizontalMargin(),
             )
-            Spacer(modifier = Modifier.height(32.dp))
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                val goBackAnnotatedString = createAnnotatedString(
-                    mainString = stringResource(
-                        id = R.string.no_email_go_back_to_edit_your_email_address,
-                    ),
-                    highlights = listOf(stringResource(id = R.string.go_back)),
-                    tag = TAG_URL,
-                )
-                ClickableText(
-                    text = goBackAnnotatedString,
-                    onClick = {
-                        goBackAnnotatedString
-                            .getStringAnnotations(TAG_URL, it, it)
-                            .firstOrNull()?.let {
-                                viewModel.trySendAction(CheckEmailAction.CloseClick)
-                            }
-                    },
-                )
-                Spacer(modifier = Modifier.height(32.dp))
-                val logInAnnotatedString = createAnnotatedString(
-                    mainString = stringResource(
-                        id = R.string.or_log_in_you_may_already_have_an_account,
-                    ),
-                    highlights = listOf(stringResource(id = R.string.log_in)),
-                    tag = TAG_URL,
-                )
-                ClickableText(
-                    text = logInAnnotatedString,
-                    onClick = {
-                        logInAnnotatedString
-                            .getStringAnnotations(TAG_URL, it, it)
-                            .firstOrNull()?.let {
-                                viewModel.trySendAction(CheckEmailAction.LoginClick)
-                            }
-                    },
-                )
-            }
             Spacer(modifier = Modifier.navigationBarsPadding())
         }
+    }
+}
+
+@Suppress("LongMethod")
+@Composable
+private fun CheckEmailContent(
+    email: String,
+    onOpenEmailAppClick: () -> Unit,
+    onChangeEmailClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+        Image(
+            painter = rememberVectorPainter(id = R.drawable.open_email),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(100.dp),
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = stringResource(id = R.string.check_your_email),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .wrapContentHeight()
+                .fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        val descriptionAnnotatedString = createAnnotatedString(
+            mainString = stringResource(
+                id = R.string.we_sent_an_email_to,
+                email,
+            ),
+            highlights = listOf(email),
+            highlightStyle = SpanStyle(
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                fontWeight = FontWeight.Bold,
+            ),
+            tag = "EMAIL",
+        )
+        Text(
+            text = descriptionAnnotatedString,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        @Suppress("MaxLineLength")
+        Text(
+            text = stringResource(R.string.select_the_link_in_the_email_to_verify_your_email_address_and_continue_creating_your_account),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(),
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        BitwardenFilledButton(
+            label = stringResource(id = R.string.open_email_app),
+            onClick = onOpenEmailAppClick,
+            modifier = Modifier
+                .testTag("OpenEmailApp")
+                .fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        BitwardenTextButton(
+            label = stringResource(R.string.change_email_address),
+            onClick = onChangeEmailClick,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CheckEmailScreenPreview() {
+    BitwardenTheme {
+        CheckEmailContent(
+            email = "email@fake.com",
+            onOpenEmailAppClick = { },
+            onChangeEmailClick = { },
+            modifier = Modifier.standardHorizontalMargin(),
+        )
     }
 }
