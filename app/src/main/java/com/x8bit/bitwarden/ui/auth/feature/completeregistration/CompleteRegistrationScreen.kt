d22a38e9f56c2dbc09544259a68efc093978edf3
Dave Severns <dseverns@livefront.com>
2024-08-20 14:30:23 -0400

    feedback

--- app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/completeregistration/CompleteRegistrationScreen.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/completeregistration/CompleteRegistrationScreen.kt
@@ -272,7 +272,6 @@ private fun CompleteRegistrationContentHeader(
     modifier: Modifier = Modifier,
     configuration: Configuration = LocalConfiguration.current,
 ) {
-
     if (configuration.isPortrait) {
         Column(
             modifier = modifier,
