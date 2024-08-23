76e7e4179d9da6f058939db0ada758a8ea56581a
mpbw2 <59324545+mpbw2@users.noreply.github.com>
2024-08-23 13:19:21 -0400

    update comment with tile service names

--- app/src/main/java/com/x8bit/bitwarden/BitwardenAppComponentFactory.kt
+++ app/src/main/java/com/x8bit/bitwarden/BitwardenAppComponentFactory.kt
@@ -24,10 +24,11 @@ private const val LEGACY_GENERATOR_TILE_SERVICE_NAME = "com.x8bit.bitwarden.Gene
 @OmitFromCoverage
 class BitwardenAppComponentFactory : AppComponentFactory() {
     /**
-     * Used to intercept when the [BitwardenAutofillService] or [BitwardenFido2ProviderService] is
-     * being instantiated and modify which service is created. This is required because the
-     * [className] used in the manifest must match the legacy Xamarin app service name but the
-     * service name in this app is different.
+     * Used to intercept when the [BitwardenAutofillService], [BitwardenFido2ProviderService],
+     * [BitwardenVaultTileService], or [BitwardenGeneratorTileService] is being instantiated and
+     * modify which service is created. This is required because the [className] used in the
+     * manifest must match the legacy Xamarin app service name but the service name in this app is
+     * different.
      */
     override fun instantiateServiceCompat(
         cl: ClassLoader,
