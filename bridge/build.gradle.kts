de48bb37d78397ddad89c15531210448b004d3c6
Andrew Haisting <142518658+ahaisting-livefront@users.noreply.github.com>
2024-09-05 12:22:49 -0500

    BITAU-96 Setup Bridge SDK (#3857)

---
+++ bridge/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+// For more info on versioning, see the README.
+val version = "0.1.0"
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "com.bitwarden.bridge"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        // This min value is selected to accommodate known consumers
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        buildConfigField("String", "VERSION", "\"$version\"")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility(libs.versions.jvmTarget.get())
+        targetCompatibility(libs.versions.jvmTarget.get())
+    }
+    buildFeatures {
+        buildConfig = true
+        aidl = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(libs.versions.jvmTarget.get()))
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+}
