368ca49fd6df6757ff7426c6f280a4071e19531f
David Perez <david@livefront.com>
2024-09-16 15:00:00 -0500

    Add BitwardenLegacyAppComponents for legacy app component names (#3922)

--- app/src/main/java/com/x8bit/bitwarden/data/autofill/accessibility/manager/AccessibilityActivityManagerImpl.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/autofill/accessibility/manager/AccessibilityActivityManagerImpl.kt
@@ -3,18 +3,10 @@ package com.x8bit.bitwarden.data.autofill.accessibility.manager
 import android.content.Context
 import android.provider.Settings
 import androidx.lifecycle.LifecycleCoroutineScope
+import com.x8bit.bitwarden.LEGACY_ACCESSIBILITY_SERVICE_NAME
 import com.x8bit.bitwarden.data.platform.manager.AppForegroundManager
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-
-/**
- * The accessibility service name as it is known in the AndroidManifest.
- *
- * Note: This is not the name of the actual service but the name defined in the Android Manifest
- * which matches the service in the Xamarin app.
- */
-private const val LEGACY_ACCESSIBILITY_SERVICE_NAME =
-    "com.x8bit.bitwarden.Accessibility.AccessibilityService"
 
 /**
  * The default implementation of the [AccessibilityActivityManager].
