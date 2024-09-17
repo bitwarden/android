14ba916e4800967c617a84727421f5c82f623e6d
David Perez <david@livefront.com>
2024-09-17 10:32:47 -0500

    Create reusable isAccessibilityServiceEnabled extension

--- app/src/main/java/com/x8bit/bitwarden/data/autofill/accessibility/manager/AccessibilityActivityManagerImpl.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/autofill/accessibility/manager/AccessibilityActivityManagerImpl.kt
@@ -1,9 +1,8 @@
 package com.x8bit.bitwarden.data.autofill.accessibility.manager
 
 import android.content.Context
-import android.provider.Settings
 import androidx.lifecycle.LifecycleCoroutineScope
-import com.x8bit.bitwarden.LEGACY_ACCESSIBILITY_SERVICE_NAME
+import com.x8bit.bitwarden.data.autofill.accessibility.util.isAccessibilityServiceEnabled
 import com.x8bit.bitwarden.data.platform.manager.AppForegroundManager
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -17,28 +16,12 @@ class AccessibilityActivityManagerImpl(
     appForegroundManager: AppForegroundManager,
     lifecycleScope: LifecycleCoroutineScope,
 ) : AccessibilityActivityManager {
-    private val isAccessibilityServiceEnabled: Boolean
-        get() {
-            val appContext = context.applicationContext
-            val accessibilityService = appContext
-                .packageName
-                ?.let { "$it/$LEGACY_ACCESSIBILITY_SERVICE_NAME" }
-                ?: return false
-            return Settings
-                .Secure
-                .getString(
-                    appContext.contentResolver,
-                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
-                )
-                ?.contains(accessibilityService)
-                ?: false
-        }
-
     init {
         appForegroundManager
             .appForegroundStateFlow
             .onEach {
-                accessibilityEnabledManager.isAccessibilityEnabled = isAccessibilityServiceEnabled
+                accessibilityEnabledManager.isAccessibilityEnabled =
+                    context.isAccessibilityServiceEnabled
             }
             .launchIn(lifecycleScope)
     }
