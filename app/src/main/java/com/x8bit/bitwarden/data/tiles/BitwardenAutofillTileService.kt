897567e81d195a5fb39e3fb9678726c75f1c5364
David Perez <david@livefront.com>
2024-09-19 11:37:40 -0500

    Add FLAG_ACTIVITY_NEW_TASK flag for autofill tile to avoid crash

--- app/src/main/java/com/x8bit/bitwarden/data/tiles/BitwardenAutofillTileService.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/tiles/BitwardenAutofillTileService.kt
@@ -44,6 +44,7 @@ class BitwardenAutofillTileService : TileService() {
         }
         accessibilityAutofillManager.accessibilityAction = AccessibilityAction.AttemptParseUri
         val intent = Intent(applicationContext, AccessibilityActivity::class.java)
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         if (isBuildVersionBelow(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)) {
             @Suppress("DEPRECATION")
             startActivityAndCollapse(intent)
