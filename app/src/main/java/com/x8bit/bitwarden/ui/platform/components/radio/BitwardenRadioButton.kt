f6030609a088e80bc25e7ac98b01a1693e2e946a
David Perez <david@livefront.com>
2024-09-26 10:28:24 -0500

    Add reusable radio button

---
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/radio/BitwardenRadioButton.kt
@@ -0,0 +1,27 @@
+package com.x8bit.bitwarden.ui.platform.components.radio
+
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * A custom Bitwarden-themed radio button.
+ *
+ * @param isSelected Whether this radio button is selected or not.
+ * @param onClick The lambda to be invoked when the item is clicked.
+ * @param modifier The [Modifier] to be applied to this radio button.
+ */
+@Composable
+fun BitwardenRadioButton(
+    isSelected: Boolean,
+    onClick: (() -> Unit)?,
+    modifier: Modifier = Modifier,
+) {
+    RadioButton(
+        modifier = modifier,
+        selected = isSelected,
+        onClick = onClick,
+        colors = RadioButtonDefaults.colors(),
+    )
+}
