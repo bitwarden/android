067fda133c5d96370a4cec544f168986e4cf765f
Andrew Haisting <ahaisting@livefront.com>
2024-09-30 13:20:26 -0500

    Add doc for SymmetricKeyStorageProvider

--- authenticatorbridge/src/main/java/com/bitwarden/authenticatorbridge/provider/SymmetricKeyStorageProvider.kt
+++ authenticatorbridge/src/main/java/com/bitwarden/authenticatorbridge/provider/SymmetricKeyStorageProvider.kt
@@ -3,9 +3,12 @@ package com.bitwarden.authenticatorbridge.provider
 import com.bitwarden.authenticatorbridge.model.SymmetricEncryptionKeyData
 
 /**
- * TODO: document me
+ * Provides a way for calling Applications to implement symmetric key storage.
  */
 interface SymmetricKeyStorageProvider {
 
+    /**
+     * Stored symmetric encryption key.
+     */
     var symmetricKey: SymmetricEncryptionKeyData?
 }
