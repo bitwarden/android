2e415b60af0d1972194d263ba3e49ac3f6a0b295
Andrew Haisting <ahaisting@livefront.com>
2024-10-01 10:50:24 -0500

    make safeCall private in same file

--- authenticatorbridge/src/main/java/com/bitwarden/authenticatorbridge/manager/AuthenticatorBridgeManagerImpl.kt
+++ authenticatorbridge/src/main/java/com/bitwarden/authenticatorbridge/manager/AuthenticatorBridgeManagerImpl.kt
@@ -193,3 +193,13 @@ internal class AuthenticatorBridgeManagerImpl(
         }
     }
 }
+/**
+ * Helper function for wrapping all calls to [IAuthenticatorBridgeService] around try catch.
+ *
+ * This is important because all calls to [IAuthenticatorBridgeService] can throw
+ * DeadObjectExceptions as well as RemoteExceptions.
+ */
+private fun <T> IAuthenticatorBridgeService?.safeCall(action: IAuthenticatorBridgeService.() -> T): Result<T?> =
+    runCatching {
+        this?.let { action.invoke(it) }
+    }
