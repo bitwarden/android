9fd44ed04ef3d161260119b7c8629040dd1cf28c
Andrew Haisting <ahaisting@livefront.com>
2024-10-14 16:56:01 -0400

    Turn feature flag on and adjust authenticator app signing cert

--- app/src/test/java/com/x8bit/bitwarden/data/platform/manager/FlagKeyTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/platform/manager/FlagKeyTest.kt
@@ -2,13 +2,14 @@ package com.x8bit.bitwarden.data.platform.manager
 
 import com.x8bit.bitwarden.data.platform.manager.model.FlagKey
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class FlagKeyTest {
 
     @Test
-    fun `AuthenticatorSync default value should be false`() {
-        assertFalse(FlagKey.AuthenticatorSync.defaultValue)
+    fun `AuthenticatorSync default value should be true`() {
+        assertTrue(FlagKey.AuthenticatorSync.defaultValue)
     }
 
     @Test
