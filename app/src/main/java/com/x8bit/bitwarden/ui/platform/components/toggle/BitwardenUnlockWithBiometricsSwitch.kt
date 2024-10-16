80b13a7367a797ccf7d1c4dca8317fbb7f898864
Dave Severns <dseverns@livefront.com>
2024-10-16 07:21:26 -0400

    detekt

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/toggle/BitwardenUnlockWithBiometricsSwitch.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/toggle/BitwardenUnlockWithBiometricsSwitch.kt
@@ -1,16 +1,11 @@
 package com.x8bit.bitwarden.ui.platform.components.toggle
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.x8bit.bitwarden.R
 import com.x8bit.bitwarden.ui.platform.manager.biometrics.BiometricSupportStatus
-import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
 
 /**
  * Displays a switch for enabling or disabling unlock with biometrics functionality.
