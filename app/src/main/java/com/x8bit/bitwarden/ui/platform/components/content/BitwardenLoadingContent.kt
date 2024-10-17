51849ab59f7dc9bfcfc2464d86bf87d787dc446f
Dave Severns <dseverns@livefront.com>
2024-10-17 13:05:59 -0400

    PM-11186 show full screen loading for import logins and attempt to sync vault, show dialog on error, move to success screen otherwise

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/content/BitwardenLoadingContent.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/content/BitwardenLoadingContent.kt
@@ -3,11 +3,15 @@ package com.x8bit.bitwarden.ui.platform.components.content
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.x8bit.bitwarden.ui.platform.components.indicator.BitwardenCircularProgressIndicator
+import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
 
 /**
  * A Bitwarden-themed, re-usable loading state.
@@ -15,12 +19,22 @@ import com.x8bit.bitwarden.ui.platform.components.indicator.BitwardenCircularPro
 @Composable
 fun BitwardenLoadingContent(
     modifier: Modifier = Modifier,
+    message: String? = null,
 ) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        message?.let {
+            Text(
+                text = it,
+                style = BitwardenTheme.typography.titleMedium,
+                // setting color explicitly here as we can't assume what the surface will be.
+                color = BitwardenTheme.colorScheme.text.primary,
+            )
+            Spacer(Modifier.height(16.dp))
+        }
         BitwardenCircularProgressIndicator()
         Spacer(modifier = Modifier.navigationBarsPadding())
     }
