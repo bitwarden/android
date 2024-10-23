fd24c9eaaf26f47c07b0e2f7a175ef6acf16b9ba
Patrick Honkonen <phonkonen@bitwarden.com>
2024-10-23 12:43:55 -0400

    Don't mock the clock! Yeeaaaaa boiiiiii!

--- app/src/test/java/com/x8bit/bitwarden/data/vault/datasource/disk/callback/DatabaseSchemeCallbackTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/vault/datasource/disk/callback/DatabaseSchemeCallbackTest.kt
@@ -9,21 +9,24 @@ import io.mockk.verify
 import org.junit.Test
 import java.time.Clock
 import java.time.Instant
+import java.time.ZoneOffset
 
 class DatabaseSchemeCallbackTest {
 
-    private val databaseSchemeManager: DatabaseSchemeManager = mockk()
-    private val clock: Clock = mockk()
-    private val callback = DatabaseSchemeCallback(databaseSchemeManager, clock)
+    private val databaseSchemeManager: DatabaseSchemeManager = mockk {
+        every { lastDatabaseSchemeChangeInstant = any() } just runs
+    }
+    private val callback = DatabaseSchemeCallback(databaseSchemeManager, FIXED_CLOCK)
 
     @Test
     fun `onDestructiveMigration updates lastDatabaseSchemeChangeInstant`() {
-        val now = Instant.now()
-        every { clock.instant() } returns now
-        every { databaseSchemeManager.lastDatabaseSchemeChangeInstant = any() } just runs
-
         callback.onDestructiveMigration(mockk())
 
-        verify { databaseSchemeManager.lastDatabaseSchemeChangeInstant = now }
+        verify { databaseSchemeManager.lastDatabaseSchemeChangeInstant = FIXED_CLOCK.instant() }
     }
 }
+
+private val FIXED_CLOCK: Clock = Clock.fixed(
+    Instant.parse("2023-10-27T12:00:00Z"),
+    ZoneOffset.UTC,
+)
