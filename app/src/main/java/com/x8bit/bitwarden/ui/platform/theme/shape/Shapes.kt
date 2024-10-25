b64175ff6ee275bcaa8a6c3925d29d5c17d5b074
David Perez <david@livefront.com>
2024-10-25 16:29:16 -0500

    Update fab design (#4168)

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/theme/shape/Shapes.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/theme/shape/Shapes.kt
@@ -9,14 +9,15 @@ import androidx.compose.ui.unit.dp
  */
 val bitwardenShapes: BitwardenShapes = BitwardenShapes(
     actionCard = RoundedCornerShape(size = 12.dp),
+    bottomSheet = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
     coachmark = RoundedCornerShape(size = 8.dp),
     content = RoundedCornerShape(size = 8.dp),
     contentBottom = RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp),
     contentTop = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
     dialog = RoundedCornerShape(size = 28.dp),
+    fab = CircleShape,
     infoCallout = RoundedCornerShape(size = 8.dp),
     menu = RoundedCornerShape(size = 4.dp),
     segmentedControl = CircleShape,
     snackbar = RoundedCornerShape(size = 8.dp),
-    bottomSheet = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
 )
