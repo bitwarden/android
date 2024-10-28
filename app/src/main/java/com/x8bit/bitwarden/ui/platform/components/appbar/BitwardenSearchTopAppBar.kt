8d26a9697b87d3bba34416ce638cb4ca7cc49f46
David Perez <david@livefront.com>
2024-10-28 18:23:43 -0500

    PM-14201: Update the default divider thickness

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/appbar/BitwardenSearchTopAppBar.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/appbar/BitwardenSearchTopAppBar.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
 import com.x8bit.bitwarden.R
 import com.x8bit.bitwarden.ui.platform.base.util.bottomDivider
 import com.x8bit.bitwarden.ui.platform.base.util.mirrorIfRtl
@@ -49,7 +48,7 @@ fun BitwardenSearchTopAppBar(
     TopAppBar(
         modifier = modifier
             .testTag(tag = "HeaderBarComponent")
-            .bottomDivider(thickness = (0.5).dp),
+            .bottomDivider(),
         colors = bitwardenTopAppBarColors(),
         scrollBehavior = scrollBehavior,
         navigationIcon = {
