ec85122b69219519a93e7be2c94f9f9ece9691e7
David Perez <david@livefront.com>
2024-11-12 09:17:08 -0600

    PM-14854: Keep NetworkResult to avoid obfuscation crash in release

--- app/src/main/java/com/x8bit/bitwarden/data/platform/datasource/network/model/NetworkResult.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/datasource/network/model/NetworkResult.kt
@@ -1,10 +1,13 @@
 package com.x8bit.bitwarden.data.platform.datasource.network.model
 
+import androidx.annotation.Keep
+
 /**
  * A wrapper class for a network result for type [T]. If the network request is successful, the
  * response will be a [Success] containing the data. If the network request is a failure, the
  * response will be a [Failure] containing the [Throwable].
  */
+@Keep
 sealed class NetworkResult<out T> {
     /**
      * A successful network result with the relevant [T] data.
