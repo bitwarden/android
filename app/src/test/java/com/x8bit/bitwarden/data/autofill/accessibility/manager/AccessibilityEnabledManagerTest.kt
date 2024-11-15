30eb11b85ebe7dd1832621da0fcb9616dccebdee
David Perez <david@livefront.com>
2024-11-15 16:15:19 -0600

    PM-14409: Add realtime check for when the accessibility service is enabled or disabled (#4314)

--- app/src/test/java/com/x8bit/bitwarden/data/autofill/accessibility/manager/AccessibilityEnabledManagerTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/autofill/accessibility/manager/AccessibilityEnabledManagerTest.kt
@@ -1,6 +1,10 @@
 package com.x8bit.bitwarden.data.autofill.accessibility.manager
 
+import android.view.accessibility.AccessibilityManager
 import app.cash.turbine.test
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -8,23 +12,33 @@ import org.junit.jupiter.api.Test
 
 class AccessibilityEnabledManagerTest {
 
+    private val accessibilityStateChangeListener =
+        slot<AccessibilityManager.AccessibilityStateChangeListener>()
+    private val accessibilityManager = mockk<AccessibilityManager> {
+        every {
+            addAccessibilityStateChangeListener(capture(accessibilityStateChangeListener))
+        } returns true
+    }
+
     private val accessibilityEnabledManager: AccessibilityEnabledManager =
-        AccessibilityEnabledManagerImpl()
+        AccessibilityEnabledManagerImpl(
+            accessibilityManager = accessibilityManager,
+        )
 
     @Suppress("MaxLineLength")
     @Test
-    fun `isAccessibilityEnabledStateFlow should emit whenever isAccessibilityEnabled is set to a unique value`() =
+    fun `isAccessibilityEnabledStateFlow should emit whenever accessibilityStateChangeListener emits a unique value`() =
         runTest {
             accessibilityEnabledManager.isAccessibilityEnabledStateFlow.test {
                 assertFalse(awaitItem())
 
-                accessibilityEnabledManager.isAccessibilityEnabled = true
+                accessibilityStateChangeListener.captured.onAccessibilityStateChanged(true)
                 assertTrue(awaitItem())
 
-                accessibilityEnabledManager.isAccessibilityEnabled = true
+                accessibilityStateChangeListener.captured.onAccessibilityStateChanged(true)
                 expectNoEvents()
 
-                accessibilityEnabledManager.isAccessibilityEnabled = false
+                accessibilityStateChangeListener.captured.onAccessibilityStateChanged(false)
                 assertFalse(awaitItem())
             }
         }
