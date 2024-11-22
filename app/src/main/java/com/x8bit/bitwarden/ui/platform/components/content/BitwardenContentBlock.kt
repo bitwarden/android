222cd790257948234932b1c0361a5328f7384b4c
Philip Cappelli <phil@livefront.com>
2024-11-22 09:55:02 -0500

    fix lint error

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/content/BitwardenContentBlock.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/content/BitwardenContentBlock.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
