97285f463eab7cbbfd8fc7aa70076643166a0fcd
Dave Severns <149429124+dseverns-livefront@users.noreply.github.com>
2024-12-03 15:49:37 -0500

    PM-15514 add feature flag key for app review prompt (#4414)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/manager/model/FlagKey.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/manager/model/FlagKey.kt
@@ -35,6 +35,7 @@ sealed class FlagKey<out T : Any> {
                 VerifiedSsoDomainEndpoint,
                 CredentialExchangeProtocolImport,
                 CredentialExchangeProtocolExport,
+                AppReviewPrompt,
             )
         }
     }
@@ -118,6 +119,15 @@ sealed class FlagKey<out T : Any> {
      */
     data object CredentialExchangeProtocolExport : FlagKey<Boolean>() {
         override val keyName: String = "cxp-export-mobile"
+        override val defaultValue: Boolean = false
+        override val isRemotelyConfigured: Boolean = true
+    }
+
+    /**
+     * Data object holding the feature flag key for the App Review Prompt feature.
+     */
+    data object AppReviewPrompt : FlagKey<Boolean>() {
+        override val keyName: String = "app-review-prompt"
         override val defaultValue: Boolean = false
         override val isRemotelyConfigured: Boolean = true
     }
