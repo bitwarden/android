0da5d9afbfea43679142373ac437d1baa4f2174e
Dave Severns <dseverns@livefront.com>
2024-12-04 14:58:47 -0600

    working clipboard copy effect with listener

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/util/ClipboardDataEffect.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/util/ClipboardDataEffect.kt
@@ -1,12 +1,15 @@
 package com.x8bit.bitwarden.ui.platform.components.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ClipboardManager
 import androidx.compose.ui.platform.LocalClipboardManager
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.onEach
 
 /**
  * Side effect which is triggered when the clipboard data's text changes.
@@ -18,11 +21,22 @@ fun ClipboardDataEffect(
     onClipboardDataUpdated: (String) -> Unit,
 ) {
     val localClipboardManager: ClipboardManager = LocalClipboardManager.current
-    LaunchedEffect(localClipboardManager) {
-        snapshotFlow { localClipboardManager.getText()?.text }
-            .filterNotNull()
-            .onEach {
-                onClipboardDataUpdated(it)
+    var clipDataText: String? by rememberSaveable { mutableStateOf(null) }
+    val listener: android.content.ClipboardManager.OnPrimaryClipChangedListener =
+        remember(localClipboardManager) {
+            android.content.ClipboardManager.OnPrimaryClipChangedListener {
+                clipDataText = localClipboardManager.getText().toString()
             }
+        }
+    LaunchedEffect(clipDataText) {
+        clipDataText?.let {
+            onClipboardDataUpdated(it)
+        }
+    }
+    DisposableEffect(localClipboardManager) {
+        localClipboardManager.nativeClipboard.addPrimaryClipChangedListener(listener)
+        onDispose {
+            localClipboardManager.nativeClipboard.removePrimaryClipChangedListener(listener)
+        }
     }
 }
