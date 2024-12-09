f882dc334fa259128df2c46f94dabd499782c8ae
Philip Cappelli <phil@livefront.com>
2024-12-09 13:47:01 -0500

    Revert "PM-15831 - Set `enable-authenticator-sync-android` remotely configurable value to `true`"
    This reverts commit b0dc3f512d5460e6ac0d5152bb734dfd0a370311.

--- app/src/test/java/com/x8bit/bitwarden/data/platform/manager/FlagKeyTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/platform/manager/FlagKeyTest.kt
@@ -2,7 +2,6 @@ package com.x8bit.bitwarden.data.platform.manager
 
 import com.x8bit.bitwarden.data.platform.manager.model.FlagKey
 import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class FlagKeyTest {
@@ -10,11 +9,6 @@ class FlagKeyTest {
     @Test
     fun `AuthenticatorSync default value should be false`() {
         assertFalse(FlagKey.AuthenticatorSync.defaultValue)
-    }
-
-    @Test
-    fun `AuthenticatorSync is remotely configured value should be true`() {
-        assertTrue(FlagKey.AuthenticatorSync.isRemotelyConfigured)
     }
 
     @Test
