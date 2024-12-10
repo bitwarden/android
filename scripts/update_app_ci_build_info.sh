c4d5769830d1b195e60976268a4304ea4f22565c
Álison Fernandes <vvolkgang@users.noreply.github.com>
2024-12-10 22:01:09 +0000

    Update build info script to receive run number and run attempt instead of url

--- scripts/update_app_ci_build_info.sh
+++ scripts/update_app_ci_build_info.sh
@@ -7,9 +7,9 @@
 #   - Git command line tools installed
 #   - Write access to CIBuildInfo.kt file
 
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <repository> <branch> <commit_hash> <ci_run_url>"
-    echo "E.g: $0 bitwarden/android main abc123 https://github.com/bitwarden/android/actions/runs/123"
+if [ $# -ne 5 ]; then
+    echo "Usage: $0 <repository> <branch> <commit_hash> <ci_run_number> <ci_run_attempt>"
+    echo "E.g: $0 bitwarden/android main abc123 123 1"
     exit 1
 fi
 
@@ -18,20 +18,23 @@ set -euo pipefail
 repository=$1
 branch=$2
 commit_hash=$3
-ci_run_url=$4
+ci_run_number=$4
+ci_run_attempt=$5
 
 ci_build_info_file="../app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/settings/about/utils/CIBuildInfo.kt"
 git_source="${repository}/${branch}@${commit_hash}"
+ci_run_source="${repository}/actions/runs/${ci_run_number}/attempts/${ci_run_attempt}"
 
 echo "🧱 Updating app CI Build info..."
-echo "🧱 CI run url: ${ci_run_url}"
+echo "🧱 🧱 commit: ${git_source}"
+echo "🧱 💻 build source: ${ci_run_source}"
 
 
 cat << EOF > ${ci_build_info_file}
 object CIBuildInfo {
     val info: List<Pair<String, String>> = listOf(
-        ":seedling:" to "${git_source}",
-        ":octocat:" to "${ci_run_url}",
+        "🧱 commit:" to "${git_source}",
+        "💻 build source:" to "${ci_run_source}",
     )
 }
 EOF
