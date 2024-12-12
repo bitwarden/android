086b05a75a37e49425532c390a5011fee41c7bee
Philip Cappelli <phil@livefront.com>
2024-12-12 17:04:12 -0500

    PR comments

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/base/util/Text.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/base/util/Text.kt
@@ -203,7 +203,7 @@ fun createClickableAnnotatedString(
             // Skip adding the link if the text to highlight is not found in the main string.
             // This can happen if the highlighted text is correctly translated, but the main string
             // is not yet translated, causing the startIndex to be -1.
-            if (startIndex < 0) { continue }
+            if (startIndex < 0) continue
 
             val endIndex = startIndex + highlight.textToHighlight.length
             val link = LinkAnnotation.Clickable(
