d57c44a1befdd8ca15308f060ef8c67ac236e0ab
Dave Severns <149429124+dseverns-livefront@users.noreply.github.com>
2024-12-19 14:31:09 -0500

    Update app/src/fdroid/java/com/x8bit/bitwarden/ui/platform/manager/review/AppReviewManagerImpl.kt
    Co-authored-by: Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>

--- app/src/fdroid/java/com/x8bit/bitwarden/ui/platform/manager/review/AppReviewManagerImpl.kt
+++ app/src/fdroid/java/com/x8bit/bitwarden/ui/platform/manager/review/AppReviewManagerImpl.kt
@@ -3,7 +3,7 @@ package com.x8bit.bitwarden.ui.platform.manager.review
 import android.app.Activity
 
 /**
- * No-op implementation of [AppReviewManager] for FDroid builds.
+ * No-op implementation of [AppReviewManager] for F-Droid builds.
  */
 class AppReviewManagerImpl(
     activity: Activity,
