b7a6303058dccdd5ac3c4f138ef6cc2ee793086e
Philip Cappelli <phil@livefront.com>
2025-01-17 13:42:53 -0500

    PR comments

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/dialog/BitwardenLoadingDialog.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/dialog/BitwardenLoadingDialog.kt
@@ -1,15 +1,17 @@
 package com.x8bit.bitwarden.ui.platform.components.dialog
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
+import com.x8bit.bitwarden.ui.platform.base.util.toDp
 import com.x8bit.bitwarden.ui.platform.components.content.BitwardenLoadingContent
 import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
 
@@ -23,23 +25,19 @@ fun BitwardenLoadingDialog(
     text: String,
 ) {
     Popup(
-        alignment = Alignment.Center,
         properties = PopupProperties(
             dismissOnBackPress = false,
             dismissOnClickOutside = false,
         ),
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+        BitwardenLoadingContent(
+            text = text,
             modifier = Modifier
                 .fillMaxSize()
                 .background(
                     color = BitwardenTheme.colorScheme.background.secondary.copy(alpha = 0.90f),
-                ),
-        ) {
-            BitwardenLoadingContent(text = text)
-        }
+                )
+        )
     }
 }
 
