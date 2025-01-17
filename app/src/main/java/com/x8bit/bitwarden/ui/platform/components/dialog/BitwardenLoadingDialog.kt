49c2f7adce668affbf14614809a3fbf596322790
Philip Cappelli <phil@livefront.com>
2025-01-17 13:44:35 -0500

    lint fixes

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/components/dialog/BitwardenLoadingDialog.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/components/dialog/BitwardenLoadingDialog.kt
@@ -1,17 +1,12 @@
 package com.x8bit.bitwarden.ui.platform.components.dialog
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
-import com.x8bit.bitwarden.ui.platform.base.util.toDp
 import com.x8bit.bitwarden.ui.platform.components.content.BitwardenLoadingContent
 import com.x8bit.bitwarden.ui.platform.theme.BitwardenTheme
 
@@ -36,7 +31,7 @@ fun BitwardenLoadingDialog(
                 .fillMaxSize()
                 .background(
                     color = BitwardenTheme.colorScheme.background.secondary.copy(alpha = 0.90f),
-                )
+                ),
         )
     }
 }
