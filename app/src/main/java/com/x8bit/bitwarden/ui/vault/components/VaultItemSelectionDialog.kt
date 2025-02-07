1accea79966bf514f5849f59e88c5a1761918711
Dave Severns <dseverns@livefront.com>
2025-02-07 14:01:34 -0500

    remove option

--- app/src/main/java/com/x8bit/bitwarden/ui/vault/components/VaultItemSelectionDialog.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/vault/components/VaultItemSelectionDialog.kt
@@ -17,9 +17,7 @@ import kotlinx.collections.immutable.persistentListOf
  * @param onOptionSelected Lambda to be called when an item is selected.
  * @param onDismissRequest Lambda to call when the dialog is dismissed by user.
  * @param selectedOption The currently selected item, if null will assign the selected item to
- * the default type.
- * @param shouldDismissOnSelection Whether or not to call [onDismissRequest] when a selection is
- * made. Default value is set to true.
+ * the default type. Calls [onDismissRequest] when selection is made.
  * @param excludedOptions List of [VaultItemCipherType] to exclude from the presented list.
  */
 @Composable
@@ -27,7 +25,6 @@ fun VaultItemSelectionDialog(
     onOptionSelected: (option: CreateVaultItemType) -> Unit,
     onDismissRequest: () -> Unit,
     selectedOption: CreateVaultItemType? = null,
-    shouldDismissOnSelection: Boolean = true,
     // TODO: PM-TBD possibly remove SSH_KEY as default exclusion once added SSH_KEY is enabled.
     excludedOptions: ImmutableList<CreateVaultItemType> = persistentListOf(
         CreateVaultItemType.SSH_KEY,
@@ -46,9 +43,7 @@ fun VaultItemSelectionDialog(
             BitwardenSelectionRow(
                 text = it.selectionText.asText(),
                 onClick = {
-                    if (shouldDismissOnSelection) {
-                        onDismissRequest()
-                    }
+                    onDismissRequest()
                     onOptionSelected(it)
                 },
                 isSelected = it == selectedOptionOrDefault,
