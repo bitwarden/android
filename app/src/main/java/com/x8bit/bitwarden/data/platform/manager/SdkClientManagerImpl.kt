5c076871abaa7e58c37c3088a6bf3ad58b3dabfa
Patrick Honkonen <phonkonen@bitwarden.com>
2025-02-26 12:28:28 -0500

    Implement NativeLibraryManager and NativeLibraryManagerImpl for loading native libraries
    This commit introduces the `NativeLibraryManager` interface and its implementation, `NativeLibraryManagerImpl`.
    - `NativeLibraryManager` defines a method `loadLibrary` for loading native libraries.
    - `NativeLibraryManagerImpl` uses `System.loadLibrary` to load libraries and handles potential `UnsatisfiedLinkError`.
    - `PlatformManagerModule` is updated to provide `NativeLibraryManager` instance and to inject it into `SdkClientManager`.

--- app/src/main/java/com/x8bit/bitwarden/data/platform/manager/SdkClientManagerImpl.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/manager/SdkClientManagerImpl.kt
@@ -3,13 +3,13 @@ package com.x8bit.bitwarden.data.platform.manager
 import android.os.Build
 import com.bitwarden.sdk.Client
 import com.x8bit.bitwarden.data.platform.util.isBuildVersionBelow
-import timber.log.Timber
 
 /**
  * Primary implementation of [SdkClientManager].
  */
 class SdkClientManagerImpl(
     private val featureFlagManager: FeatureFlagManager,
+    nativeLibraryManager: NativeLibraryManager,
     private val clientProvider: suspend () -> Client = {
         Client(settings = null).apply {
             platform().loadFlags(featureFlagManager.sdkFeatureFlags)
@@ -23,12 +23,7 @@ class SdkClientManagerImpl(
         // to work around this limitation the SDK must be manually loaded prior to initializing any
         // [Client] instance.
         if (isBuildVersionBelow(Build.VERSION_CODES.S)) {
-            @Suppress("TooGenericExceptionCaught")
-            try {
-                System.loadLibrary("bitwarden_uniffi")
-            } catch (e: Exception) {
-                Timber.e(e, "Failed to load bitwarden_uniffi library.")
-            }
+            nativeLibraryManager.loadLibrary("bitwarden_uniffi")
         }
     }
 
