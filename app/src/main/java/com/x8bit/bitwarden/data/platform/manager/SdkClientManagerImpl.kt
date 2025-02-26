dab06b0ed4a3ef12a721b77ed759f986cacc2fa1
Patrick Honkonen <phonkonen@bitwarden.com>
2025-02-26 10:04:24 -0500

    [PM-14303] Update Bitwarden SDK and load `bitwarden_uniffi` on older Android versions
    The Bitwarden SDK dependency was updated to version 1.0.0-20250225.125021-120.
    
    The SDK requires access to Android APIs that were made public in API 31 in order to generate email aliases. To address this limitation for devices with earlier API versions, the `bitwarden_uniffi` library is now loaded manually before initializing any `Client` instance.

--- app/src/main/java/com/x8bit/bitwarden/data/platform/manager/SdkClientManagerImpl.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/manager/SdkClientManagerImpl.kt
@@ -1,6 +1,9 @@
 package com.x8bit.bitwarden.data.platform.manager
 
+import android.os.Build
 import com.bitwarden.sdk.Client
+import com.x8bit.bitwarden.data.platform.util.isBuildVersionBelow
+import timber.log.Timber
 
 /**
  * Primary implementation of [SdkClientManager].
@@ -14,6 +17,20 @@ class SdkClientManagerImpl(
     },
 ) : SdkClientManager {
     private val userIdToClientMap = mutableMapOf<String?, Client>()
+
+    init {
+        // The SDK requires access to Android APIs that were not made public until API 31. In order
+        // to work around this limitation the SDK must be manually loaded prior to initializing any
+        // [Client] instance.
+        if (isBuildVersionBelow(Build.VERSION_CODES.S)) {
+            @Suppress("TooGenericExceptionCaught")
+            try {
+                System.loadLibrary("bitwarden_uniffi")
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to load bitwarden_uniffi library.")
+            }
+        }
+    }
 
     override suspend fun getOrCreateClient(
         userId: String?,
