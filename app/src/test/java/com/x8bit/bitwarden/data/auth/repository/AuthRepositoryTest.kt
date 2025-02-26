ea6d561b0d401a5a9e40d5e64f1fdd1d47619a84
André Bispo <abispo@bitwarden.com>
2025-02-26 11:20:21 +0000

    [PM-18451] Elevated privileges do not exempt from remove pin unlock policy (#4788)

--- app/src/test/java/com/x8bit/bitwarden/data/auth/repository/AuthRepositoryTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/data/auth/repository/AuthRepositoryTest.kt
@@ -4454,7 +4454,6 @@ class AuthRepositoryTest {
                 every { name } returns "orgName"
                 every { permissions } returns mockk {
                     every { shouldManageResetPassword } returns false
-                    every { shouldManagePolicies } returns false
                 }
                 every { shouldUseKeyConnector } returns true
                 every { type } returns OrganizationType.USER
@@ -4480,7 +4479,6 @@ class AuthRepositoryTest {
                     every { name } returns "orgName"
                     every { permissions } returns mockk {
                         every { shouldManageResetPassword } returns false
-                        every { shouldManagePolicies } returns false
                     }
                     every { shouldUseKeyConnector } returns true
                     every { type } returns OrganizationType.USER
@@ -4517,7 +4515,6 @@ class AuthRepositoryTest {
                     every { name } returns "orgName"
                     every { permissions } returns mockk {
                         every { shouldManageResetPassword } returns false
-                        every { shouldManagePolicies } returns false
                     }
                     every { shouldUseKeyConnector } returns true
                     every { type } returns OrganizationType.USER
