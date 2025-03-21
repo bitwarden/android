6d196b521492b95bbd7e5505cc95ce8e455d1125
Álison Fernandes <vvolkgang@users.noreply.github.com>
2025-03-21 00:43:09 +0000

    Render QRCode with bitwarden colours

--- app/src/main/java/com/x8bit/bitwarden/ui/vault/feature/viewasqrcode/util/QrCodeGenerator.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/vault/feature/viewasqrcode/util/QrCodeGenerator.kt
@@ -2,13 +2,14 @@ package com.x8bit.bitwarden.ui.vault.feature.viewasqrcode.util
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import androidx.core.graphics.createBitmap
+import androidx.core.graphics.set
+import androidx.core.graphics.toColorInt
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.common.BitMatrix
 import com.x8bit.bitwarden.data.platform.annotation.OmitFromCoverage
-import com.x8bit.bitwarden.ui.vault.feature.viewasqrcode.model.QrCodeType
-import java.net.URLEncoder
 
 /**
  * Utility class for generating QR codes.
@@ -16,7 +17,7 @@ import java.net.URLEncoder
 @OmitFromCoverage
 object QrCodeGenerator {
 
-    private const val QR_CODE_SIZE = 512
+    private const val QR_CODE_SIZE = 512 + 256
     private const val UTF_8 = "UTF-8"
 //
 //    /**
@@ -101,21 +102,24 @@ object QrCodeGenerator {
      * @param content The content to encode in the QR code.
      * @return A bitmap containing the generated QR code.
      */
-    private fun generateQrCodeBitmap(content: String): Bitmap {
+    fun generateQrCodeBitmap(
+        content: String,
+        barcodeFormat: BarcodeFormat = BarcodeFormat.QR_CODE,
+    ): Bitmap {
         val hints = mapOf(
             EncodeHintType.CHARACTER_SET to UTF_8,
-            EncodeHintType.MARGIN to 1
+            EncodeHintType.MARGIN to 0
         )
 
         val bitMatrix = MultiFormatWriter().encode(
             content,
-            BarcodeFormat.QR_CODE,
+            barcodeFormat,
             QR_CODE_SIZE,
             QR_CODE_SIZE,
             hints
         )
 
-        return createBitmapFromBitMatrix(bitMatrix)
+        return createBitmapFromBitMatrix(bitMatrix, barcodeFormat)
     }
 
     /**
@@ -124,17 +128,81 @@ object QrCodeGenerator {
      * @param bitMatrix The BitMatrix to convert.
      * @return A bitmap representation of the BitMatrix.
      */
-    private fun createBitmapFromBitMatrix(bitMatrix: BitMatrix): Bitmap {
+    private fun createBitmapFromBitMatrix(
+        bitMatrix: BitMatrix,
+        barcodeFormat: BarcodeFormat,
+    ): Bitmap {
         val width = bitMatrix.width
         val height = bitMatrix.height
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val bitmap = createBitmap(width, height)
+        val finderSize = findFinderPatternSize(bitMatrix, width, height)
 
+        val contentColor = "#165DDC".toColorInt() // bitwarden blue
+        val finderPatternColor = "#030E65".toColorInt() // dark blue
+        val backgroundColor = Color.WHITE
+        
         for (x in 0 until width) {
             for (y in 0 until height) {
-                bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                val bit = bitMatrix[x, y]
+
+                val color = if (barcodeFormat == BarcodeFormat.QR_CODE) {
+                    val drawingFinderPattern =
+                        isInsideFinderPattern(x, y, width, height, finderSize)
+                    when {
+                        bit && drawingFinderPattern -> finderPatternColor
+                        bit && !drawingFinderPattern -> contentColor
+                        else -> backgroundColor
+                    }
+                } else if (bit) finderPatternColor else backgroundColor
+
+                bitmap[x, y] = color
             }
         }
 
         return bitmap
+    }
+
+    private fun findFinderPatternSize(bitMatrix: BitMatrix, width: Int, height: Int): Int {
+        var firstBit = false
+        var bitCount = 0
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                val bit = bitMatrix[x, y]
+                when {
+                    !bit && firstBit -> return bitCount + (y - bitCount)
+                    !bit && !firstBit -> continue
+                    bit && !firstBit -> firstBit = true
+                }
+
+                bitCount++
+            }
+        }
+
+        return bitCount //finder pattern wasn't found
+    }
+
+    private fun isInsideFinderPattern(
+        x: Int,
+        y: Int,
+        width: Int,
+        height: Int,
+        squareSize: Int,
+    ): Boolean {
+        // Top-left square
+        if (x < squareSize && y < squareSize) {
+            return true
+        }
+
+        // Top-right square
+        if (x > width - squareSize && y < squareSize) {
+            return true
+        }
+
+        // Bottom-left square
+        if (x < squareSize && y > height - squareSize) {
+            return true
+        }
+
+        return false
     }
 }
