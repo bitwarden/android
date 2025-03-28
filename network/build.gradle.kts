a2f4e4f3b5d0de778c257cc92d6a1f7bb5a086a6
Patrick Honkonen <phonkonen@bitwarden.com>
2025-03-28 16:12:24 -0400

    [PM-19738] Create `network` module
    Create a library module responsible for communicating with the Bitwarden network API.
    
    Additionally, the following changes were made to checks and workflows:
    - Updated `build.gradle.kts` to include Kover for the `network` module.
    - Updated `Fastfile` to include lint, test and kover report generation tasks for the `network` module.

---
+++ network/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.bitwarden.network"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility(libs.versions.jvmTarget.get())
+        targetCompatibility(libs.versions.jvmTarget.get())
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+}
+
+dependencies {
+}
