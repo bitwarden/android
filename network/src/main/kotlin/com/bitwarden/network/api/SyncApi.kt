ed9769522852dd6aa78134ada51e7b66d020ed88
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-04-11 10:18:09 -0400

    [PM-20073] Migrate SyncApi to the network module (#5028)

--- network/src/main/kotlin/com/bitwarden/network/api/SyncApi.kt
+++ network/src/main/kotlin/com/bitwarden/network/api/SyncApi.kt
@@ -1,4 +1,4 @@
-package com.x8bit.bitwarden.data.vault.datasource.network.api
+package com.bitwarden.network.api
 
 import com.bitwarden.network.model.NetworkResult
 import com.bitwarden.network.model.SyncResponseJson
