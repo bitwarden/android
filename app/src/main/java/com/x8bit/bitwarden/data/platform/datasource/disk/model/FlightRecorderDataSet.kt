8f311861e33ae56fcf7e2f0ae90553b6d670ec26
David Perez <david@livefront.com>
2025-04-14 15:00:20 -0500

    PM-19593: Display all flight recorder logs (#5050)

--- app/src/main/java/com/x8bit/bitwarden/data/platform/datasource/disk/model/FlightRecorderDataSet.kt
+++ app/src/main/java/com/x8bit/bitwarden/data/platform/datasource/disk/model/FlightRecorderDataSet.kt
@@ -42,5 +42,8 @@ data class FlightRecorderDataSet(
 
         @SerialName("isActive")
         val isActive: Boolean,
+
+        @SerialName("expirationTime")
+        val expirationTimeMs: Long? = null,
     )
 }
