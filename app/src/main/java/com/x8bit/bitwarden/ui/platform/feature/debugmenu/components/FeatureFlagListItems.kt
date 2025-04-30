1a9fd9dad2b14f9332360211ddbca4f0cf68b54a
Andre Rosado <arosado@bitwarden.com>
2025-04-30 13:20:34 +0100

    removed unnecessary flagkey

--- app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/debugmenu/components/FeatureFlagListItems.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/platform/feature/debugmenu/components/FeatureFlagListItems.kt
@@ -44,7 +44,6 @@ fun <T : Any> FlagKey<T>.ListItemContent(
     FlagKey.ChromeAutofill,
     FlagKey.MobileErrorReporting,
     FlagKey.FlightRecorder,
-    FlagKey.AccountDeprovisioning,
         -> {
         @Suppress("UNCHECKED_CAST")
         BooleanFlagItem(
@@ -108,5 +107,4 @@ private fun <T : Any> FlagKey<T>.getDisplayLabel(): String = when (this) {
     FlagKey.ChromeAutofill -> stringResource(R.string.enable_chrome_autofill)
     FlagKey.MobileErrorReporting -> stringResource(R.string.enable_error_reporting_dialog)
     FlagKey.FlightRecorder -> stringResource(R.string.enable_flight_recorder)
-    FlagKey.AccountDeprovisioning -> stringResource(R.string.enable_account_deprovisioning)
 }
