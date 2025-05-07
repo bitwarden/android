966bdcd5d683f2e26ec1cb450be4c5d433350943
David Perez <david@livefront.com>
2025-05-07 11:03:17 -0500

    PM-21285: Ensure route data is serializable

--- app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/loginwithdevice/model/LoginWithDeviceType.kt
+++ app/src/main/java/com/x8bit/bitwarden/ui/auth/feature/loginwithdevice/model/LoginWithDeviceType.kt
@@ -1,10 +1,12 @@
 package com.x8bit.bitwarden.ui.auth.feature.loginwithdevice.model
 
 import com.x8bit.bitwarden.ui.auth.feature.loginwithdevice.LoginWithDeviceScreen
+import kotlinx.serialization.Serializable
 
 /**
  * Represents the different ways you may want to display the [LoginWithDeviceScreen].
  */
+@Serializable
 enum class LoginWithDeviceType {
     OTHER_DEVICE,
     SSO_ADMIN_APPROVAL,
