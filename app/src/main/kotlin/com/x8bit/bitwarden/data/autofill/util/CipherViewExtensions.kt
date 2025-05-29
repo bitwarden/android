4832513979e8dd3551e5fe1086d31ed9dfa29927
Kilian Eller <kilian.eller@obi.de>
2025-05-29 16:55:02 +0200

    add emptyline

--- app/src/main/kotlin/com/x8bit/bitwarden/data/autofill/util/CipherViewExtensions.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/autofill/util/CipherViewExtensions.kt
@@ -51,6 +51,7 @@ fun CipherView.toAutofillCipherProvider(): AutofillCipherProvider =
  */
 val CipherView.isActiveWithFido2Credentials: Boolean
     get() = deletedDate == null && !(login?.fido2Credentials.isNullOrEmpty())
+
 /**
  * Returns true when the cipher is not deleted and contains at least one Pasword credential.
  */
