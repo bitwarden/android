f55fa2bae5969493b4266d837eab8ded0e8972aa
Eran Boudjnah <eranbou@gmail.com>
2025-06-09 16:55:55 +0100

    PM-19476: Changed default passphrase word separator to empty string.

--- app/src/main/kotlin/com/x8bit/bitwarden/ui/tools/feature/generator/GeneratorViewModel.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/ui/tools/feature/generator/GeneratorViewModel.kt
@@ -699,7 +699,7 @@ class GeneratorViewModel @Inject constructor(
     private suspend fun generatePassphrase(passphrase: GeneratorState.MainType.Passphrase) {
         val request = PassphraseGeneratorRequest(
             numWords = passphrase.numWords.toUByte(),
-            wordSeparator = passphrase.wordSeparator?.toString() ?: " ",
+            wordSeparator = passphrase.wordSeparator?.toString().orEmpty(),
             capitalize = passphrase.capitalize,
             includeNumber = passphrase.includeNumber,
         )
