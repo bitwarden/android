56e8acf81f80cf4b6795331ee2cff2eb6f9f6ea3
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-06-18 14:42:54 -0400

    [PM-22786] Migrate PersistentListExtensions to core module (#5380)

--- app/src/main/kotlin/com/x8bit/bitwarden/ui/tools/feature/send/SendListItem.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/ui/tools/feature/send/SendListItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.bitwarden.core.util.persistentListOfNotNull
 import com.bitwarden.ui.platform.components.model.CardStyle
 import com.bitwarden.ui.platform.theme.BitwardenTheme
 import com.x8bit.bitwarden.R
@@ -16,7 +17,6 @@ import com.x8bit.bitwarden.ui.platform.components.dialog.BitwardenTwoButtonDialo
 import com.x8bit.bitwarden.ui.platform.components.listitem.BitwardenListItem
 import com.x8bit.bitwarden.ui.platform.components.listitem.SelectionItemData
 import com.x8bit.bitwarden.ui.platform.components.model.IconData
-import com.x8bit.bitwarden.ui.platform.util.persistentListOfNotNull
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
