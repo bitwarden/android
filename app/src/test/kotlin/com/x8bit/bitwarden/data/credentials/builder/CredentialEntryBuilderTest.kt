704797f50d211419ba6ccd34674713429efc24eb
Nailik <13292441+Nailik@users.noreply.github.com>
2025-06-19 15:52:04 +0200

    add CredentialEntryBuilderTest tests for passwords

--- app/src/test/kotlin/com/x8bit/bitwarden/data/credentials/builder/CredentialEntryBuilderTest.kt
+++ app/src/test/kotlin/com/x8bit/bitwarden/data/credentials/builder/CredentialEntryBuilderTest.kt
@@ -4,9 +4,12 @@ import android.app.PendingIntent
 import android.content.Context
 import android.graphics.drawable.Icon
 import androidx.core.graphics.drawable.IconCompat
+import androidx.credentials.provider.BeginGetPasswordOption
 import androidx.credentials.provider.BeginGetPublicKeyCredentialOption
+import androidx.credentials.provider.PasswordCredentialEntry
 import androidx.credentials.provider.PublicKeyCredentialEntry
 import com.bitwarden.fido.Fido2CredentialAutofillView
+import com.x8bit.bitwarden.data.autofill.model.AutofillCipher
 import com.x8bit.bitwarden.data.platform.manager.BiometricsEncryptionManager
 import com.x8bit.bitwarden.data.platform.manager.FeatureFlagManager
 import com.x8bit.bitwarden.data.platform.manager.model.FlagKey
@@ -31,6 +34,7 @@ class CredentialEntryBuilderTest {
 
     private val mockContext = mockk<Context>()
     private val mockGetPublicKeyCredentialIntent = mockk<PendingIntent>(relaxed = true)
+    private val mockGetPasswordCredentialIntent = mockk<PendingIntent>(relaxed = true)
     private val mockIntentManager = mockk<IntentManager> {
         every {
             createFido2GetCredentialPendingIntent(
@@ -42,10 +46,20 @@ class CredentialEntryBuilderTest {
                 isUserVerified = any(),
             )
         } returns mockGetPublicKeyCredentialIntent
+        every {
+            createPasswordGetCredentialPendingIntent(
+                action = any(),
+                userId = any(),
+                cipherId = any(),
+                requestCode = any(),
+                isUserVerified = any(),
+            )
+        } returns mockGetPasswordCredentialIntent
     }
     private val mockFeatureFlagManager = mockk<FeatureFlagManager>()
     private val mockBiometricsEncryptionManager = mockk<BiometricsEncryptionManager>()
     private val mockBeginGetPublicKeyOption = mockk<BeginGetPublicKeyCredentialOption>()
+    private val mockBeginGetPasswordOption = mockk<BeginGetPasswordOption>()
     private val credentialEntryBuilder = CredentialEntryBuilderImpl(
         context = mockContext,
         intentManager = mockIntentManager,
@@ -53,20 +67,26 @@ class CredentialEntryBuilderTest {
         biometricsEncryptionManager = mockBiometricsEncryptionManager,
     )
     private val mockPublicKeyCredentialEntry = mockk<PublicKeyCredentialEntry>(relaxed = true)
+    private val mockPasswordCredentialEntry = mockk<PasswordCredentialEntry>(relaxed = true)
     private val mockIcon = mockk<Icon>()
 
     @BeforeEach
     @Test
     fun setUp() {
         mockkConstructor(PublicKeyCredentialEntry.Builder::class)
+        mockkConstructor(PasswordCredentialEntry.Builder::class)
         mockkStatic(IconCompat::class)
         mockBuilder<PublicKeyCredentialEntry.Builder> { it.setIcon(any()) }
+        mockBuilder<PasswordCredentialEntry.Builder> { it.setIcon(any()) }
         every { IconCompat.createWithResource(any(), any()) } returns mockk {
             every { toIcon(mockContext) } returns mockIcon
         }
         every {
             anyConstructed<PublicKeyCredentialEntry.Builder>().build()
         } returns mockPublicKeyCredentialEntry
+        every {
+            anyConstructed<PasswordCredentialEntry.Builder>().build()
+        } returns mockPasswordCredentialEntry
     }
 
     @AfterEach
@@ -240,4 +260,110 @@ class CredentialEntryBuilderTest {
             anyConstructed<PublicKeyCredentialEntry.Builder>().setBiometricPromptData(any())
         }
     }
+
+    @Suppress("MaxLineLength")
+    @Test
+    fun `buildPasswordCredentialEntries should return Success with empty list when options list is empty`() =
+        runTest {
+            val options = emptyList<BeginGetPasswordOption>()
+            val passwordCredentialAutofillViews: List<AutofillCipher.Login> = listOf(
+                createMockPasswordCredentialAutofillCipherLogin(),
+            )
+
+            val result = credentialEntryBuilder
+                .buildPasswordCredentialEntries(
+                    userId = "userId",
+                    isUserVerified = false,
+                    passwordCredentialAutofillViews = passwordCredentialAutofillViews,
+                    beginGetPasswordCredentialOptions = options,
+                )
+            assertTrue(result.isEmpty())
+        }
+
+    @Suppress("MaxLineLength")
+    @Test
+    fun `buildPasswordCredentialEntries should return Success with empty list when passwordAutofillViews is empty`() =
+        runTest {
+            val options = listOf(mockBeginGetPasswordOption)
+            val passwordCredentialAutofillViews = emptyList<AutofillCipher.Login>()
+            val result = credentialEntryBuilder
+                .buildPasswordCredentialEntries(
+                    userId = "userId",
+                    isUserVerified = false,
+                    passwordCredentialAutofillViews = passwordCredentialAutofillViews,
+                    beginGetPasswordCredentialOptions = options,
+                )
+            assertTrue(result.isEmpty())
+        }
+
+    @Suppress("MaxLineLength")
+    @Test
+    fun `buildPasswordCredentialEntries should return Success with list of PasswordCredentialEntry`() =
+        runTest {
+            val options = listOf(mockBeginGetPasswordOption)
+            val passwordAutofillViews: List<AutofillCipher.Login> = listOf(
+                createMockPasswordCredentialAutofillCipherLogin(),
+            )
+            every {
+                mockBiometricsEncryptionManager.getOrCreateCipher("userId")
+            } returns null
+
+            val result = credentialEntryBuilder
+                .buildPasswordCredentialEntries(
+                    userId = "userId",
+                    isUserVerified = false,
+                    passwordCredentialAutofillViews = passwordAutofillViews,
+                    beginGetPasswordCredentialOptions = options,
+                )
+
+            assertTrue(result.isNotEmpty())
+
+            verify {
+                mockIntentManager.createPasswordGetCredentialPendingIntent(
+                    action = "com.x8bit.bitwarden.credentials.ACTION_GET_PASSWORD",
+                    userId = "userId",
+                    cipherId = "mockCipherId",
+                    requestCode = any(),
+                    isUserVerified = false,
+                )
+
+                anyConstructed<PasswordCredentialEntry.Builder>().setIcon(mockIcon)
+            }
+        }
+
+    @Test
+    fun `buildPasswordCredentialEntries should set biometric prompt data correctly`() = runTest {
+        mockkStatic(::isBuildVersionBelow)
+        val options = listOf(mockBeginGetPasswordOption)
+        val passwordAutofillViews: List<AutofillCipher.Login> = listOf(
+            createMockPasswordCredentialAutofillCipherLogin(),
+        )
+
+        every {
+            mockBiometricsEncryptionManager.getOrCreateCipher(any())
+        } returns mockk(relaxed = true)
+        every { isBuildVersionBelow(any()) } returns false
+
+        // Verify biometric prompt data is set when buildVersion is >= 35, cipher is
+        // not null, and user is not verified
+        credentialEntryBuilder
+            .buildPasswordCredentialEntries(
+                userId = "userId",
+                isUserVerified = false,
+                passwordCredentialAutofillViews = passwordAutofillViews,
+                beginGetPasswordCredentialOptions = options,
+            )
+        verify(exactly = 1) {
+            anyConstructed<PasswordCredentialEntry.Builder>().setBiometricPromptData(any())
+        }
+    }
+
+    private fun createMockPasswordCredentialAutofillCipherLogin() = AutofillCipher.Login(
+        cipherId = "mockCipherId",
+        name = "Cipher One",
+        isTotpEnabled = false,
+        password = "mock-password",
+        username = "mock-username",
+        subtitle = "Subtitle",
+    )
 }
