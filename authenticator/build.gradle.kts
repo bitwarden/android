26db578277bc9556235922db7389637becdffc56
David Perez <david@livefront.com>
2025-06-23 14:30:15 -0500

    Update Kotlin to v2.2.0

--- authenticator/build.gradle.kts
+++ authenticator/build.gradle.kts
@@ -143,7 +143,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.fromTarget(libs.versions.jvmTarget.get()))
+        jvmTarget = JvmTarget.fromTarget(libs.versions.jvmTarget.get())
     }
 }
 
