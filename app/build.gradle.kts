1e4fc31ed441eaa9a1469b0ebdf5f82cbda8b784
David Perez <david@livefront.com>
2025-06-23 15:57:10 -0500

    Update Kotlin to v2.2.0 (#5408)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -193,7 +193,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.fromTarget(libs.versions.jvmTarget.get()))
+        jvmTarget = JvmTarget.fromTarget(libs.versions.jvmTarget.get())
     }
 }
 
