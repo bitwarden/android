b61e2248b5345cc8284df6ac485d61819e94b3f6
David Perez <david@livefront.com>
2025-06-25 11:25:31 -0500

    Improve autofill version checking

--- app/src/main/kotlin/com/x8bit/bitwarden/data/autofill/util/FillRequestExtensions.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/autofill/util/FillRequestExtensions.kt
@@ -1,6 +1,5 @@
 package com.x8bit.bitwarden.data.autofill.util
 
-import android.annotation.SuppressLint
 import android.os.Build
 import android.service.autofill.FillRequest
 import android.widget.inline.InlinePresentationSpec
@@ -9,12 +8,11 @@ import com.x8bit.bitwarden.data.autofill.model.AutofillAppInfo
 /**
  * Extract the list of [InlinePresentationSpec]s. If it fails, return an empty list.
  */
-@SuppressLint("NewApi")
 fun FillRequest?.getInlinePresentationSpecs(
     autofillAppInfo: AutofillAppInfo,
     isInlineAutofillEnabled: Boolean,
 ): List<InlinePresentationSpec>? =
-    if (autofillAppInfo.sdkInt < Build.VERSION_CODES.R) {
+    if (!autofillAppInfo.isVersionAtLeast(version = Build.VERSION_CODES.R)) {
         // When SDK version is bellow 30, InlinePresentationSpec is not available and null
         // must be returned.
         null
@@ -28,14 +26,13 @@ fun FillRequest?.getInlinePresentationSpecs(
  * Extract the max inline suggestions count. If the OS is below Android R, this will always
  * return 0.
  */
-@SuppressLint("NewApi")
 fun FillRequest?.getMaxInlineSuggestionsCount(
     autofillAppInfo: AutofillAppInfo,
     isInlineAutofillEnabled: Boolean,
 ): Int =
     if (this != null &&
         isInlineAutofillEnabled &&
-        autofillAppInfo.sdkInt >= Build.VERSION_CODES.R
+        autofillAppInfo.isVersionAtLeast(version = Build.VERSION_CODES.R)
     ) {
         inlineSuggestionsRequest?.maxSuggestionCount ?: 0
     } else {
