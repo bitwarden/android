2995aa10760cd973017baa7f097a505996929136
Patrick Honkonen <phonkonen@bitwarden.com>
2025-06-30 13:39:17 -0400

    [PM-23131] Make "About privileged apps" screen scrollable
    The "About privileged apps" screen content is made vertically scrollable.

--- app/src/main/kotlin/com/x8bit/bitwarden/ui/platform/feature/settings/autofill/privilegedapps/about/AboutPrivilegedAppsScreen.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/ui/platform/feature/settings/autofill/privilegedapps/about/AboutPrivilegedAppsScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -51,6 +53,7 @@ fun AboutPrivilegedAppsScreen(
         AboutPrivilegedAppsContent(
             modifier = Modifier
                 .fillMaxWidth()
+                .verticalScroll(state = rememberScrollState())
                 .standardHorizontalMargin(),
         )
     }
