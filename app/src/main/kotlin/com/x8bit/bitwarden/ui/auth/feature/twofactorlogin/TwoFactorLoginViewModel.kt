39b1409cbd3f98e239c6582ea48e5456c531ff79
André Bispo <abispo@bitwarden.com>
2025-07-15 17:31:37 +0100

    [PM-22399] Send 2FA email when view appears (#5498)
    Co-authored-by: Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>

--- app/src/main/kotlin/com/x8bit/bitwarden/ui/auth/feature/twofactorlogin/TwoFactorLoginViewModel.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/ui/auth/feature/twofactorlogin/TwoFactorLoginViewModel.kt
@@ -122,6 +122,13 @@ class TwoFactorLoginViewModel @Inject constructor(
             .map { TwoFactorLoginAction.Internal.ReceiveWebAuthResult(webAuthResult = it) }
             .onEach(::sendAction)
             .launchIn(viewModelScope)
+
+        viewModelScope.launch {
+            // If the auth method is email and it is not to verify the device, call resendEmail.
+            if (state.authMethod == TwoFactorAuthMethod.EMAIL && !state.isNewDeviceVerification) {
+                sendAction(TwoFactorLoginAction.Internal.SendVerificationCodeEmail)
+            }
+        }
     }
 
     override fun handleAction(action: TwoFactorLoginAction) {
@@ -158,6 +165,10 @@ class TwoFactorLoginViewModel @Inject constructor(
 
             is TwoFactorLoginAction.Internal.ReceiveResendEmailResult -> {
                 handleReceiveResendEmailResult(action)
+            }
+
+            TwoFactorLoginAction.Internal.SendVerificationCodeEmail -> {
+                handleSendVerificationCodeEmail()
             }
         }
     }
@@ -476,6 +487,20 @@ class TwoFactorLoginViewModel @Inject constructor(
      * Resend the verification code email.
      */
     private fun handleResendEmailClick() {
+        sendVerificationCodeEmail(isUserInitiated = true)
+    }
+
+    /**
+     * send the verification code email without user interaction.
+     */
+    private fun handleSendVerificationCodeEmail() {
+        sendVerificationCodeEmail(isUserInitiated = false)
+    }
+
+    /**
+     * Send the verification code email.
+     */
+    private fun sendVerificationCodeEmail(isUserInitiated: Boolean) {
         // Ensure that the user is in fact verifying with email.
         if (state.authMethod != TwoFactorAuthMethod.EMAIL) {
             return
@@ -500,7 +525,7 @@ class TwoFactorLoginViewModel @Inject constructor(
             sendAction(
                 TwoFactorLoginAction.Internal.ReceiveResendEmailResult(
                     resendEmailResult = result,
-                    isUserInitiated = true,
+                    isUserInitiated = isUserInitiated,
                 ),
             )
         }
@@ -821,5 +846,10 @@ sealed class TwoFactorLoginAction {
         data class ReceiveWebAuthResult(
             val webAuthResult: WebAuthResult,
         ) : Internal()
+
+        /**
+         * Indicates that the verification code email should be sent.
+         */
+        data object SendVerificationCodeEmail : Internal()
     }
 }
