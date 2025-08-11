9bfbe0c08724b08f6225777df5d7ace23a5d4ac3
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-08-11 15:37:30 -0400

    [PM-24568] Add `accountKeys` to `SyncResponseJson.Profile` (#5678)

--- network/src/main/kotlin/com/bitwarden/network/model/SyncResponseJson.kt
+++ network/src/main/kotlin/com/bitwarden/network/model/SyncResponseJson.kt
@@ -142,6 +142,8 @@ data class SyncResponseJson(
      * @property isEmailVerified If the profile has a verified email.
      * @property isTwoFactorEnabled If the profile has two factor authentication enabled.
      * @property privateKey The private key of the profile (nullable).
+     * @property accountKeys The account keys associated with the profile. This is temporarily
+     * nullable to maintain backwards compatibility.
      * @property isPremium If the profile is premium.
      * @property culture The culture of the profile (nullable).
      * @property name The name of the profile (nullable).
@@ -175,8 +177,12 @@ data class SyncResponseJson(
         @SerialName("twoFactorEnabled")
         val isTwoFactorEnabled: Boolean,
 
+        @Deprecated("Use accountKeys instead", ReplaceWith("accountKeys"))
         @SerialName("privateKey")
         val privateKey: String?,
+
+        @SerialName("accountKeys")
+        val accountKeys: AccountKeys?,
 
         @SerialName("premium")
         val isPremium: Boolean,
@@ -410,6 +416,77 @@ data class SyncResponseJson(
             @SerialName("managePolicies")
             val shouldManagePolicies: Boolean,
         )
+
+        /**
+         * Represents private keys in the vault response.
+         *
+         * @property signatureKeyPair The signature key pair of the profile.
+         * @property publicKeyEncryptionKeyPair The public key encryption key pair of the profile.
+         * @property securityState The security state of the profile (nullable).
+         */
+        @Serializable
+        data class AccountKeys(
+            @SerialName("signatureKeyPair")
+            val signatureKeyPair: SignatureKeyPair?,
+
+            @SerialName("publicKeyEncryptionKeyPair")
+            val publicKeyEncryptionKeyPair: PublicKeyEncryptionKeyPair,
+
+            @SerialName("securityState")
+            val securityState: SecurityState?,
+        ) {
+
+            /**
+             * Represents a signature key pair in the vault response.
+             *
+             * @property wrappedSigningKey The wrapped signing key of the signature key pair.
+             * @property verifyingKey The verifying key of the signature key pair.
+             */
+            @Serializable
+            data class SignatureKeyPair(
+                @SerialName("wrappedSigningKey")
+                val wrappedSigningKey: String,
+
+                @SerialName("verifyingKey")
+                val verifyingKey: String,
+            )
+
+            /**
+             * Represents a public key encryption key pair in the vault response.
+             *
+             * @property wrappedPrivateKey The wrapped private key of the public key encryption key
+             * pair.
+             * @property publicKey The public key of the public key encryption key pair.
+             * @property signedPublicKey The signed public key of the public key encryption key pair
+             * (nullable).
+             */
+            @Serializable
+            data class PublicKeyEncryptionKeyPair(
+                @SerialName("wrappedPrivateKey")
+                val wrappedPrivateKey: String,
+
+                @SerialName("publicKey")
+                val publicKey: String,
+
+                @SerialName("signedPublicKey")
+                val signedPublicKey: String?,
+            )
+
+            /**
+             * Represents security state in the vault response.
+             *
+             * @property securityState The security state of the profile.
+             * @property securityVersion The security version of the profile.
+             */
+            @Serializable
+            data class SecurityState(
+                @SerialName("securityState")
+                val securityState: String,
+
+                @SerialName("securityVersion")
+                val securityVersion: Int,
+            )
+        }
     }
 
     /**
