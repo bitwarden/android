6a9437d0774d8652cadf81360484173d32bd28d5
Andy Pixley <3723676+pixman20@users.noreply.github.com>
2025-08-11 17:36:43 -0400

    [BRE-1074] Removing old debug message

--- fastlane/patches/supply_custom_promote.rb
+++ fastlane/patches/supply_custom_promote.rb
@@ -26,7 +26,6 @@ module Supply
       end
 
       releases = track_from.releases
-      # UI.message("Track contents: #{track_from.to_json}")
 
       version_code = Supply.config[:version_code].to_s
       if !Supply.config[:skip_release_verification]
