474ec4907f62fe67461cd073e1233c1b76006239
David Perez <david@livefront.com>
2025-08-14 13:21:24 -0500

    PM-24726: Update MDM functionality (#5694)

--- app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/restriction/RestrictionManagerImpl.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/restriction/RestrictionManagerImpl.kt
@@ -1,57 +1,19 @@
 package com.x8bit.bitwarden.data.platform.manager.restriction
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.RestrictionsManager
-import android.os.Bundle
-import com.bitwarden.data.manager.DispatcherManager
+import com.bitwarden.data.datasource.disk.model.EnvironmentUrlDataJson
 import com.bitwarden.data.repository.model.Environment
-import com.x8bit.bitwarden.data.platform.manager.AppStateManager
-import com.x8bit.bitwarden.data.platform.manager.model.AppForegroundState
 import com.x8bit.bitwarden.data.platform.repository.EnvironmentRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 
 /**
  * The default implementation of the [RestrictionManager].
  */
 class RestrictionManagerImpl(
-    appStateManager: AppStateManager,
-    dispatcherManager: DispatcherManager,
-    private val context: Context,
     private val environmentRepository: EnvironmentRepository,
     private val restrictionsManager: RestrictionsManager,
 ) : RestrictionManager {
-    private val mainScope = CoroutineScope(dispatcherManager.main)
-    private val intentFilter = IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED)
-    private val restrictionsChangedReceiver = RestrictionsChangedReceiver()
-    private var isReceiverRegistered = false
 
-    init {
-        appStateManager
-            .appForegroundStateFlow
-            .onEach {
-                when (it) {
-                    AppForegroundState.BACKGROUNDED -> handleBackground()
-                    AppForegroundState.FOREGROUNDED -> handleForeground()
-                }
-            }
-            .launchIn(mainScope)
-    }
-
-    private fun handleBackground() {
-        if (isReceiverRegistered) {
-            context.unregisterReceiver(restrictionsChangedReceiver)
-        }
-        isReceiverRegistered = false
-    }
-
-    private fun handleForeground() {
-        context.registerReceiver(restrictionsChangedReceiver, intentFilter)
-        isReceiverRegistered = true
+    override fun initialize() {
         updatePreconfiguredRestrictionSettings()
     }
 
@@ -59,65 +21,22 @@ class RestrictionManagerImpl(
         restrictionsManager
             .applicationRestrictions
             ?.takeUnless { it.isEmpty }
-            ?.let { setPreconfiguredSettings(it) }
-    }
-
-    private fun setPreconfiguredSettings(bundle: Bundle) {
-        bundle
-            .getString(BASE_ENVIRONMENT_URL_RESTRICTION_KEY)
+            ?.getString(BASE_ENVIRONMENT_URL_RESTRICTION_KEY)
             ?.let { url -> setPreconfiguredUrl(baseEnvironmentUrl = url) }
     }
 
     private fun setPreconfiguredUrl(baseEnvironmentUrl: String) {
-        environmentRepository.environment = when (val current = environmentRepository.environment) {
-            Environment.Us -> {
-                when (baseEnvironmentUrl) {
-                    // If the base matches the predefined US environment, leave it alone
-                    Environment.Us.environmentUrlData.base -> current
-                    // If the base does not match the predefined US environment, create a
-                    // self-hosted environment with the new base
-                    else -> current.toSelfHosted(base = baseEnvironmentUrl)
-                }
-            }
-
-            Environment.Eu -> {
-                when (baseEnvironmentUrl) {
-                    // If the base matches the predefined EU environment, leave it alone
-                    Environment.Eu.environmentUrlData.base -> current
-                    // If the base does not match the predefined EU environment, create a
-                    // self-hosted environment with the new base
-                    else -> current.toSelfHosted(base = baseEnvironmentUrl)
-                }
-            }
-
-            is Environment.SelfHosted -> current.toSelfHosted(base = baseEnvironmentUrl)
-        }
-    }
-
-    /**
-     * A [BroadcastReceiver] used to listen for [Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED]
-     * updates.
-     *
-     * Note: The `Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED` will only be received if the
-     * `BroadcastReceiver` is dynamically registered, so this cannot be registered in the manifest.
-     */
-    private inner class RestrictionsChangedReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            if (intent.action == Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED) {
-                updatePreconfiguredRestrictionSettings()
-            }
+        environmentRepository.environment = when (baseEnvironmentUrl) {
+            // If the baseEnvironmentUrl matches the predefined US environment, assume it is the
+            // default US environment.
+            Environment.Us.environmentUrlData.base -> Environment.Us
+            // If the baseEnvironmentUrl matches the predefined EU environment, assume it is the
+            // default EU environment.
+            Environment.Eu.environmentUrlData.base -> Environment.Eu
+            // Otherwise make a custom self-host environment.
+            else -> Environment.SelfHosted(EnvironmentUrlDataJson(baseEnvironmentUrl))
         }
     }
 }
 
 private const val BASE_ENVIRONMENT_URL_RESTRICTION_KEY: String = "baseEnvironmentUrl"
-
-/**
- * Helper method for creating a new [Environment.SelfHosted] with a new base.
- */
-private fun Environment.toSelfHosted(
-    base: String,
-): Environment.SelfHosted =
-    Environment.SelfHosted(
-        environmentUrlData = environmentUrlData.copy(base = base),
-    )
--- app/src/main/kotlin/com/x8bit/bitwarden/data/platform/repository/EnvironmentRepositoryImpl.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/platform/repository/EnvironmentRepositoryImpl.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import timber.log.Timber
@@ -20,7 +21,7 @@ import timber.log.Timber
  */
 class EnvironmentRepositoryImpl(
     private val environmentDiskSource: EnvironmentDiskSource,
-    authDiskSource: AuthDiskSource,
+    private val authDiskSource: AuthDiskSource,
     dispatcherManager: DispatcherManager,
 ) : EnvironmentRepository {
 
@@ -44,16 +45,13 @@ class EnvironmentRepositoryImpl(
             initialValue = environment,
         )
 
-    init {
+    override fun initialize() {
         authDiskSource
             .userStateFlow
-            .onEach { userState ->
+            .mapNotNull { userState -> userState?.activeAccount?.settings?.environmentUrlData }
+            .onEach { environmentUrlDataJson ->
                 // If the active account has environment data, set that as the current value.
-                userState
-                    ?.activeAccount
-                    ?.settings
-                    ?.environmentUrlData
-                    ?.let { environmentDiskSource.preAuthEnvironmentUrlData = it }
+                environmentDiskSource.preAuthEnvironmentUrlData = environmentUrlDataJson
             }
             .launchIn(scope)
     }
