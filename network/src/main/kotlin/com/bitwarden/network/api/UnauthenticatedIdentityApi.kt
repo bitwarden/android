a38b0bf522c66ad9c3e7387a68127e1a03cd9e66
sneakernuts <671942+sneakernuts@users.noreply.github.com>
2025-08-18 10:16:58 -0600

    removed import for Header

--- network/src/main/kotlin/com/bitwarden/network/api/UnauthenticatedIdentityApi.kt
+++ network/src/main/kotlin/com/bitwarden/network/api/UnauthenticatedIdentityApi.kt
@@ -17,7 +17,6 @@ import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
