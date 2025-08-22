3facfc9b426007270359853930805059c5fca19a
Carlos Gonçalves <cgoncalves@bitwarden.com>
2025-08-22 15:52:47 +0100

    pm-24990 Removed unused imports

--- app/src/test/kotlin/com/x8bit/bitwarden/data/vault/repository/util/VaultSdkCollectionExtensionsTest.kt
+++ app/src/test/kotlin/com/x8bit/bitwarden/data/vault/repository/util/VaultSdkCollectionExtensionsTest.kt
@@ -2,10 +2,8 @@ package com.x8bit.bitwarden.data.vault.repository.util
 
 import com.bitwarden.collections.Collection
 import com.bitwarden.collections.CollectionType
-import com.bitwarden.network.model.CipherTypeJson
 import com.bitwarden.network.model.CollectionTypeJson
 import com.bitwarden.network.model.SyncResponseJson
-import com.bitwarden.vault.CipherType
 import com.x8bit.bitwarden.data.vault.datasource.sdk.model.createMockCollectionView
 import org.junit.Assert
 import org.junit.Test
