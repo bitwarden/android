c4adf3ad423c6bb52c88cfc1956e769ee4653367
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-09-11 10:06:57 -0400

    [PM-25661] Add placeholder ProviderEvents API for credential import/export (#5866)

---
+++ cxf/src/main/kotlin/androidx/credentials/providerevents/exception/ImportCredentialsCancellationException.kt
@@ -0,0 +1,6 @@
+package androidx.credentials.providerevents.exception
+
+/**
+ * Placeholder class representing a cancellation exception for importing credentials.
+ */
+class ImportCredentialsCancellationException : Exception()
