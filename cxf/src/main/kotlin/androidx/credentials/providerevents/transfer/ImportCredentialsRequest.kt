ff03f49f43e5a5bba09dccf1373b00a0afd94c78
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-09-17 16:42:42 -0400

    [PM-25912] Remove ImportCredentialsRequest (#5901)

--- cxf/src/main/kotlin/androidx/credentials/providerevents/transfer/ImportCredentialsRequest.kt
+++
@@ -1,6 +0,0 @@
-package androidx.credentials.providerevents.transfer
-
-/**
- * Placeholder class representing a request to import credentials.
- */
-data class ImportCredentialsRequest(val requestJson: String)
