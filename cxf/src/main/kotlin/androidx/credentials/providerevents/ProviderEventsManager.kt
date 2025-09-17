293c0e3e06167494e7a3b47f751696ffc90b4e11
Patrick Honkonen <phonkonen@bitwarden.com>
2025-09-17 17:46:09 -0400

    Update `registerExport` to return `RegisterExportResponse`
    The `registerExport` function in `ProviderEventsManager` and `CredentialExchangeRegistry` now returns a `RegisterExportResponse` instead of a `Boolean`.
    
    A placeholder `RegisterExportResponse` class has been added.
    
    The `StubProviderEventsManager` has been updated to return a `RegisterExportResponse` and provide canned data for `importCredentials` for testing purposes.

--- cxf/src/main/kotlin/androidx/credentials/providerevents/ProviderEventsManager.kt
+++ cxf/src/main/kotlin/androidx/credentials/providerevents/ProviderEventsManager.kt
@@ -2,11 +2,13 @@ package androidx.credentials.providerevents
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.SigningInfo
 import androidx.credentials.provider.CallingAppInfo
 import androidx.credentials.providerevents.transfer.ImportCredentialsRequest
 import androidx.credentials.providerevents.transfer.ImportCredentialsResponse
 import androidx.credentials.providerevents.transfer.ProviderImportCredentialsResponse
 import androidx.credentials.providerevents.transfer.RegisterExportRequest
+import androidx.credentials.providerevents.transfer.RegisterExportResponse
 import com.bitwarden.annotation.OmitFromCoverage
 
 /**
@@ -17,7 +19,7 @@ interface ProviderEventsManager {
     /**
      * Register as a credential export source.
      */
-    fun registerExport(request: RegisterExportRequest): Boolean
+    fun registerExport(request: RegisterExportRequest): RegisterExportResponse
 
     /**
      * Begin the process of importing credentials.
@@ -42,8 +44,8 @@ interface ProviderEventsManager {
  */
 @OmitFromCoverage
 internal class StubProviderEventsManager : ProviderEventsManager {
-    override fun registerExport(request: RegisterExportRequest): Boolean {
-        return true
+    override fun registerExport(request: RegisterExportRequest): RegisterExportResponse {
+        return RegisterExportResponse()
     }
 
     override fun importCredentials(
@@ -53,13 +55,57 @@ internal class StubProviderEventsManager : ProviderEventsManager {
         @SuppressLint("VisibleForTests")
         return ProviderImportCredentialsResponse(
             response = ImportCredentialsResponse(
-                responseJson = "",
+                responseJson = CANNED_RESPONSE,
             ),
             callingAppInfo = CallingAppInfo(
-                packageName = "",
-                signatures = emptyList(),
+                packageName = "${context.packageName}",
+                signingInfo = SigningInfo(),
                 origin = null,
             ),
         )
     }
 }
+
+private val CANNED_RESPONSE = """
+    {
+      "id": "3zGV3pmoSs6mT7IEAPXfOw",
+      "username": "",
+      "email": "user@email.com",
+      "fullName": "Test User",
+      "collections": [],
+      "items": [
+        {
+          "id": "8cCs0RV_ViySk7KCACA",
+          "creationAt": 1739325421,
+          "modifiedAt": 1739325421,
+          "title": "test import credentials",
+          "favorite": false,
+          "scope": {
+            "urls": [
+              "https://www.sample-url.io/"
+            ],
+            "androidApps": []
+          },
+          "credentials": [
+            {
+              "type": "basic-auth",
+              "username": {
+                "fieldType": "string",
+                "value": "MyUsername"
+              }
+            },
+            {
+              "type": "passkey",
+              "credentialId": "xMA-5emp0WsQASnuNmuzQA",
+              "rpId": "www.sample-url.io",
+              "username": "user@email.com",
+              "userDisplayName": "user@email.com",
+              "userHandle": "lEn2KqNnS7SsUdVbrdoFiw",
+              "key": "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgQx8Smx_KdvQ7nJXt2_62Xrn-im9ibCOtsphj_xZo_uWhRANCAARUDaIFJIUaRyUehAy_d1_a-DK63Ws_d-zkYj-uqHdrGZI0dnhazQGva4tJZQFN35iLoLzFFj_CSjqeYAEOX7Ck"
+            }
+          ]
+        }
+      ]
+    }
+"""
+    .trimIndent()
