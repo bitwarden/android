3c2306a6df11e2c4e3ece76ec0deac112a7cdad5
Carlos Gonçalves <cgoncalves@bitwarden.com>
2025-09-29 12:00:11 +0100

    Add token

--- authenticator/src/main/kotlin/com/bitwarden/authenticator/data/platform/manager/SdkClientManagerImpl.kt
+++ authenticator/src/main/kotlin/com/bitwarden/authenticator/data/platform/manager/SdkClientManagerImpl.kt
@@ -1,12 +1,27 @@
 package com.bitwarden.authenticator.data.platform.manager
 
+import com.bitwarden.core.ClientManagedTokens
 import com.bitwarden.sdk.Client
+
+/**
+ * The token provider to pass to the SDK.
+ */
+class Token : ClientManagedTokens {
+    override suspend fun getAccessToken(): String? {
+        return null
+    }
+}
 
 /**
  * Primary implementation of [SdkClientManager].
  */
 class SdkClientManagerImpl(
-    private val clientProvider: suspend () -> Client = { Client(null) },
+    private val clientProvider: suspend () -> Client = {
+        Client(
+            tokenProvider = Token(),
+            settings = null,
+        )
+    },
 ) : SdkClientManager {
     private var client: Client? = null
 
