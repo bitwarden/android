1279c814af13c447f2900eaa1b3a0d919a9cef43
André Bispo <abispo@bitwarden.com>
2025-10-01 14:46:22 +0100

    [PM-23278] Fix server naming inconsistency.

--- network/src/main/kotlin/com/bitwarden/network/model/MasterPasswordUnlockDataJson.kt
+++ network/src/main/kotlin/com/bitwarden/network/model/MasterPasswordUnlockDataJson.kt
@@ -1,12 +1,15 @@
 package com.bitwarden.network.model
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 /**
  * Represents the data used to unlock with the master password.
  */
 @Serializable
+@OptIn(ExperimentalSerializationApi::class)
 data class MasterPasswordUnlockDataJson(
     @SerialName("Salt")
     val salt: String,
@@ -14,6 +17,9 @@ data class MasterPasswordUnlockDataJson(
     @SerialName("Kdf")
     val kdf: KdfJson,
 
+    // TODO: PM-26397 this was done due to naming inconsistency server side,
+    //  should be cleaned up when server side is updated
     @SerialName("MasterKeyWrappedUserKey")
+    @JsonNames("MasterKeyEncryptedUserKey")
     val masterKeyWrappedUserKey: String,
 )
