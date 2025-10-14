d15bb61d4c9ea15b72f00a21165ac8187f449d93
David Perez <david@livefront.com>
2025-10-14 11:43:40 -0500

    Move camera ui composables to ui module

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.browser)
     implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
     implementation(libs.androidx.compose.animation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive)
