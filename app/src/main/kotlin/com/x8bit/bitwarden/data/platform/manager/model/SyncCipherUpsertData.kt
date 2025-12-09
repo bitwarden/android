4641fd8709b6035e77cd4ad76d1e3b4bc00d62f6
David Perez <david@livefront.com>
2025-12-09 11:53:52 -0600

    PM-1908: Push notifications for non-active accounts prompt for future sync

--- app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/model/SyncCipherUpsertData.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/model/SyncCipherUpsertData.kt
@@ -5,12 +5,14 @@ import java.time.ZonedDateTime
 /**
  * Required data for sync cipher upsert operations.
  *
+ * @property userId The user ID associated with this update.
  * @property cipherId The cipher ID.
  * @property revisionDate The cipher's revision date. This is used to determine if the local copy of
  * the cipher is out-of-date.
  * @property isUpdate Whether or not this is an update of an existing cipher.
  */
 data class SyncCipherUpsertData(
+    val userId: String,
     val cipherId: String,
     val revisionDate: ZonedDateTime,
     val organizationId: String?,
--- app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/model/SyncFolderUpsertData.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/model/SyncFolderUpsertData.kt
@@ -5,12 +5,14 @@ import java.time.ZonedDateTime
 /**
  * Required data for sync folder upsert operations.
  *
+ * @property userId The user ID associated with this update.
  * @property folderId The folder ID.
  * @property revisionDate The folder's revision date. This is used to determine if the local copy of
  * the folder is out-of-date.
  * @property isUpdate Whether or not this is an update of an existing folder.
  */
 data class SyncFolderUpsertData(
+    val userId: String,
     val folderId: String,
     val revisionDate: ZonedDateTime,
     val isUpdate: Boolean,
