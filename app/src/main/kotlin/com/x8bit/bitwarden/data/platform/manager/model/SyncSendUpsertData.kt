00cded3a020f70067a85e70f0ce135303caee400
David Perez <david@livefront.com>
2025-12-10 09:27:09 -0600

    PM-1908: Push notifications for non-active accounts prompt for future sync (#6252)

--- app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/model/SyncSendUpsertData.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/platform/manager/model/SyncSendUpsertData.kt
@@ -5,12 +5,14 @@ import java.time.ZonedDateTime
 /**
  * Required data for sync send upsert operations.
  *
+ * @property userId The user ID associated with this update.
  * @property sendId The send ID.
  * @property revisionDate The send's revision date. This is used to determine if the local copy of
  * the send is out-of-date.
  * @property isUpdate Whether or not this is an update of an existing send.
  */
 data class SyncSendUpsertData(
+    val userId: String,
     val sendId: String,
     val revisionDate: ZonedDateTime,
     val isUpdate: Boolean,
