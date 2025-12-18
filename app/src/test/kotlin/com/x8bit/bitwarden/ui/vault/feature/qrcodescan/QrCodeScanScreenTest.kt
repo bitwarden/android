913d877737d5fc109e1610505927470dd3d39aff
David Perez <david@livefront.com>
2025-12-18 15:47:12 -0600

    Remove flaky tests (#6278)

--- app/src/test/kotlin/com/x8bit/bitwarden/ui/vault/feature/qrcodescan/QrCodeScanScreenTest.kt
+++ app/src/test/kotlin/com/x8bit/bitwarden/ui/vault/feature/qrcodescan/QrCodeScanScreenTest.kt
@@ -1,6 +1,5 @@
 package com.x8bit.bitwarden.ui.vault.feature.qrcodescan
 
-import androidx.camera.core.ImageProxy
 import androidx.compose.ui.test.onNodeWithText
 import com.bitwarden.core.data.repository.util.bufferedMutableSharedFlow
 import com.bitwarden.ui.platform.feature.qrcodescan.util.FakeQrCodeAnalyzer
@@ -20,7 +19,6 @@ class QrCodeScanScreenTest : BitwardenComposeTest() {
     private var onNavigateBackCalled = false
     private var onNavigateToManualCodeEntryScreenCalled = false
 
-    private val imageProxy: ImageProxy = mockk()
     private val qrCodeAnalyzer = FakeQrCodeAnalyzer()
 
     private val mutableEventFlow = bufferedMutableSharedFlow<QrCodeScanEvent>()
@@ -54,30 +52,6 @@ class QrCodeScanScreenTest : BitwardenComposeTest() {
     fun `on NavigateToManualCodeEntry event should invoke onNavigateToManualCodeEntryScreen`() {
         mutableEventFlow.tryEmit(QrCodeScanEvent.NavigateToManualCodeEntry)
         assertTrue(onNavigateToManualCodeEntryScreenCalled)
-    }
-
-    @Test
-    fun `when a scan is successful a result will be sent`() = runTest {
-        val result = "testCode"
-
-        qrCodeAnalyzer.scanResult = result
-        qrCodeAnalyzer.analyze(imageProxy)
-
-        verify {
-            viewModel.trySendAction(QrCodeScanAction.QrCodeScanReceive(result))
-        }
-    }
-
-    @Test
-    fun `when a scan is unsuccessful a result will not be sent`() = runTest {
-        val result = "testCode"
-
-        qrCodeAnalyzer.scanResult = null
-        qrCodeAnalyzer.analyze(imageProxy)
-
-        verify(exactly = 0) {
-            viewModel.trySendAction(QrCodeScanAction.QrCodeScanReceive(result))
-        }
     }
 
     @Config(qualifiers = "land")
