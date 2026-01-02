3c242377053988ce1e17b6bcc826280e79540baa
Gavin Gui <guizichen2000@outlook.com>
2026-01-02 10:12:41 -0700

    Skip biometric prompt on Xiaomi HyperOS

--- app/src/main/kotlin/com/x8bit/bitwarden/data/credentials/util/CredentialEntryBuilderExtensions.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/credentials/util/CredentialEntryBuilderExtensions.kt
@@ -7,15 +7,17 @@ import androidx.credentials.provider.PasswordCredentialEntry
 import androidx.credentials.provider.PublicKeyCredentialEntry
 import com.bitwarden.annotation.OmitFromCoverage
 import com.bitwarden.core.util.isBuildVersionAtLeast
+import com.bitwarden.core.util.isHyperOS
 import javax.crypto.Cipher
 
 /**
  * Sets the biometric prompt data on the [PublicKeyCredentialEntry.Builder] if supported.
+ * Note: Xiaomi HyperOS is known to not support biometric prompt.
  */
 fun PublicKeyCredentialEntry.Builder.setBiometricPromptDataIfSupported(
     cipher: Cipher?,
 ): PublicKeyCredentialEntry.Builder =
-    if (isBuildVersionAtLeast(Build.VERSION_CODES.VANILLA_ICE_CREAM) && cipher != null) {
+    if (isBuildVersionAtLeast(Build.VERSION_CODES.VANILLA_ICE_CREAM) && !isHyperOS() && cipher != null) {
         setBiometricPromptData(
             biometricPromptData = buildPromptDataWithCipher(cipher),
         )
@@ -25,11 +27,12 @@ fun PublicKeyCredentialEntry.Builder.setBiometricPromptDataIfSupported(
 
 /**
  * Sets the biometric prompt data on the [PasswordCredentialEntry.Builder] if supported.
+ * Note: Xiaomi HyperOS is known to not support biometric prompt.
  */
 fun PasswordCredentialEntry.Builder.setBiometricPromptDataIfSupported(
     cipher: Cipher?,
 ): PasswordCredentialEntry.Builder =
-    if (isBuildVersionAtLeast(Build.VERSION_CODES.VANILLA_ICE_CREAM) && cipher != null) {
+    if (isBuildVersionAtLeast(Build.VERSION_CODES.VANILLA_ICE_CREAM) && !isHyperOS() && cipher != null) {
         setBiometricPromptData(
             biometricPromptData = buildPromptDataWithCipher(cipher),
         )
