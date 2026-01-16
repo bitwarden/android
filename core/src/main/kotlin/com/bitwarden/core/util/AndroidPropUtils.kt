70aa1ac5c682dc345106efe39ad781ae2452a2b3
Andre Rosado <arosado@bitwarden.com>
2026-01-16 13:56:22 +0000

    Set landscape orientation for HorizonOS devices

--- core/src/main/kotlin/com/bitwarden/core/util/AndroidPropUtils.kt
+++ core/src/main/kotlin/com/bitwarden/core/util/AndroidPropUtils.kt
@@ -5,11 +5,25 @@ package com.bitwarden.core.util
 import com.bitwarden.annotation.OmitFromCoverage
 
 private const val KEY_XIAOMI_HYPER_OS_NAME = "ro.mi.os.version.name"
+private const val HORIZON_OS_SDK = "horizonos.os.Build\$HorizonOsSdk"
 
 /**
  * Returns true if the device is running Xiaomi HyperOS.
  */
 fun isHyperOS(): Boolean = !getSystemProperty(KEY_XIAOMI_HYPER_OS_NAME).isNullOrEmpty()
+
+/**
+ * Returns true if the device is running Horizon OS.
+ */
+fun isHorizonOS(): Boolean {
+    return try {
+        val horizonOsSdk = Class.forName(HORIZON_OS_SDK)
+        val getVersionMethod = horizonOsSdk.getMethod("getVersion")
+        getVersionMethod.invoke(null) != null
+    } catch (_: Throwable) {
+        false
+    }
+}
 
 /**
  * Reads an Android system property using the android.os.SystemProperties API
