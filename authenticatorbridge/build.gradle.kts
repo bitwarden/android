ba2eda1ad1b184c3b346060154aa5651388c37b5
David Perez <david@livefront.com>
2026-01-28 13:30:40 -0600

    Update build optimizations

--- authenticatorbridge/build.gradle.kts
+++ authenticatorbridge/build.gradle.kts
@@ -75,15 +75,3 @@ dependencies {
     testImplementation(libs.mockk.mockk)
     testImplementation(libs.square.turbine)
 }
-
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-    @Suppress("MagicNumber")
-    forkEvery = 100
-    maxHeapSize = "2g"
-    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1)
-    jvmArgs = jvmArgs.orEmpty() + "-XX:+UseParallelGC" +
-        // Explicitly setting the user Country and Language because tests assume en-US
-        "-Duser.country=US" +
-        "-Duser.language=en"
-}
