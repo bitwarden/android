ce5356d273e789bd186eed798040d312405b5409
Andre Rosado <arosado@bitwarden.com>
2026-02-13 19:02:50 +0000

    addressed PR comment

--- app/src/main/kotlin/com/x8bit/bitwarden/ui/tools/feature/send/addedit/AddEditSendViewModel.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/ui/tools/feature/send/addedit/AddEditSendViewModel.kt
@@ -587,7 +587,7 @@ class AddEditSendViewModel @Inject constructor(
 
             val updatedEmails = currentAuth.emails.map { authEmail ->
                 if (authEmail.id == action.authEmail.id) {
-                    authEmail.copy(value = action.authEmail.value)
+                    action.authEmail
                 } else {
                     authEmail
                 }
