e0d880860ff051e41a18a67e3bd109986a36bc9a
Patrick Honkonen <phonkonen@bitwarden.com>
2026-03-10 16:39:14 -0400

    [PM-33510] feat: Add Play Billing Library dependency and PlayBillingManager

--- app/src/main/kotlin/com/x8bit/bitwarden/data/billing/repository/BillingRepository.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/billing/repository/BillingRepository.kt
@@ -2,11 +2,17 @@ package com.x8bit.bitwarden.data.billing.repository
 
 import com.x8bit.bitwarden.data.billing.repository.model.CheckoutSessionResult
 import com.x8bit.bitwarden.data.billing.repository.model.CustomerPortalResult
+import kotlinx.coroutines.flow.StateFlow
 
 /**
  * Provides an API for managing billing operations.
  */
 interface BillingRepository {
+
+    /**
+     * Emits `true` when in-app billing is supported, or `false` otherwise.
+     */
+    val isInAppBillingSupportedFlow: StateFlow<Boolean>
 
     /**
      * Creates a Stripe checkout session and returns the checkout URL.
