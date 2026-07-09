fe0696e39c0e792a94eac041cdbdf0c3aa5ec556
Álison Fernandes <vvolkgang@users.noreply.github.com>
2026-07-09 21:57:37 +0100

    format

--- cxf/src/testFdroid/kotlin/com/bitwarden/cxf/importer/CredentialExchangeImporterTest.kt
+++ cxf/src/testFdroid/kotlin/com/bitwarden/cxf/importer/CredentialExchangeImporterTest.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import com.bitwarden.cxf.importer.model.ImportCredentialsSelectionResult
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -13,7 +12,7 @@ class CredentialExchangeImporterTest {
     private val importer = CredentialExchangeImporterImpl(
         activity = mockk<Activity>(relaxed = true),
     )
-    
+
     @Test
     fun `importCredentials should return Failure`() = runTest {
         val result = importer.importCredentials(listOf("basic-auth"))
