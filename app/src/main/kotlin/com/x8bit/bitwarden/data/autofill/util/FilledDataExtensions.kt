7152a4389de490c12e96566a08e6cb6ea0e57781
Andre Rosado <arosado@bitwarden.com>
2026-07-31 16:21:25 +0100

    Displaying Identities vault list when autofilling for an Identity

--- app/src/main/kotlin/com/x8bit/bitwarden/data/autofill/util/FilledDataExtensions.kt
+++ app/src/main/kotlin/com/x8bit/bitwarden/data/autofill/util/FilledDataExtensions.kt
@@ -37,10 +37,7 @@ fun FilledData.buildVaultItemDataset(
         type = when (this.originalPartition) {
             is AutofillPartition.Card -> AutofillSelectionData.Type.CARD
             is AutofillPartition.Login -> AutofillSelectionData.Type.LOGIN
-            // AutofillSelectionData.Type has no IDENTITY value yet (deliberately deferred -- it
-            // cascades into the vault item listing/add-edit UI flows). This is a no-op placeholder
-            // today since nothing yet produces an Identity-only partition.
-            is AutofillPartition.Identity -> AutofillSelectionData.Type.LOGIN
+            is AutofillPartition.Identity -> AutofillSelectionData.Type.IDENTITY
         },
         uri = this.uri,
     )
